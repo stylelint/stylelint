737cd3a4e6ee9da67a9b70ac35a90a2de4165b5c
Richard Hallows <hello@richardhallows.com>
2015-05-10 18:01:09 +0100

    Correct trailing-semicolon name

--- src/rules/index.js
+++ src/rules/index.js
@@ -1,6 +1,6 @@
 export default {
   "block-opening-brace-after": require("./block-opening-brace-after"),
   "block-opening-brace-before": require("./block-opening-brace-before"),
+  "declaration-block-trailing-semicolon": require("./declaration-block-trailing-semicolon"),
   "declaration-no-important": require("./declaration-no-important"),
-  "value-trailing-semicolon": require("./value-trailing-semicolon"),
 }
