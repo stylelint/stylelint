6da124c3ff9e8873d16710552f80d459c7f480e9
David Clark <david.dave.clark@gmail.com>
2015-05-10 21:07:02 -0700

    Add number-leading-zero to rule index

--- src/rules/index.js
+++ src/rules/index.js
@@ -3,5 +3,6 @@ export default {
   "block-opening-brace-before": require("./block-opening-brace-before"),
   "declaration-block-trailing-semicolon": require("./declaration-block-trailing-semicolon"),
   "declaration-no-important": require("./declaration-no-important"),
+  "number-leading-zero": require("./number-leading-zero"),
   "rule-set-no-single-line": require("./rule-set-no-single-line"),
 }
