8b64ccde17910f0cca4a60dd18840a4169c8c46e
David Clark <david.dave.clark@gmail.com>
2015-05-12 19:14:54 -0700

    Add selector-combinator-space tests

--- src/rules/selector-combinator-space/__tests__/index.js
+++ src/rules/selector-combinator-space/__tests__/index.js
@@ -3,6 +3,218 @@ import selectorCombinatorSpace, { ruleName, messages } from ".."
 
 const testSelectorCombinatorSpace = ruleTester(selectorCombinatorSpace, ruleName)
 
+testSelectorCombinatorSpace({ before: "always" }, tr => {
+  tr.ok("a {}", "no combinator")
+  tr.ok("a + a {}", "space before and after + combinator")
+  tr.ok("a > a {}", "space before and after > combinator")
+  tr.ok("a ~ a {}", "space before and after ~ combinator")
+  tr.ok(".foo ~ a + bar {}", "multiple spaced combinators")
+  tr.ok("a +a {}", "space before and none after + combinator")
+  tr.ok("a >a {}", "space before and none after > combinator")
+  tr.ok("a ~a {}", "space before and none after ~ combinator")
+  tr.ok("a +\na {}", "space before and newline after + combinator")
+  tr.ok("a >\na {}", "space before and newline after > combinator")
+  tr.ok("a ~\na {}", "space before and newline after ~ combinator")
+  tr.ok(".foo ~a +bar {}", "multiple combinators with space before and none after")
+
+  tr.notOk(
+    "a  +a {}",
+    "two spaces before + combinator",
+    messages.expectedBefore("+")
+  )
+  tr.notOk(
+    "a\n+ a {}",
+    "newline before + combinator",
+    messages.expectedBefore("+")
+  )
+  tr.notOk(
+    "a+a {}",
+    "no space before + combinator",
+    messages.expectedBefore("+")
+  )
+  tr.notOk(
+    "a>a {}",
+    "no space before > combinator",
+    messages.expectedBefore(">")
+  )
+  tr.notOk(
+    "a~a {}",
+    "no space before ~ combinator",
+    messages.expectedBefore("~")
+  )
+  tr.notOk(
+    "a + .foo.bar~ a {}",
+    "multiple combinators: no space before ~ combinator",
+    messages.expectedBefore("~")
+  )
+  tr.notOk(
+    "#foo+ .foo.bar ~ a {}",
+    "multiple combinators: no space before + combinator",
+    messages.expectedBefore("+")
+  )
+})
+
+testSelectorCombinatorSpace({ before: "never" }, tr => {
+  tr.ok("a {}", "no combinator")
+  tr.ok("a+ a {}", "no space before one after + combinator")
+  tr.ok("a> a {}", "no space before one after > combinator")
+  tr.ok("a~ a {}", "no space before one after ~ combinator")
+  tr.ok("a+a {}", "no space before or after + combinator")
+  tr.ok("a>a {}", "no space before or after > combinator")
+  tr.ok("a~a {}", "no space before or after ~ combinator")
+  tr.ok("a+\na {}", "no space before and newline after + combinator")
+  tr.ok("a>\na {}", "no space before and newline after > combinator")
+  tr.ok("a~\na {}", "no space before and newline after ~ combinator")
+  tr.ok(".foo~ a+ bar {}", "multiple combinators with no space before")
+
+  tr.notOk(
+    "a +a {}",
+    "space before + combinator",
+    messages.rejectedBefore("+")
+  )
+  tr.notOk(
+    "a >a {}",
+    "space before > combinator",
+    messages.rejectedBefore(">")
+  )
+  tr.notOk(
+    "a ~a {}",
+    "space before ~ combinator",
+    messages.rejectedBefore("~")
+  )
+  tr.notOk(
+    "a\n+a {}",
+    "newline before + combinator",
+    messages.rejectedBefore("+")
+  )
+  tr.notOk(
+    "a\n>a {}",
+    "newline before > combinator",
+    messages.rejectedBefore(">")
+  )
+  tr.notOk(
+    "a\n~a {}",
+    "newline before ~ combinator",
+    messages.rejectedBefore("~")
+  )
+  tr.notOk(
+    "a + .foo.bar~ a {}",
+    "multiple combinators: space before + combinator",
+    messages.rejectedBefore("+")
+  )
+  tr.notOk(
+    "#foo+ .foo.bar ~ a {}",
+    "multiple combinators: no space before ~ combinator",
+    messages.rejectedBefore("~")
+  )
+})
+
+testSelectorCombinatorSpace({ after: "always" }, tr => {
+  tr.ok("a {}", "no combinator")
+  tr.ok("a + a {}", "space before and after + combinator")
+  tr.ok("a > a {}", "space before and after > combinator")
+  tr.ok("a ~ a {}", "space before and after ~ combinator")
+  tr.ok(".foo ~ a + bar {}", "multiple spaced combinators")
+  tr.ok("a+ a {}", "no before and one after + combinator")
+  tr.ok("a> a {}", "no before and one after > combinator")
+  tr.ok("a~ a {}", "no before and one after ~ combinator")
+  tr.ok("a\n+ a {}", "newline before space after + combinator")
+  tr.ok("a\n> a {}", "newline before space after > combinator")
+  tr.ok("a\n~ a {}", "newline before space after ~ combinator")
+  tr.ok(".foo~ a+ bar {}", "multiple combinators with no space before and one after")
+
+  tr.notOk(
+    "a+  a {}",
+    "two spaces after + combinator",
+    messages.expectedAfter("+")
+  )
+  tr.notOk(
+    "a+\na {}",
+    "newline after + combinator",
+    messages.expectedAfter("+")
+  )
+  tr.notOk(
+    "a+a {}",
+    "no space after + combinator",
+    messages.expectedAfter("+")
+  )
+  tr.notOk(
+    "a>a {}",
+    "no space after > combinator",
+    messages.expectedAfter(">")
+  )
+  tr.notOk(
+    "a~a {}",
+    "no space after ~ combinator",
+    messages.expectedAfter("~")
+  )
+  tr.notOk(
+    "a + .foo.bar ~a {}",
+    "multiple combinators: no space after ~ combinator",
+    messages.expectedAfter("~")
+  )
+  tr.notOk(
+    "#foo +.foo.bar ~ a {}",
+    "multiple combinators: no space after + combinator",
+    messages.expectedAfter("+")
+  )
+})
+
+testSelectorCombinatorSpace({ after: "never" }, tr => {
+  tr.ok("a {}", "no combinator")
+  tr.ok("a +a {}", "space before none after + combinator")
+  tr.ok("a >a {}", "space before none after > combinator")
+  tr.ok("a ~a {}", "space before none after ~ combinator")
+  tr.ok(".foo ~a +bar {}", "multiple combinators with no space after")
+  tr.ok("a+a {}", "no space before or after + combinator")
+  tr.ok("a>a {}", "no space before or after > combinator")
+  tr.ok("a~a {}", "no space before or after ~ combinator")
+  tr.ok("a\n+a {}", "newline before and no space after + combinator")
+  tr.ok("a\n>a {}", "newline before and no space after > combinator")
+  tr.ok("a\n~a {}", "newline before and no space after ~ combinator")
+
+  tr.notOk(
+    "a+ a {}",
+    "space after + combinator",
+    messages.rejectedAfter("+")
+  )
+  tr.notOk(
+    "a> a {}",
+    "space after > combinator",
+    messages.rejectedAfter(">")
+  )
+  tr.notOk(
+    "a~ a {}",
+    "space after ~ combinator",
+    messages.rejectedAfter("~")
+  )
+  tr.notOk(
+    "a+\na{}",
+    "newline after + combinator",
+    messages.rejectedAfter("+")
+  )
+  tr.notOk(
+    "a>\na{}",
+    "newline after > combinator",
+    messages.rejectedAfter(">")
+  )
+  tr.notOk(
+    "a~\na{}",
+    "newline after ~ combinator",
+    messages.rejectedAfter("~")
+  )
+  tr.notOk(
+    "a + .foo.bar ~a {}",
+    "multiple combinators: space after + combinator",
+    messages.rejectedAfter("+")
+  )
+  tr.notOk(
+    "#foo +.foo.bar ~ a {}",
+    "multiple combinators: no space after ~ combinator",
+    messages.rejectedAfter("~")
+  )
+})
+
 testSelectorCombinatorSpace({ before: "always", after: "always" }, tr => {
   tr.ok("a {}", "no combinator")
   tr.ok("a + a {}", "space before and after + combinator")
