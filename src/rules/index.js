844af4e16f33291b2de1075253a72e7058047cf7
Richard Hallows <hello@richardhallows.com>
2015-05-13 13:30:31 +0100

    Correct rule name in index

--- src/rules/index.js
+++ src/rules/index.js
@@ -2,7 +2,7 @@ export default {
   "block-opening-brace-after": require("./block-opening-brace-after"),
   "block-opening-brace-before": require("./block-opening-brace-before"),
   "declaration-block-trailing-semicolon": require("./declaration-block-trailing-semicolon"),
-  "declaration-colon-before": require("./declaration-colon-before"),
+  "declaration-colon-space": require("./declaration-colon-space"),
   "declaration-no-important": require("./declaration-no-important"),
   "number-leading-zero": require("./number-leading-zero"),
   "rule-set-no-single-line": require("./rule-set-no-single-line"),
