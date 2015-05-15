72ada08c890a373504bbb420ad2e55f03e8fd018
David Clark <david.dave.clark@gmail.com>
2015-05-15 16:21:32 -0700

    Create and test block-closing-brace-space and refactor whitespaceChecker; addresses #21

--- src/utils/standardWhitespaceChecker.js
+++ src/utils/standardWhitespaceChecker.js
@@ -6,10 +6,10 @@ import isWhitespace from "./isWhitespace"
  *
  * @param {string} expectedWhitespace - The whitespace to look for
  * @param {object} whitespaceOptions - A set of options created by
- *   `standardWhitespaceOptions()`; so it will have properties
+ *   `standardWhitespaceOptions()` so it will have properties
  *   `expectBefore`, `expectAfter`, `rejectBefore`, etc.
  * @param {object} whitespaceMessages - A set of messages passed through
- *   `standardWhitespaceMessages()`; so it will have functions
+ *   `standardWhitespaceMessages()` so it will have functions
  *   `expectedBefore()`, `expectedAfter()`, `rejectedBefore()`, etc.
  * @return {objects} The standardWhitespaceChecker with fun functions to use
  */
@@ -22,39 +22,47 @@ export default function (expectedWhitespace, whitespaceOptions, whitespaceMessag
      * @param {string} source
      * @param {number} index - The index of the character to look before
      *   in `source`
-     * @param {function} cb - A callback that accepts a message
+     * @param {function} err - A callback that accepts a message
      */
-    before(source, index, cb) {
+    before(source, index, err) {
       if (whitespaceOptions.ignoreBefore) { return }
+
+      const oneCharBefore = source[index - 1]
+      const twoCharsBefore = source[index - 2]
 
       if (whitespaceOptions.expectBefore) {
         // Check for the space 1 character before and no additional
         // whitespace 2 characters before
-        if (source[index - 1] === expectedWhitespace
-          && !isWhitespace(source[index - 2])) { return }
-        cb(whitespaceMessages.expectedBefore(source[index]))
+        if ((isValue(oneCharBefore) && oneCharBefore !== expectedWhitespace)
+          || (isValue(twoCharsBefore) && isWhitespace(twoCharsBefore))) {
+          err(whitespaceMessages.expectedBefore(source[index]))
+        }
       }
 
       if (whitespaceOptions.rejectBefore) {
         // Check that there's no whitespace at all before
-        if (!isWhitespace(source[index - 1])) { return }
-        cb(whitespaceMessages.rejectedBefore(source[index]))
+        if (isValue(oneCharBefore) && isWhitespace(oneCharBefore)) {
+          err(whitespaceMessages.rejectedBefore(source[index]))
+        }
       }
     },
 
-    oneBefore(source, index, cb) {
+    oneBefore(source, index, err) {
       if (whitespaceOptions.ignoreBefore) { return }
 
+      const oneCharBefore = source[index - 1]
+
       if (whitespaceOptions.expectBefore) {
-        // Check only the character before
-        if (source[index - 1] === expectedWhitespace) { return }
-        cb(whitespaceMessages.expectedBefore(source[index]))
+        if (isValue(oneCharBefore) && oneCharBefore !== expectedWhitespace) {
+          err(whitespaceMessages.expectedBefore(source[index]))
+        }
       }
 
       if (whitespaceOptions.rejectBefore) {
         // Check that there's no whitespace at all before
-        if (!isWhitespace(source[index - 1])) { return }
-        cb(whitespaceMessages.rejectedBefore(source[index]))
+        if (isValue(oneCharBefore) && isWhitespace(oneCharBefore)) {
+          err(whitespaceMessages.rejectedBefore(source[index]))
+        }
       }
     },
 
@@ -64,40 +72,52 @@ export default function (expectedWhitespace, whitespaceOptions, whitespaceMessag
      * @param {string} source
      * @param {number} index - The index of the character to look after
      *   in `source`
-     * @param {function} cb - A callback that accepts a message
+     * @param {function} err - A callback that accepts a message
      */
-    after(source, index, cb) {
+    after(source, index, err) {
       if (whitespaceOptions.ignoreAfter) { return }
+
+      const oneCharAfter = source[index + 1]
+      const twoCharsAfter = source[index + 2]
 
       if (whitespaceOptions.expectAfter) {
         // Check for the space 1 character after and no additional
         // whitespace 2 characters after
-        if (source[index + 1] === expectedWhitespace
-          && !isWhitespace(source[index + 2])) { return }
-        cb(whitespaceMessages.expectedAfter(source[index]))
+        if ((isValue(oneCharAfter) && oneCharAfter !== expectedWhitespace)
+          || (isValue(twoCharsAfter) && isWhitespace(twoCharsAfter))) {
+          err(whitespaceMessages.expectedAfter(source[index]))
+        }
       }
 
       if (whitespaceOptions.rejectAfter) {
         // Check that there's no whitespace at all ater
-        if (!isWhitespace(source[index + 1])) { return }
-        cb(whitespaceMessages.rejectedAfter(source[index]))
+        if (isValue(oneCharAfter) && isWhitespace(oneCharAfter)) {
+          err(whitespaceMessages.rejectedAfter(source[index]))
+        }
       }
     },
 
-    oneAfter(source, index, cb) {
+    oneAfter(source, index, err) {
       if (whitespaceOptions.ignoreAfter) { return }
 
+      const oneCharAfter = source[index + 1]
+
       if (whitespaceOptions.expectAfter) {
-        // Check only the character after
-        if (source[index + 1] === expectedWhitespace) { return }
-        cb(whitespaceMessages.expectedAfter(source[index]))
+        if (isValue(oneCharAfter) && oneCharAfter !== expectedWhitespace) {
+          err(whitespaceMessages.expectedAfter(source[index]))
+        }
       }
 
       if (whitespaceOptions.rejectAfter) {
         // Check that there's no whitespace at all ater
-        if (!isWhitespace(source[index + 1])) { return }
-        cb(whitespaceMessages.rejectedAfter(source[index]))
+        if (isValue(oneCharAfter) && isWhitespace(oneCharAfter)) {
+          err(whitespaceMessages.rejectedAfter(source[index]))
+        }
       }
     },
   }
+}
+
+function isValue(x) {
+  return x !== undefined && x !== null
 }
