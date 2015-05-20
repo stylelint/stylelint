2161e1f18ab703db4be2ad89bbbe705ec0c233ce
David Clark <david.dave.clark@gmail.com>
2015-05-19 22:00:36 -0700

    Create and test function-token-no-space; closes #83

---
+++ src/rules/function-token-no-space/__tests__/index.js
@@ -0,0 +1,19 @@
+import { ruleTester } from "../../../testUtils"
+import rule, { ruleName, messages } from ".."
+
+const testRule = ruleTester(rule, ruleName)
+
+testRule(null, tr => {
+  tr.ok("a {}")
+  tr.ok("a::before { content: \"var (--a) var\n(--a)\"; }")
+  tr.ok("a { transform: translate(1, 1); }")
+  tr.ok("a { color: color(rgb(0, 0, 0) lightness(50%)) }")
+
+  tr.notOk("a { transform: translate (1, 1); }", messages.rejected)
+  tr.notOk("a { transform: translate  (1, 1); }", messages.rejected)
+  tr.notOk("a { transform: translate\n(1, 1); }", messages.rejected)
+  tr.notOk("a { transform: translate\t(1, 1); }", messages.rejected)
+  tr.notOk("a { color: color (rgb(0, 0, 0) lightness(50%)); }", messages.rejected)
+  tr.notOk("a { color: color(rgb (0, 0, 0) lightness(50%)); }", messages.rejected)
+  tr.notOk("a { color: color(rgb(0, 0, 0) lightness (50%)); }", messages.rejected)
+})
