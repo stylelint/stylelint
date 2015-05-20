b2271af56db9c9e8dcc041e5976730b26113fbe8
David Clark <david.dave.clark@gmail.com>
2015-05-19 19:20:22 -0700

    Add always-multi-line to block-closing-brace-space-before

--- src/rules/block-closing-brace-space-before/__tests__/index.js
+++ src/rules/block-closing-brace-space-before/__tests__/index.js
@@ -26,3 +26,70 @@ testRule("never", tr => {
   tr.notOk("a { color: pink;\t}", messages.rejectedBefore())
   tr.notOk("a { color: pink;} b { color: red; }", messages.rejectedBefore())
 })
+
+testRule("always-single-line", tr => {
+  tr.ok("a { color: pink; }")
+  tr.ok("a { color: pink; } b { color: red; }")
+  tr.ok("a { color: pink; }b { color: red; }")
+
+  // Ignore multi-line
+  tr.ok("a { color: pink;\ntop: 0;}")
+  tr.ok("a { color: pink;\ntop: 0;  } b { color: red; }")
+  tr.ok("a { color: pink;\ntop: 0;\n}b { color: red; }")
+
+  tr.notOk("a { color: pink;}", messages.expectedBeforeSingleLine())
+  tr.notOk("a { color: pink;  }", messages.expectedBeforeSingleLine())
+  tr.notOk("a { color: pink;\t}", messages.expectedBeforeSingleLine())
+  tr.notOk("a { color: pink; } b { color: red;}", messages.expectedBeforeSingleLine())
+})
+
+testRule("never-single-line", tr => {
+  tr.ok("a { color: pink;}")
+  tr.ok("a { color: pink;} b { color: red;}")
+  tr.ok("a { color: pink;}b { color: red;}")
+
+  // Ignore multi-line
+  tr.ok("a { color: pink;\ntop: 0; }")
+  tr.ok("a { color: pink;\ntop: 0;  } b { color: red;}")
+  tr.ok("a { color: pink;\ntop: 0;\n}b { color: red;}")
+
+  tr.notOk("a { color: pink; }", messages.rejectedBeforeSingleLine())
+  tr.notOk("a { color: pink;  }", messages.rejectedBeforeSingleLine())
+  tr.notOk("a { color: pink;\t}", messages.rejectedBeforeSingleLine())
+  tr.notOk("a { color: pink;} b { color: red;\t}", messages.rejectedBeforeSingleLine())
+  tr.notOk("a { color: pink;  } b { color: red;}", messages.rejectedBeforeSingleLine())
+})
+
+testRule("always-multi-line", tr => {
+  tr.ok("a { color: pink;\ntop: 0; }")
+  tr.ok("a { color: pink;\ntop: 0; } b { color: red; }")
+  tr.ok("a { color: pink;\ntop: 0; }b { color: red; }")
+
+  // Ignore single-line
+  tr.ok("a { color: pink;}")
+  tr.ok("a { color: pink;  } b { color: red; }")
+  tr.ok("a { color: pink;\t}b { color: red; }")
+
+  tr.notOk("a { color: pink;\ntop: 0;}", messages.expectedBeforeMultiLine())
+  tr.notOk("a { color: pink;\ntop: 0;  }", messages.expectedBeforeMultiLine())
+  tr.notOk("a { color: pink;\ntop: 0;\t}", messages.expectedBeforeMultiLine())
+  tr.notOk("a { color: pink; } b { color: red;\ntop: 0;}", messages.expectedBeforeMultiLine())
+  tr.notOk("a { color: pink;\ntop: 0;} b { color: red; }", messages.expectedBeforeMultiLine())
+})
+
+testRule("never-multi-line", tr => {
+  tr.ok("a { color: pink;\ntop: 0;}")
+  tr.ok("a { color: pink;\ntop: 0;} b { color: red;}")
+  tr.ok("a { color: pink;\ntop: 0;}b { color: red;}")
+
+  // Ignore single-line
+  tr.ok("a { color: pink; }")
+  tr.ok("a { color: pink;  } b { color: red; }")
+  tr.ok("a { color: pink;\t}b { color: red; }")
+
+  tr.notOk("a { color: pink;\ntop: 0; }", messages.rejectedBeforeMultiLine())
+  tr.notOk("a { color: pink;\ntop: 0;  }", messages.rejectedBeforeMultiLine())
+  tr.notOk("a { color: pink;\ntop: 0;\t}", messages.rejectedBeforeMultiLine())
+  tr.notOk("a { color: pink; } b { color: red;\ntop: 0; }", messages.rejectedBeforeMultiLine())
+  tr.notOk("a { color: pink;\ntop: 0; } b { color: red; }", messages.rejectedBeforeMultiLine())
+})
--- src/rules/block-closing-brace-space-before/index.js
+++ src/rules/block-closing-brace-space-before/index.js
@@ -8,10 +8,14 @@ export const ruleName = "block-closing-brace-space-before"
 export const messages = ruleMessages(ruleName, {
   expectedBefore: () => "Expected single space before \"}\"",
   rejectedBefore: () => "Unexpected space before \"}\"",
+  expectedBeforeSingleLine: () => "Expected single space before \"}\" of a single-line block",
+  rejectedBeforeSingleLine: () => "Unexpected space before \"}\" of a single-line block",
+  expectedBeforeMultiLine: () => "Expected single space before \"}\" of a multi-line block",
+  rejectedBeforeMultiLine: () => "Unexpected space before \"}\" of a multi-line block",
 })
 
 /**
- * @param {"always"|"never"} expectation
+ * @param {"always"|"never"|"always-single-line"|"never-single-line"|"always-multi-line"|"never-multi-line"} expectation
  */
 export default function (expectation) {
   const checker = whitespaceChecker(" ", expectation, messages)
@@ -22,9 +26,10 @@ export default function (expectation) {
 
     function checkBlock(block) {
       const blockString = block.toString()
+      const blockStringNoSelector = blockString.slice(blockString.indexOf("{"))
       checker.before(blockString, blockString.length - 1, msg => {
         result.warn(msg, { node: block })
-      })
+      }, blockStringNoSelector)
     }
   }
 }
