df04d7ddb26ae4b8483eebc7613bc731de3f9bd1
David Clark <david.dave.clark@gmail.com>
2015-05-19 21:49:22 -0700

    Create and test function-space-after; closes #81

---
+++ src/rules/function-space-after/__tests__/index.js
@@ -0,0 +1,38 @@
+import { ruleTester } from "../../../testUtils"
+import rule, { ruleName, messages } from ".."
+
+const testRule = ruleTester(rule, ruleName)
+
+testRule("always", tr => {
+  tr.ok("a {}")
+  tr.ok("a::before { content: \"(a) ( a )\"; }")
+  tr.ok("a { transform: translate(1, 1); }")
+  tr.ok("a { transform: translate(1, 1) }")
+  tr.ok("a { transform: translate(1, 1)}")
+  tr.ok("a { transform: translate(1, 1) scale(3); }")
+  tr.ok("a { color: color(rgb(0,0,0) lightness(50%)) };")
+
+  tr.notOk("a { transform: translate(1, 1)scale(3); }", messages.expected)
+  tr.notOk("a { transform: translate(1, 1)  scale(3); }", messages.expected)
+  tr.notOk("a { transform: translate(1, 1)\nscale(3); }", messages.expected)
+  tr.notOk("a { color: color(rgb(0,0,0)lightness(50%)) };", messages.expected)
+  tr.notOk("a { color: color(rgb(0,0,0)  lightness(50%)) };", messages.expected)
+  tr.notOk("a { color: color(rgb(0,0,0)\nlightness(50%)) };", messages.expected)
+})
+
+testRule("never", tr => {
+  tr.ok("a {}")
+  tr.ok("a::before { content: \"(a) ( a )\"; }")
+  tr.ok("a { transform: translate(1, 1); }")
+  tr.ok("a { transform: translate(1, 1) }")
+  tr.ok("a { transform: translate(1, 1)}")
+  tr.ok("a { transform: translate(1, 1)scale(3); }")
+  tr.ok("a { color: color(rgb(0,0,0)lightness(50%)) };")
+
+  tr.notOk("a { transform: translate(1, 1) scale(3); }", messages.rejected)
+  tr.notOk("a { transform: translate(1, 1)  scale(3); }", messages.rejected)
+  tr.notOk("a { transform: translate(1, 1)\nscale(3); }", messages.rejected)
+  tr.notOk("a { color: color(rgb(0,0,0) lightness(50%)) };", messages.rejected)
+  tr.notOk("a { color: color(rgb(0,0,0)  lightness(50%)) };", messages.rejected)
+  tr.notOk("a { color: color(rgb(0,0,0)\nlightness(50%)) };", messages.rejected)
+})
