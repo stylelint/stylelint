95a4dd299aea403a3fc4d789a71fd64da99945fc
David Clark <david.dave.clark@gmail.com>
2015-05-24 08:13:14 -0700

    Create and test selector-delimiter-newline-before

---
+++ src/rules/selector-delimiter-newline-before/__tests__/index.js
@@ -0,0 +1,35 @@
+import { ruleTester } from "../../../testUtils"
+import rule, { ruleName, messages } from ".."
+
+const testRule = ruleTester(rule, ruleName)
+
+testRule("always", tr => {
+  tr.ok("a {}")
+  tr.ok("a\n,b {}")
+  tr.ok("a\n,b\n,c {}")
+  tr.ok("a\n, b {}")
+  tr.ok("a\n,\nb {}")
+  tr.ok("a\n,b[data-foo=\"tr,tr\"] {}")
+
+  tr.notOk("a,b {}", messages.expectedBefore())
+  tr.notOk("a ,b {}", messages.expectedBefore())
+  tr.notOk("a  ,b {}", messages.expectedBefore())
+  tr.notOk("a\t,b {}", messages.expectedBefore())
+  tr.notOk("a\n,b,c {}", messages.expectedBefore())
+  tr.notOk("a\n,b\n ,c {}", messages.expectedBefore())
+})
+
+testRule("never", tr => {
+  tr.ok("a {}")
+  tr.ok("a,b {}")
+  tr.ok("a,b,c {}")
+  tr.ok("a, b {}")
+  tr.ok("a,\nb {}")
+  tr.ok("a,b[data-foo=\"tr ,tr\"] {}")
+
+  tr.notOk("a ,b {}", messages.rejectedBefore())
+  tr.notOk("a  ,b {}", messages.rejectedBefore())
+  tr.notOk("a\t,b {}", messages.rejectedBefore())
+  tr.notOk("a,b ,c {}", messages.rejectedBefore())
+  tr.notOk("a,b\n ,c {}", messages.rejectedBefore())
+})
---
+++ src/rules/selector-delimiter-newline-before/index.js
@@ -0,0 +1,19 @@
+import {
+  ruleMessages,
+  whitespaceChecker
+} from "../../utils"
+import { selectorDelimiterSpaceChecker } from "../selector-delimiter-space-after"
+
+export const ruleName = "selector-delimiter-newline-before"
+
+export const messages = ruleMessages(ruleName, {
+  expectedBefore: () => "Expected newline before selector delimiter",
+  rejectedBefore: () => "Unexpected space before selector delimiter",
+})
+/**
+ * @param {"always"|"never"} expectation
+ */
+export default function (expectation) {
+  const checker = whitespaceChecker("\n", expectation, messages)
+  return selectorDelimiterSpaceChecker(checker.before)
+}
