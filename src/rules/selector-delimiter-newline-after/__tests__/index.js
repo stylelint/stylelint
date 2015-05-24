58d29b5848ab9e1fd1e06b825f108ed8e22a62fb
David Clark <david.dave.clark@gmail.com>
2015-05-24 08:10:48 -0700

    Create and test selector-delimiter-newline-after

---
+++ src/rules/selector-delimiter-newline-after/__tests__/index.js
@@ -0,0 +1,36 @@
+import { ruleTester } from "../../../testUtils"
+import rule, { ruleName, messages } from ".."
+
+const testRule = ruleTester(rule, ruleName)
+
+testRule("always", tr => {
+  tr.ok("a {}")
+  tr.ok("a,\nb {}")
+  tr.ok("a,\nb,\nc {}")
+  tr.ok("a ,\nb {}")
+  tr.ok("a\n,\nb {}")
+  tr.ok("a,\nb[data-foo=\"tr,tr\"] {}")
+
+  tr.notOk("a,b {}", messages.expectedAfter())
+  tr.notOk("a, b {}", messages.expectedAfter())
+  tr.notOk("a,  b {}", messages.expectedAfter())
+  tr.notOk("a,\tb {}", messages.expectedAfter())
+  tr.notOk("a,\nb,c {}", messages.expectedAfter())
+  tr.notOk("a,\nb,\n c {}", messages.expectedAfter())
+})
+
+testRule("never", tr => {
+  tr.ok("a {}")
+  tr.ok("a,b {}")
+  tr.ok("a,b,c {}")
+  tr.ok("a ,b {}")
+  tr.ok("a\n,b {}")
+  tr.ok("a,b[data-foo=\"tr, tr\"] {}")
+
+  tr.notOk("a, b {}", messages.rejectedAfter())
+  tr.notOk("a,  b {}", messages.rejectedAfter())
+  tr.notOk("a,\nb {}", messages.rejectedAfter())
+  tr.notOk("a,\tb {}", messages.rejectedAfter())
+  tr.notOk("a,b, c {}", messages.rejectedAfter())
+  tr.notOk("a,b,\n c {}", messages.rejectedAfter())
+})
