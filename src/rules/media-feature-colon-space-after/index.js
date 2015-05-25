8f76603c2fcbef6f11d8546dd5539a35d8c0fa18
David Clark <david.dave.clark@gmail.com>
2015-05-25 08:42:00 -0700

    Create and test media-feature-colon-space-after

---
+++ src/rules/media-feature-colon-space-after/index.js
@@ -0,0 +1,35 @@
+import {
+  ruleMessages,
+  whitespaceChecker,
+  valueIndexOf
+} from "../../utils"
+
+export const ruleName = "media-feature-colon-space-after"
+
+export const messages = ruleMessages(ruleName, {
+  expectedAfter: () => `Expected single space after ":" in media feature`,
+  rejectedAfter: () => `Unexpected space after ":" in media feature`,
+})
+
+/**
+ * @param {"always"|"never"} expectation
+ */
+export default function (expectation) {
+  const checker = whitespaceChecker(" ", expectation, messages)
+  return mediaFeatureColonSpaceChecker(checker.after)
+}
+
+export function mediaFeatureColonSpaceChecker(checkLocation) {
+  return function (css, result) {
+    css.eachAtRule(function (atRule) {
+      const params = atRule.params
+      valueIndexOf({ value: params, char: ":" }, index => {
+        checkColon(params, index, atRule)
+      })
+    })
+
+    function checkColon(source, index, node) {
+      checkLocation(source, index, m => result.warn(m, { node }))
+    }
+  }
+}
