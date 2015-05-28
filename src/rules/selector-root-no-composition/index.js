cb58726da77b1ab06e1f1c45d621c498c755cf7f
David Clark <david.dave.clark@gmail.com>
2015-05-27 21:24:23 -0700

    Create and test selector-root-no-composition

---
+++ src/rules/selector-root-no-composition/index.js
@@ -0,0 +1,19 @@
+import { ruleMessages } from "../../utils"
+
+export const ruleName = "selector-root-no-composition"
+
+export const messages = ruleMessages(ruleName, {
+  rejected: "Unexpected composition of the \":root\" selector",
+})
+
+export default function () {
+  return function (css, result) {
+    css.eachRule(function (rule) {
+      if (rule.selector.indexOf(":root") === -1) { return }
+
+      if (rule.selector.trim() !== ":root") {
+        result.warn(messages.rejected, { node: rule })
+      }
+    })
+  }
+}
