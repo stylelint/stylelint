b86bc91d10deec1663acaaf1e80e4dddc2fa27be
David Clark <david.dave.clark@gmail.com>
2015-06-02 21:27:31 -0700

    Create and test comment-empty-line-before; closes #156

---
+++ src/rules/comment-empty-line-before/__tests__/index.js
@@ -0,0 +1,37 @@
+import { ruleTester } from "../../../testUtils"
+import rule, { ruleName, messages } from ".."
+
+const testRule = ruleTester(rule, ruleName)
+
+testRule("always", tr => {
+  tr.ok("")
+  tr.ok("/** comment */", "first node ignored")
+  tr.ok("a {}\n\n/** comment */")
+  tr.ok("a { color: pink;\n\n/** comment */\ntop: 0; }")
+
+  tr.notOk("/** comment */\n/** comment */", messages.expected)
+  tr.notOk("a {} /** comment */", messages.expected)
+  tr.notOk("a { color: pink;\n/** comment */\ntop: 0; }", messages.expected)
+})
+
+testRule("always-except-inline", tr => {
+  tr.ok("")
+  tr.ok("/** comment */", "first node ignored")
+  tr.ok("a {}\n\n/** comment */")
+  tr.ok("a { color: pink; /** comment */\ntop: 0; }")
+
+  tr.notOk("/** comment */\n/** comment */", messages.expected)
+  tr.notOk("a {}\n/** comment */", messages.expected)
+  tr.notOk("a { color: pink;\n/** comment */\ntop: 0; }", messages.expected)
+})
+
+testRule("never", tr => {
+  tr.ok("")
+  tr.ok("\n\n/** comment */", "first node ignored")
+  tr.ok("a {} /** comment */")
+  tr.ok("a { color: pink;\n/** comment */\n\ntop: 0; }")
+
+  tr.notOk("/** comment */\n\n/** comment */", messages.rejected)
+  tr.notOk("a {}\n\n\n/** comment */", messages.rejected)
+  tr.notOk("a { color: pink;\n\n/** comment */\ntop: 0; }", messages.rejected)
+})
