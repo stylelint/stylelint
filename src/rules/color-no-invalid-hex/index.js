4894d2eb8c12380bc2b4f8f628006107d24e917e
Richard Hallows <hello@richardhallows.com>
2015-06-04 18:35:49 +0100

    Remove console.log()

--- src/rules/color-no-invalid-hex/index.js
+++ src/rules/color-no-invalid-hex/index.js
@@ -15,7 +15,7 @@ export default function () {
       const value = decl.value
 
       styleSearch({ source: value, target: "#" }, match => {
-        console.log(match)
+
         const hexValue = /^#[0-9A-Za-z]+/.exec(value.substr(match.startIndex))[0]
 
         if (!/^#(?:[0-9a-fA-F]{3,4}|[0-9a-fA-F]{6}|[0-9a-fA-F]{8})$/.test(hexValue)) {
