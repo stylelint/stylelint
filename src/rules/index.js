2eb6f0e2129e13a40b8c011f30129a8dd79ee31a
Richard Hallows <hello@richardhallows.com>
2015-06-04 16:48:00 +0100

    ES6-style rule imports. Closes #154

--- src/rules/index.js
+++ src/rules/index.js
@@ -1,57 +1,121 @@
+import atRuleEmptyLineBefore from "./at-rule-empty-line-before"
+import atRuleNoVendorPrefix from "./at-rule-no-vendor-prefix"
+import blockClosingBraceNewlineAfter from "./block-closing-brace-newline-after"
+import blockClosingBraceNewlineBefore from "./block-closing-brace-newline-before"
+import blockClosingBraceSpaceAfter from "./block-closing-brace-space-after"
+import blockClosingBraceSpaceBefore from "./block-closing-brace-space-before"
+import blockNoEmpty from "./block-no-empty"
+import blockOpeningBraceNewlineAfter from "./block-opening-brace-newline-after"
+import blockOpeningBraceNewlineBefore from "./block-opening-brace-newline-before"
+import blockOpeningBraceSpaceAfter from "./block-opening-brace-space-after"
+import blockOpeningBraceSpaceBefore from "./block-opening-brace-space-before"
+import colorNoInvalidHex from "./color-no-invalid-hex"
+import commentEmptyLineBefore from "./comment-empty-line-before"
+import declarationBangSpaceAfter from "./declaration-bang-space-after"
+import declarationBangSpaceBefore from "./declaration-bang-space-before"
+import declarationColonSpaceAfter from "./declaration-colon-space-after"
+import declarationColonSpaceBefore from "./declaration-colon-space-before"
+import declarationCommaSpaceAfter from "./declaration-comma-space-after"
+import declarationCommaSpaceBefore from "./declaration-comma-space-before"
+import declarationNoImportant from "./declaration-no-important"
+import declarationSemicolonNewlineAfter from "./declaration-semicolon-newline-after"
+import declarationSemicolonNewlineBefore from "./declaration-semicolon-newline-before"
+import declarationSemicolonSpaceAfter from "./declaration-semicolon-space-after"
+import declarationSemicolonSpaceBefore from "./declaration-semicolon-space-before"
+import functionCalcNoUnspacedOperator from "./function-calc-no-unspaced-operator"
+import functionCommaSpaceAfter from "./function-comma-space-after"
+import functionCommaSpaceBefore from "./function-comma-space-before"
+import functionParenthesesInsideSpace from "./function-parentheses-inside-space"
+import functionSpaceAfter from "./function-space-after"
+import functionTokenNoSpace from "./function-token-no-space"
+import functionUrlQuotes from "./function-url-quotes"
+import indentation from "./indentation"
+import mediaFeatureColonSpaceAfter from "./media-feature-colon-space-after"
+import mediaFeatureColonSpaceBefore from "./media-feature-colon-space-before"
+import mediaFeatureRangeOperatorSpaceAfter from "./media-feature-range-operator-space-after"
+import mediaFeatureRangeOperatorSpaceBefore from "./media-feature-range-operator-space-before"
+import mediaQueryParenthesesInsideSpace from "./media-query-parentheses-inside-space"
+import noEolWhitespace from "./no-eol-whitespace"
+import noMissingEofNewline from "./no-missing-eof-newline"
+import numberLeadingZero from "./number-leading-zero"
+import numberNoTrailingZeros from "./number-no-trailing-zeros"
+import propertyNoVendorPrefix from "./property-no-vendor-prefix"
+import propertyValueNoVendorPrefix from "./property-value-no-vendor-prefix"
+import rootNoStandardProperties from "./root-no-standard-properties"
+import ruleNestedEmptyLineBefore from "./rule-nested-empty-line-before"
+import ruleNoDuplicateProperties from "./rule-no-duplicate-properties"
+import ruleNoSingleLine from "./rule-no-single-line"
+import ruleNonNestedEmptyLineBefore from "./rule-non-nested-empty-line-before"
+import ruleTrailingSemicolon from "./rule-trailing-semicolon"
+import selectorCombinatorSpaceAfter from "./selector-combinator-space-after"
+import selectorCombinatorSpaceBefore from "./selector-combinator-space-before"
+import selectorDelimiterNewlineAfter from "./selector-delimiter-newline-after"
+import selectorDelimiterNewlineBefore from "./selector-delimiter-newline-before"
+import selectorDelimiterSpaceAfter from "./selector-delimiter-space-after"
+import selectorDelimiterSpaceBefore from "./selector-delimiter-space-before"
+import selectorNoVendorPrefix from "./selector-no-vendor-prefix"
+import selectorPseudoElementColonNotation from "./selector-pseudo-element-colon-notation"
+import selectorRootNoComposition from "./selector-root-no-composition"
+import stringQuotes from "./string-quotes"
+
 export default {
-  "at-rule-no-vendor-prefix": require("./at-rule-no-vendor-prefix").default,
-  "block-closing-brace-newline-after": require("./block-closing-brace-newline-after").default,
-  "block-closing-brace-newline-before": require("./block-closing-brace-newline-before").default,
-  "block-closing-brace-space-after": require("./block-closing-brace-space-after").default,
-  "block-closing-brace-space-before": require("./block-closing-brace-space-before").default,
-  "block-no-empty": require("./block-no-empty").default,
-  "block-opening-brace-newline-after": require("./block-opening-brace-newline-after").default,
-  "block-opening-brace-newline-before": require("./block-opening-brace-newline-before").default,
-  "block-opening-brace-space-after": require("./block-opening-brace-space-after").default,
-  "block-opening-brace-space-before": require("./block-opening-brace-space-before").default,
-  "color-no-invalid-hex": require("./color-no-invalid-hex").default,
-  "declaration-bang-space-after": require("./declaration-bang-space-after").default,
-  "declaration-bang-space-before": require("./declaration-bang-space-before").default,
-  "declaration-colon-space-after": require("./declaration-colon-space-after").default,
-  "declaration-colon-space-before": require("./declaration-colon-space-before").default,
-  "declaration-comma-space-after": require("./declaration-comma-space-after").default,
-  "declaration-comma-space-before": require("./declaration-comma-space-before").default,
-  "declaration-no-important": require("./declaration-no-important").default,
-  "declaration-semicolon-newline-after": require("./declaration-semicolon-newline-after").default,
-  "declaration-semicolon-newline-before": require("./declaration-semicolon-newline-before").default,
-  "declaration-semicolon-space-after": require("./declaration-semicolon-space-after").default,
-  "declaration-semicolon-space-before": require("./declaration-semicolon-space-before").default,
-  "function-calc-no-unspaced-operator": require("./function-calc-no-unspaced-operator").default,
-  "function-comma-space-after": require("./function-comma-space-after").default,
-  "function-comma-space-before": require("./function-comma-space-before").default,
-  "function-parentheses-inside-space": require("./function-parentheses-inside-space").default,
-  "function-space-after": require("./function-space-after").default,
-  "function-token-no-space": require("./function-token-no-space").default,
-  "function-url-quotes": require("./function-url-quotes").default,
-  "indentation": require("./indentation").default,
-  "media-feature-colon-space-after": require("./media-feature-colon-space-after").default,
-  "media-feature-colon-space-before": require("./media-feature-colon-space-before").default,
-  "media-feature-range-operator-space-after": require("./media-feature-range-operator-space-after").default,
-  "media-feature-range-operator-space-before": require("./media-feature-range-operator-space-before").default,
-  "media-query-parentheses-inside-space": require("./media-query-parentheses-inside-space").default,
-  "no-eol-whitespace": require("./no-eol-whitespace").default,
-  "no-missing-eof-newline": require("./no-missing-eof-newline").default,
-  "number-leading-zero": require("./number-leading-zero").default,
-  "number-no-trailing-zeros": require("./number-no-trailing-zeros").default,
-  "property-no-vendor-prefix": require("./property-no-vendor-prefix").default,
-  "property-value-no-vendor-prefix": require("./property-value-no-vendor-prefix").default,
-  "root-no-standard-properties": require("./root-no-standard-properties").default,
-  "rule-no-duplicate-properties": require("./rule-no-duplicate-properties").default,
-  "rule-no-single-line": require("./rule-set-no-single-line").default,
-  "rule-non-nested-empty-line-before": require("./rule-set-non-nested-empty-line-before").default,
-  "rule-trailing-semicolon": require("./rule-trailing-semicolon").default,
-  "selector-combinator-space-after": require("./selector-combinator-space-after").default,
-  "selector-combinator-space-before": require("./selector-combinator-space-before").default,
-  "selector-delimiter-newline-after": require("./selector-delimiter-newline-after").default,
-  "selector-delimiter-newline-before": require("./selector-delimiter-newline-before").default,
-  "selector-delimiter-space-after": require("./selector-delimiter-space-after").default,
-  "selector-delimiter-space-before": require("./selector-delimiter-space-before").default,
-  "selector-no-vendor-prefix": require("./selector-no-vendor-prefix").default,
-  "selector-root-no-composition": require("./selector-root-no-composition").default,
-  "string-quotes": require("./string-quotes").default,
+  "at-rule-empty-line-before": atRuleEmptyLineBefore,
+  "at-rule-no-vendor-prefix": atRuleNoVendorPrefix,
+  "block-closing-brace-newline-after": blockClosingBraceNewlineAfter,
+  "block-closing-brace-newline-before": blockClosingBraceNewlineBefore,
+  "block-closing-brace-space-after": blockClosingBraceSpaceAfter,
+  "block-closing-brace-space-before": blockClosingBraceSpaceBefore,
+  "block-no-empty": blockNoEmpty,
+  "block-opening-brace-newline-after": blockOpeningBraceNewlineAfter,
+  "block-opening-brace-newline-before": blockOpeningBraceNewlineBefore,
+  "block-opening-brace-space-after": blockOpeningBraceSpaceAfter,
+  "block-opening-brace-space-before": blockOpeningBraceSpaceBefore,
+  "color-no-invalid-hex": colorNoInvalidHex,
+  "comment-empty-line-before": commentEmptyLineBefore,
+  "declaration-bang-space-after": declarationBangSpaceAfter,
+  "declaration-bang-space-before": declarationBangSpaceBefore,
+  "declaration-colon-space-after": declarationColonSpaceAfter,
+  "declaration-colon-space-before": declarationColonSpaceBefore,
+  "declaration-comma-space-after": declarationCommaSpaceAfter,
+  "declaration-comma-space-before": declarationCommaSpaceBefore,
+  "declaration-no-important": declarationNoImportant,
+  "declaration-semicolon-newline-after": declarationSemicolonNewlineAfter,
+  "declaration-semicolon-newline-before": declarationSemicolonNewlineBefore,
+  "declaration-semicolon-space-after": declarationSemicolonSpaceAfter,
+  "declaration-semicolon-space-before": declarationSemicolonSpaceBefore,
+  "function-calc-no-unspaced-operator": functionCalcNoUnspacedOperator,
+  "function-comma-space-after": functionCommaSpaceAfter,
+  "function-comma-space-before": functionCommaSpaceBefore,
+  "function-parentheses-inside-space": functionParenthesesInsideSpace,
+  "function-space-after": functionSpaceAfter,
+  "function-token-no-space": functionTokenNoSpace,
+  "function-url-quotes": functionUrlQuotes,
+  "indentation": indentation,
+  "media-feature-colon-space-after": mediaFeatureColonSpaceAfter,
+  "media-feature-colon-space-before": mediaFeatureColonSpaceBefore,
+  "media-feature-range-operator-space-after": mediaFeatureRangeOperatorSpaceAfter,
+  "media-feature-range-operator-space-before": mediaFeatureRangeOperatorSpaceBefore,
+  "media-query-parentheses-inside-space": mediaQueryParenthesesInsideSpace,
+  "no-eol-whitespace": noEolWhitespace,
+  "no-missing-eof-newline": noMissingEofNewline,
+  "number-leading-zero": numberLeadingZero,
+  "number-no-trailing-zeros": numberNoTrailingZeros,
+  "property-no-vendor-prefix": propertyNoVendorPrefix,
+  "property-value-no-vendor-prefix": propertyValueNoVendorPrefix,
+  "root-no-standard-properties": rootNoStandardProperties,
+  "rule-nested-empty-line-before": ruleNestedEmptyLineBefore,
+  "rule-no-duplicate-properties": ruleNoDuplicateProperties,
+  "rule-no-single-line": ruleNoSingleLine,
+  "rule-non-nested-empty-line-before": ruleNonNestedEmptyLineBefore,
+  "rule-trailing-semicolon": ruleTrailingSemicolon,
+  "selector-combinator-space-after": selectorCombinatorSpaceAfter,
+  "selector-combinator-space-before": selectorCombinatorSpaceBefore,
+  "selector-delimiter-newline-after": selectorDelimiterNewlineAfter,
+  "selector-delimiter-newline-before": selectorDelimiterNewlineBefore,
+  "selector-delimiter-space-after": selectorDelimiterSpaceAfter,
+  "selector-delimiter-space-before": selectorDelimiterSpaceBefore,
+  "selector-no-vendor-prefix": selectorNoVendorPrefix,
+  "selector-pseudo-element-colon-notation": selectorPseudoElementColonNotation,
+  "selector-root-no-composition": selectorRootNoComposition,
+  "string-quotes": stringQuotes,
 }
