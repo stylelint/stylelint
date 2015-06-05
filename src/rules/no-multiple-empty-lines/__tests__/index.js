99fb8aa5158ce7f6a9e6d700f9ccfd472768a0ab
David Clark <david.dave.clark@gmail.com>
2015-06-04 20:47:54 -0700

    Create and test no-multiple-empty-line

---
+++ src/rules/no-multiple-empty-lines/__tests__/index.js
@@ -0,0 +1,16 @@
+import { ruleTester } from "../../../testUtils"
+import rule, { ruleName, messages } from ".."
+
+const testRule = ruleTester(rule, ruleName)
+
+testRule(null, tr => {
+  tr.ok("")
+  tr.ok("a {}")
+  tr.ok("a {}\nb {}")
+  tr.ok("a {}\n\nb{}")
+  tr.ok("/** horse */\n\nb{}")
+  tr.ok("a{}\n\n/** horse */\n\nb{}")
+
+  tr.notOk("a {}\n\n\nb{}", messages.rejected(3))
+  tr.notOk("a {}\n\n/** horse */\n\n\nb{}", messages.rejected(5))
+})
