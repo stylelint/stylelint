d83deba290244253bfc9b8fe8393951141754129
David Clark <david.dave.clark@gmail.com>
2015-06-07 09:31:28 -0700

    Create and test number-no-unitized-zero; closes #95

---
+++ src/rules/number-no-unitized-zero/__tests__/index.js
@@ -0,0 +1,27 @@
+import { ruleTester } from "../../../testUtils"
+import rule, { ruleName, messages } from ".."
+
+const testRule = ruleTester(rule, ruleName)
+
+testRule(null, tr => {
+  tr.ok("a { top: 0; }", "unitless zero")
+  tr.ok("a { top: 10px; }", "zero at end of non-zero value")
+  tr.ok("a { top: 100.00px; }", "zero at end of non-zero value after decimal")
+  tr.ok("a { top: 100.010px; }")
+  tr.ok("a { padding: 1px 0 2px 3px; }", "unitless zero in list")
+  tr.ok("a { padding: 1px 1px 2px 0; }", "unitless zero in list")
+  tr.ok("a { color: pink; }", "no zero")
+  tr.ok("a { color: #0ac0ac; }", "hex color value")
+  tr.ok("a::before { content: \"0px 0em 0cm\"; }", "zero with units within a string")
+  tr.ok("a { color: color(rgb(0,0,0) lightness(50%)); }", "zero in functions")
+  tr.ok("a { color: color(rgb(0,0,0) lightness(0%)); }", "% is ok")
+  tr.ok("a { transition-delay: 0s; }", "dimension unit is ok")
+
+  tr.notOk("a { top: 0px; }", messages.rejected)
+  tr.notOk("a { top: 0.000px; }", messages.rejected)
+  tr.notOk("a { padding: 0px 1px 2px 3px; }", messages.rejected)
+  tr.notOk("a { padding: 1px 0vmax 2px 3px; }", messages.rejected)
+  tr.notOk("a { padding: 1px 2px 0rem 3px; }", messages.rejected)
+  tr.notOk("a { padding: 1px 2px 3px 0em; }", messages.rejected)
+  tr.notOk("a { padding: calc(1in + 0in * 2)); }", messages.rejected)
+})
---
+++ src/rules/number-no-unitized-zero/index.js
@@ -0,0 +1,91 @@
+import {
+  findLastIndex,
+  findIndex,
+  range
+} from "lodash"
+import {
+  ruleMessages,
+  styleSearch
+} from "../../utils"
+
+export const ruleName = "number-no-unitized-zero"
+export const messages = ruleMessages(ruleName, {
+  rejected: "Unexpected unit on zero value",
+})
+
+// Only length units can be left off
+// cf. http://www.w3.org/TR/css3-values/#length-value
+// cf. https://github.com/brigade/scss-lint/issues/154
+const lengthUnits = new Set([
+  "em", "ex", "ch", "vw", "vh", "cm", "mm", "in", "pt", "pc", "px",
+  "rem",
+  "vmin", "vmax",
+])
+
+export default function () {
+  return function (css, result) {
+    css.eachDecl(function (decl) {
+      const value = decl.value
+      const ignorableIndexes = new Set()
+
+      styleSearch({ source: value, target: "0" }, match => {
+        const index = match.startIndex
+
+        // Given a 0 somewhere in the full property value (not in a string, thanks
+        // to styleSearch) we need to isolate the value that contains the zero.
+        // To do so, we'll find the last index before the 0 of a character that would
+        // divide one value in a list from another, and the next index of such a
+        // character; then we build a substring from those indexes, which we can
+        // assess.
+
+        // If a single value includes multiple 0's (e.g. 100.01px), we don't want
+        // each 0 to be treated as a separate value, possibly resulting in multiple
+        // warnings for the same value (e.g. 0.00px).
+        //
+        // This check prevents that from happening: we build and check against a
+        // Set containing all the indexes that are part of a value already validated.
+        if (ignorableIndexes.has(index)) { return }
+
+        const prevValueBreakIndex = findLastIndex(value.substr(0, index), char => {
+          return [ " ", ",", ")", "(", "#" ].indexOf(char) !== -1
+        })
+
+        // Ignore hex colors
+        if (value[prevValueBreakIndex] === "#") { return }
+
+        // If no prev break was found, this value starts at 0
+        const valueWithZeroStart = (prevValueBreakIndex === -1)
+          ? 0
+          : prevValueBreakIndex + 1
+
+        const nextValueBreakIndex = findIndex(value.substr(valueWithZeroStart), char => {
+          return [ " ", ",", ")" ].indexOf(char) !== -1
+        })
+
+        // If no next break was found, this value ends at the end of the string
+        const valueWithZeroEnd = (nextValueBreakIndex === -1)
+          ? value.length
+          : nextValueBreakIndex + valueWithZeroStart
+
+        const valueWithZero = value.slice(valueWithZeroStart, valueWithZeroEnd)
+
+        // Add the indexes to ignorableIndexes so the same value will not
+        // be checked multiple times.
+        range(valueWithZeroStart, valueWithZeroEnd).forEach(i => ignorableIndexes.add(i))
+
+        // Only pay attention if the value parses to 0
+        if (parseInt(valueWithZero, 10) !== 0) { return }
+
+        // If there is not a length unit at the end of this value, ignore.
+        // (Length units are 2, 3, or 4 characters)
+        if (
+          !lengthUnits.has(valueWithZero.slice(-2))
+          && !lengthUnits.has(valueWithZero.slice(-3))
+          && !lengthUnits.has(valueWithZero.slice(-4))
+        ) { return }
+
+        result.warn(messages.rejected, { node: decl })
+      })
+    })
+  }
+}
