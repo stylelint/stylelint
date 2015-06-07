9c64f670ed4b7afc965b1a430b46b793cb4861fb
Richard Hallows <hello@richardhallows.com>
2015-06-07 13:18:02 +0100

    Fix lint errors

--- scripts/rules.js
+++ scripts/rules.js
@@ -3,15 +3,15 @@ import fs from "fs"
 const ruleDirectory = "src/rules"
 const rulesMdFile = "docs/rules.md"
 
-fs.writeFileSync(rulesMdFile, `# Rules\n\n`);
+fs.writeFileSync(rulesMdFile, `# Rules\n\n`)
 
 fs.readdirSync(ruleDirectory).forEach(filename => {
 
-  let readmeFile = `${ruleDirectory}/${filename}/README.md`;
+  let readmeFile = `${ruleDirectory}/${filename}/README.md`
 
   if (!fs.existsSync(readmeFile)) { return }
 
-  let lines = fs.readFileSync(readmeFile).toString('utf-8').split("\n");
+  let lines = fs.readFileSync(readmeFile).toString("utf-8").split("\n")
 
-  fs.appendFileSync(rulesMdFile, `* [\`${filename}\`](../${readmeFile}): ${lines[2]}\n`);
-});
+  fs.appendFileSync(rulesMdFile, `* [\`${filename}\`](../${readmeFile}): ${lines[2]}\n`)
+})
