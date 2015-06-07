e3c64813f926c85d484e2c9dfff1536b79786d52
David Clark <david.dave.clark@gmail.com>
2015-06-07 16:39:54 -0700

    Create and test media-query-list-comma-newline-after

---
+++ src/rules/media-query-list-comma-newline-after/index.js
@@ -0,0 +1,23 @@
+import {
+  ruleMessages,
+  whitespaceChecker
+} from "../../utils"
+import { mediaQueryListCommaChecker } from "../media-query-list-comma-space-after"
+
+export const ruleName = "media-query-list-comma-newline-after"
+
+export const messages = ruleMessages(ruleName, {
+  expectedAfter: () => `Expected newline after ","`,
+  rejectedAfter: () => `Unexpected space after ","`,
+})
+
+/**
+ * @param {"always"|"never"} expectation
+ */
+export default function (expectation) {
+  const checker = whitespaceChecker("\n", expectation, messages)
+
+  // Only check for the newline after the comma, while allowing
+  // arbitrary whitespace to indent after the newline
+  return mediaQueryListCommaChecker(checker.afterOneOnly)
+}
