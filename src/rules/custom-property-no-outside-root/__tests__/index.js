90df9bf82b7c9563a6c3f4c1efba2d35006778ca
David Clark <david.dave.clark@gmail.com>
2015-06-07 17:35:02 -0700

    Create and test custom-property-no-outside-root

---
+++ src/rules/custom-property-no-outside-root/__tests__/index.js
@@ -0,0 +1,15 @@
+import { ruleTester } from "../../../testUtils"
+import rule, { ruleName, messages } from ".."
+
+const testRule = ruleTester(rule, ruleName)
+
+testRule(null, tr => {
+  tr.ok(":root { --foo-bar: 1px; }")
+  tr.ok("a { color: pink; }")
+  tr.ok("a { -webkit-transform: 1px; }")
+
+  tr.notOk("a { --foo-bar: 1px; }", messages.rejected)
+  tr.notOk("a { color: pink; -webkit-transform: 1px; --foo-bar: 1px; }", messages.rejected)
+  tr.notOk(":root, a { --foo-bar: 1px; }", messages.rejected)
+  tr.notOk(":root a { --foo-bar: 1px; }", messages.rejected)
+})
