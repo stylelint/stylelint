702ec2dec112dbd414faeed08478f312cfe6f9e7
David Clark <david.dave.clark@gmail.com>
2015-06-15 06:43:11 -0700

    Create and test selector-no-id

---
+++ src/rules/selector-no-id/index.js
@@ -0,0 +1,29 @@
+import selectorParser from "postcss-selector-parser"
+import {
+  report,
+  ruleMessages
+} from "../../utils"
+
+export const ruleName = "selector-no-id"
+
+export const messages = ruleMessages(ruleName, {
+  rejected: "Unexpected id selector",
+})
+
+export default function () {
+  return (css, result) => {
+    css.eachRule(rule => {
+      selectorParser(selectorAST => {
+        selectorAST.eachId(() => {
+          report({
+            message: messages.rejected,
+            node: rule,
+            ruleName,
+            result,
+          })
+        })
+      })
+        .process(rule.selector)
+    })
+  }
+}
