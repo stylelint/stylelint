3a7cdf343e70aa39c9e2081d66102def4654c3b1
David Clark <david.dave.clark@gmail.com>
2015-06-16 06:35:48 -0700

    Create and test value-list-comma-newline-after

---
+++ src/rules/value-list-comma-newline-after/__tests__/index.js
@@ -0,0 +1,52 @@
+import { ruleTester } from "../../../testUtils"
+import rule, { ruleName, messages } from ".."
+
+const testRule = ruleTester(rule, ruleName)
+
+testRule("always", tr => {
+  tr.ok("a {}")
+  tr.ok("a::before { content: \"foo,bar,baz\"; }")
+  tr.ok("a::before { content: attr(data-foo,\"baz\"); }")
+  tr.ok("a::before { background: url('foo,bar,baz'); }")
+  tr.ok("a { transform: translate(1,1); }")
+  tr.ok("a { background-size: 0,\n0; }")
+  tr.ok("a { background-size: 0 ,\n  0; }")
+
+  tr.notOk("a { background-size: 0, 0; }", messages.expectedAfter())
+  tr.notOk("a { background-size: 0,  0; }", messages.expectedAfter())
+  tr.notOk("a { background-size: 0,\t0; }", messages.expectedAfter())
+})
+
+testRule("never", tr => {
+  tr.ok("a {}")
+  tr.ok("a::before { content: \"foo ,bar ,baz\"; }")
+  tr.ok("a::before { content: attr(data-foo ,\"baz\"); }")
+  tr.ok("a::before { background: url('foo ,bar ,baz'); }")
+  tr.ok("a { transform: translate(1,1); }")
+  tr.ok("a { background-size: 0,0; }")
+  tr.ok("a { background-size: 0 ,0; }")
+
+  tr.notOk("a { background-size: 0, 0; }", messages.rejectedAfter())
+  tr.notOk("a { background-size: 0,  0; }", messages.rejectedAfter())
+  tr.notOk("a { background-size: 0,\n0; }", messages.rejectedAfter())
+  tr.notOk("a { background-size: 0,\t0; }", messages.rejectedAfter())
+})
+
+testRule("always-multi-line", tr => {
+  tr.ok("a { background-size: 0,\n0,\n0; }")
+  tr.ok("a { background-size: 0 ,\n  0,\n0; }")
+  tr.ok("a { background-size: 0, 0; }", "ignores single-line")
+
+  tr.notOk("a { background-size: 0,\n0, 0; }", messages.expectedAfterMultiLine())
+  tr.notOk("a { background-size: 0,\n0,  0; }", messages.expectedAfterMultiLine())
+  tr.notOk("a { background-size: 0,\n0,\t0; }", messages.expectedAfterMultiLine())
+})
+
+testRule("never-multi-line", tr => {
+  tr.ok("a { background-size: 0\n,0\n,0; }")
+  tr.ok("a { background-size: 0, 0; }", "ignores single-line")
+
+  tr.notOk("a { background-size: 0\n,0\n, 0; }", messages.rejectedAfterMultiLine())
+  tr.notOk("a { background-size: 0\n,0\n,  0; }", messages.rejectedAfterMultiLine())
+  tr.notOk("a { background-size: 0\n,0\n,\t0; }", messages.rejectedAfterMultiLine())
+})
