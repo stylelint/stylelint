153aa4521993e096a6aaef2f60b16fc05922c83b
Richard Hallows <hello@richardhallows.com>
2015-07-16 17:18:35 +0100

    Incorporate suggestions

--- src/rules/color-hex-length/index.js
+++ src/rules/color-hex-length/index.js
@@ -7,7 +7,7 @@ import {
 export const ruleName = "color-hex-length"
 
 export const messages = ruleMessages(ruleName, {
-  expected: (l, h) => `Expected ${l} hex color ${h}`,
+  expected: (l, h, v) => `Expected "${h}"" to be in the ${l} notation form of "${v}"`,
 })
 
 /**
@@ -26,23 +26,41 @@ export default function (expectation) {
 
         if (expectation === "short" && (hexValue.length < 6 || !canShrink(hexValue))) { return }
 
+        const variant = expectation === "long" ? longer : shorter
+
         report({
-          message: messages.expected(expectation, hexValue),
+          message: messages.expected(expectation, hexValue, variant(hexValue)),
           node: decl,
           result,
           ruleName,
         })
       })
     })
-
-    function canShrink(hex) {
-      hex = hex.toLowerCase()
-
-      return (
-        hex[1] === hex[2] &&
-        hex[3] === hex[4] &&
-        hex[5] === hex[6] &&
-        (hex.length === 7 || (hex.length === 9 && hex[7] === hex[8])))
-    }
   }
+}
+
+function canShrink(hex) {
+  hex = hex.toLowerCase()
+
+  return (
+    hex[1] === hex[2] &&
+    hex[3] === hex[4] &&
+    hex[5] === hex[6] &&
+    (hex.length === 7 || (hex.length === 9 && hex[7] === hex[8])))
+}
+
+function shorter(hex) {
+  let hexVariant = "#"
+  for (let i = 1; i < hex.length; i = i + 2) {
+    hexVariant += hex[i]
+  }
+  return hexVariant
+}
+
+function longer(hex) {
+  let hexVariant = "#"
+  for (let i = 1; i < hex.length; i++) {
+    hexVariant += hex[i] + hex[i]
+  }
+  return hexVariant
 }
