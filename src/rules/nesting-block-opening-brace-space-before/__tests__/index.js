a80e325509821ef92ba413febde67b722ca51761
David Clark <david.dave.clark@gmail.com>
2015-08-29 12:34:15 -0700

    Add accuracy for nesting-block-opening-brace-space-before

--- src/rules/nesting-block-opening-brace-space-before/__tests__/index.js
+++ src/rules/nesting-block-opening-brace-space-before/__tests__/index.js
@@ -13,11 +13,31 @@ testRule("always", tr => {
   tr.ok("a { { &:hover { color: pink; }}}")
   tr.ok("a { { &:hover { &:before { color: pink; }}}}")
 
-  tr.notOk("a {{ &:hover { color: pink; }}}", messages.expectedBefore())
-  tr.notOk("a {{ &:hover {\ncolor: pink; }}}", messages.expectedBefore())
-  tr.notOk("a {\t{ &:hover { color: pink; }}}", messages.expectedBefore())
-  tr.notOk("a { { &:hover {\n{ &:before { color: pink; }}}}}", messages.expectedBefore())
-  tr.notOk("a {\n{ &:hover { { &:before { color: pink; }}}}}", messages.expectedBefore())
+  tr.notOk("a {{ &:hover { color: pink; }}}", {
+    message: messages.expectedBefore(),
+    line: 1,
+    column: 4,
+  })
+  tr.notOk("a {{ &:hover {\ncolor: pink; }}}", {
+    message: messages.expectedBefore(),
+    line: 1,
+    column: 4,
+  })
+  tr.notOk("a {\t{ &:hover { color: pink; }}}", {
+    message: messages.expectedBefore(),
+    line: 1,
+    column: 5,
+  })
+  tr.notOk("a { { &:hover {\n{ &:before { color: pink; }}}}}", {
+    message: messages.expectedBefore(),
+    line: 2,
+    column: 1,
+  })
+  tr.notOk("a {\n{ &:hover { { &:before { color: pink; }}}}}", {
+    message: messages.expectedBefore(),
+    line: 2,
+    column: 1,
+  })
 })
 
 testRule("never", tr => {
@@ -27,11 +47,31 @@ testRule("never", tr => {
   tr.ok("a {{ &:hover { color: pink; }}}")
   tr.ok("a {{ &:hover { &:before { color: pink; }}}}")
 
-  tr.notOk("a { { &:hover { color: pink; }}}", messages.rejectedBefore())
-  tr.notOk("a {  { &:hover {\ncolor: pink; }}}", messages.rejectedBefore())
-  tr.notOk("a {\t{ &:hover { color: pink; }}}", messages.rejectedBefore())
-  tr.notOk("a {\r\n{ &:hover {{ &:before { color: pink; }}}}}", messages.rejectedBefore())
-  tr.notOk("a {{ &:hover {\n{ &:before { color: pink; }}}}}", messages.rejectedBefore())
+  tr.notOk("a { { &:hover { color: pink; }}}", {
+    message: messages.rejectedBefore(),
+    line: 1,
+    column: 5,
+  })
+  tr.notOk("a {  { &:hover {\ncolor: pink; }}}", {
+    message: messages.rejectedBefore(),
+    line: 1,
+    column: 6,
+  })
+  tr.notOk("a {\t{ &:hover { color: pink; }}}", {
+    message: messages.rejectedBefore(),
+    line: 1,
+    column: 5,
+  })
+  tr.notOk("a {\r\n{ &:hover {{ &:before { color: pink; }}}}}", {
+    message: messages.rejectedBefore(),
+    line: 2,
+    column: 1,
+  })
+  tr.notOk("a {{ &:hover {\n{ &:before { color: pink; }}}}}", {
+    message: messages.rejectedBefore(),
+    line: 2,
+    column: 1,
+  })
 })
 
 testRule("always-single-line", tr => {
@@ -45,10 +85,26 @@ testRule("always-single-line", tr => {
   tr.ok("a {{ &:hover\n{ color: pink;\n}}}", "multi-line")
   tr.ok("a {{ &:hover \n{ { &:before { color: pink; }}}}}", "multi-line")
 
-  tr.notOk("a {{ &:hover { color: pink; }}}", messages.expectedBeforeSingleLine())
-  tr.notOk("a {\t{ &:hover { color: pink; }}}", messages.expectedBeforeSingleLine())
-  tr.notOk("a {\n{ &:hover { { &:before { color: pink; }}}}}", messages.expectedBeforeSingleLine())
-  tr.notOk("a { { &:hover {\n{ &:before { color: pink; }}}}}", messages.expectedBeforeSingleLine())
+  tr.notOk("a {{ &:hover { color: pink; }}}", {
+    message: messages.expectedBeforeSingleLine(),
+    line: 1,
+    column: 4,
+  })
+  tr.notOk("a {\t{ &:hover { color: pink; }}}", {
+    message: messages.expectedBeforeSingleLine(),
+    line: 1,
+    column: 5,
+  })
+  tr.notOk("a {\n{ &:hover { { &:before { color: pink; }}}}}", {
+    message: messages.expectedBeforeSingleLine(),
+    line: 2,
+    column: 1,
+  })
+  tr.notOk("a { { &:hover {\n{ &:before { color: pink; }}}}}", {
+    message: messages.expectedBeforeSingleLine(),
+    line: 2,
+    column: 1,
+  })
 })
 
 testRule("never-single-line", tr => {
@@ -60,10 +116,26 @@ testRule("never-single-line", tr => {
 
   tr.ok("a { { &:hover\n{ color: pink; }}}", "multi-line")
 
-  tr.notOk("a { { &:hover { color: pink; }}}", messages.rejectedBeforeSingleLine())
-  tr.notOk("a {\n{ &:hover { color: pink; }}}", messages.rejectedBeforeSingleLine())
-  tr.notOk("a {\r\n{ &:hover { color: pink; }}}", messages.rejectedBeforeSingleLine())
-  tr.notOk("a {\n\t{ &:hover {{ &:before { color: pink; }}}}}", messages.rejectedBeforeSingleLine())
+  tr.notOk("a { { &:hover { color: pink; }}}", {
+    message: messages.rejectedBeforeSingleLine(),
+    line: 1,
+    column: 5,
+  })
+  tr.notOk("a {\n{ &:hover { color: pink; }}}", {
+    message: messages.rejectedBeforeSingleLine(),
+    line: 2,
+    column: 1,
+  })
+  tr.notOk("a {\r\n{ &:hover { color: pink; }}}", {
+    message: messages.rejectedBeforeSingleLine(),
+    line: 2,
+    column: 1,
+  })
+  tr.notOk("a {\n\t{ &:hover {{ &:before { color: pink; }}}}}", {
+    message: messages.rejectedBeforeSingleLine(),
+    line: 2,
+    column: 2,
+  })
 })
 
 testRule("always-multi-line", tr => {
@@ -77,9 +149,21 @@ testRule("always-multi-line", tr => {
   tr.ok("a {{ &:hover { color: pink; }}}", "single-line")
   tr.ok("a {{ &:hover {\t\t\t{ &:before { color: pink; }}}}}", "single-line")
 
-  tr.notOk("a {{ &:hover\n{ color: pink; }}}", messages.expectedBeforeMultiLine())
-  tr.notOk("a {\t{ &:hover\n{ color: pink; }}}", messages.expectedBeforeMultiLine())
-  tr.notOk("a { { &:hover {{ &:before\r\n{ color: pink; }}}}}", messages.expectedBeforeMultiLine())
+  tr.notOk("a {{ &:hover\n{ color: pink; }}}", {
+    message: messages.expectedBeforeMultiLine(),
+    line: 1,
+    column: 4,
+  })
+  tr.notOk("a {\t{ &:hover\n{ color: pink; }}}", {
+    message: messages.expectedBeforeMultiLine(),
+    line: 1,
+    column: 5,
+  })
+  tr.notOk("a { { &:hover {{ &:before\r\n{ color: pink; }}}}}", {
+    message: messages.expectedBeforeMultiLine(),
+    line: 1,
+    column: 16,
+  })
 })
 
 testRule("never-multi-line", tr => {
@@ -92,7 +176,19 @@ testRule("never-multi-line", tr => {
   tr.ok("a { { &:hover { color: pink; }}}", "single-line")
   tr.ok("a {\t\t{ &:hover { color: pink; }}}", "single-line")
 
-  tr.notOk("a { { &:hover\n{ color: pink; }}}", messages.rejectedBeforeMultiLine())
-  tr.notOk("a {\t{ &:hover\r\n{ color: pink; }}}", messages.rejectedBeforeMultiLine())
-  tr.notOk("a { { &:hover {{ &:before\n{ color: pink; }}}}}", messages.rejectedBeforeMultiLine())
+  tr.notOk("a { { &:hover\n{ color: pink; }}}", {
+    message: messages.rejectedBeforeMultiLine(),
+    line: 1,
+    column: 5,
+  })
+  tr.notOk("a {\t{ &:hover\r\n{ color: pink; }}}", {
+    message: messages.rejectedBeforeMultiLine(),
+    line: 1,
+    column: 5,
+  })
+  tr.notOk("a { { &:hover {{ &:before\n{ color: pink; }}}}}", {
+    message: messages.rejectedBeforeMultiLine(),
+    line: 1,
+    column: 5,
+  })
 })
