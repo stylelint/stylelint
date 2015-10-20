71b5f6fc5ea9e2706a5d0120089fd63b369b3165
jeddy3 <jeddy3@users.noreply.github.com>
2015-10-20 21:25:21 +0100

    Add nested function and nested block tests

--- src/rules/function-blacklist/__tests__/index.js
+++ src/rules/function-blacklist/__tests__/index.js
@@ -15,6 +15,8 @@ testRule([
   tr.ok("a { color: pink; }")
   tr.ok("a { transform: rotate(7deg) }")
   tr.ok("a { background: -webkit-radial-gradient(red, green, blue); }")
+  tr.ok("a { color: color(rgb(0, 0, 0) lightness(50%)); }")
+  tr.ok("@media (max-width: 10px) { a { color: color(rgb(0, 0, 0) lightness(50%)); } }")
 
   tr.notOk("a { transform: scale(1); }", {
     message: messages.rejected("scale"),
@@ -46,9 +48,20 @@ testRule([
     line: 1,
     column: 12,
   })
+  tr.notOk("a { color: color(rgba(0, 0, 0, 0) lightness(50%)); }", {
+    message: messages.rejected("rgba"),
+    line: 1,
+    column: 18,
+  })
   tr.notOk("a { background: red, -moz-linear-gradient(45deg, blue, red); }", {
     message: messages.rejected("-moz-linear-gradient"),
     line: 1,
     column: 22,
   })
+  tr.notOk("@media (max-width: 10px) { a { color: color(rgba(0, 0, 0) lightness(50%)); } }", {
+    message: messages.rejected("rgba"),
+    line: 1,
+    column: 45,
+  })
+
 })
