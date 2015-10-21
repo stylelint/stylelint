e030409bfb51ed41a41e2e63ebfb882a8c2d6e3d
makotot <makoto.ttn@gmail.com>
2015-10-21 17:55:16 +0900

    Add rule : color-no-named

---
+++ src/rules/color-no-named/index.js
@@ -0,0 +1,41 @@
+import valueParser from "postcss-value-parser"
+import isCssColorName from "is-css-color-name"
+
+import {
+  report,
+  ruleMessages,
+  validateOptions,
+} from "../../utils"
+
+export const ruleName = "color-no-named"
+
+export const messages = ruleMessages(ruleName, {
+  rejected: c => `Unexpected named color "${c}"`,
+})
+
+export default function (actual) {
+  return (root, result) => {
+    const validOptions = validateOptions(result, ruleName, { actual })
+    if (!validOptions) { return }
+
+    root.walkDecls(decl => {
+      const { value } = decl
+
+      valueParser(value).walk(function (node) {
+        const charsBeforeColon = decl.toString().indexOf(":")
+        const charsAfterColon = decl.raw("between").length - decl.raw("between").indexOf(":")
+
+        if (isCssColorName(node.value)) {
+          report({
+            message: messages.rejected(node.value),
+            node: decl,
+            index: charsBeforeColon + charsAfterColon + node.sourceIndex,
+            result,
+            ruleName,
+          })
+        }
+      })
+    })
+  }
+}
+
