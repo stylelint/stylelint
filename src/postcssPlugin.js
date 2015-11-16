d2f2abba25808983ca903b9da0c1d02835885fa4
Bogdan Chadkin <trysound@yandex.ru>
2015-11-16 04:19:20 +0300

    Promised refactoring

--- src/postcssPlugin.js
+++ src/postcssPlugin.js
@@ -7,52 +7,48 @@ import buildConfig from "./buildConfig"
 
 export default postcss.plugin("stylelint", (options = {}) => {
   return (root, result) => {
-    return new Promise((resolve, reject) => {
-      // result.stylelint is the namespace for passing stylelint-related
-      // configuration and data across sub-plugins via the PostCSS Result
-      result.stylelint = result.stylelint || {}
-      result.stylelint.ruleSeverities = {}
+    // result.stylelint is the namespace for passing stylelint-related
+    // configuration and data across sub-plugins via the PostCSS Result
+    result.stylelint = result.stylelint || {}
+    result.stylelint.ruleSeverities = {}
 
-      buildConfig(options).then(config => {
-        if (!config) {
-          throw configurationError("No configuration provided")
+    return buildConfig(options).then(config => {
+      if (!config) {
+        throw configurationError("No configuration provided")
+      }
+      if (!config.rules) {
+        throw configurationError("No rules found within configuration")
+      }
+      if (config.plugins) {
+        merge(ruleDefinitions, mapValues(config.plugins, plugin => require(plugin)))
+      }
+
+      // Register details about the configuration
+      result.stylelint.quiet = config.quiet
+
+      // First check for disabled ranges, adding them to the result object
+      disableRanges(root, result)
+
+      Object.keys(config.rules).forEach(ruleName => {
+        if (!ruleDefinitions[ruleName]) {
+          throw configurationError(`Undefined rule ${ruleName}`)
         }
-        if (!config.rules) {
-          throw configurationError("No rules found within configuration")
+
+        // If severity is 0, run nothing
+        const ruleSettings = config.rules[ruleName]
+        const ruleSeverity = (Array.isArray(ruleSettings))
+          ? ruleSettings[0]
+          : ruleSettings
+        if (ruleSeverity === 0) {
+          return
         }
-        if (config.plugins) {
-          merge(ruleDefinitions, mapValues(config.plugins, plugin => require(plugin)))
-        }
 
-        // Register details about the configuration
-        result.stylelint.quiet = config.quiet
+        // Log the rule's severity
+        result.stylelint.ruleSeverities[ruleName] = ruleSeverity
 
-        // First check for disabled ranges, adding them to the result object
-        disableRanges(root, result)
-
-        Object.keys(config.rules).forEach(ruleName => {
-          if (!ruleDefinitions[ruleName]) {
-            throw configurationError(`Undefined rule ${ruleName}`)
-          }
-
-          // If severity is 0, run nothing
-          const ruleSettings = config.rules[ruleName]
-          const ruleSeverity = (Array.isArray(ruleSettings))
-            ? ruleSettings[0]
-            : ruleSettings
-          if (ruleSeverity === 0) {
-            return
-          }
-
-          // Log the rule's severity
-          result.stylelint.ruleSeverities[ruleName] = ruleSeverity
-
-          // Run the rule with the primary and secondary options
-          ruleDefinitions[ruleName](ruleSettings[1], ruleSettings[2])(root, result)
-        })
-
-        resolve()
-      }).catch(reject)
+        // Run the rule with the primary and secondary options
+        ruleDefinitions[ruleName](ruleSettings[1], ruleSettings[2])(root, result)
+      })
     })
   }
 })
