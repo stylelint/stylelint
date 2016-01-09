d6591204a90578226137807f0c4aa685b5fa2acb
jeddy3 <jeddy3@users.noreply.github.com>
2016-01-09 23:58:38 +0000

    Add more nth-child exclusions

--- src/rules/selector-no-type/index.js
+++ src/rules/selector-no-type/index.js
@@ -15,15 +15,18 @@ export default function (actual) {
   return (root, result) => {
     const validOptions = validateOptions(result, ruleName, { actual })
     if (!validOptions) { return }
-    // Nesting, nth-child, and keyframes
-    const exclusions = ["&", "n", "to", "from"]
+    // 1st for nesting, 2nd to 4th for nth-child, and last two for keyframes
+    const exclusions = ["&", "n", "even", "odd", "to", "from"]
     root.walkRules(rule => {
       selectorParser(selectorAST => {
         selectorAST.eachTag(tag => {
+          const firstChar = tag.value.charAt(0)
           // Check if in exclusions
           if (exclusions.indexOf(tag.value) !== -1) { return }
           // Also check if starts with a digit, for nth-child(3n) and %s in keyframes
-          if (!isNaN(tag.value.charAt(0))) { return }
+          if (!isNaN(firstChar)) { return }
+          // Lastly check for "-", for nth-child(-n)
+          if (firstChar === "-") { return }
           report({
             message: messages.rejected,
             node: rule,
