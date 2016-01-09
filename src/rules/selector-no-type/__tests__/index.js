9ab1abb563be7c0abc8203979cf167e13a9970af
jeddy3 <jeddy3@users.noreply.github.com>
2016-01-09 23:49:20 +0000

    Add exclusions to selector-no-type

--- src/rules/selector-no-type/__tests__/index.js
+++ src/rules/selector-no-type/__tests__/index.js
@@ -19,7 +19,11 @@ testRule(undefined, tr => {
   tr.ok(".foo { [&] {} }")
   tr.ok(".foo { & [class*=bar] {} }")
   tr.ok(".foo { @nest & {} }")
-  
+  tr.ok(".foo:nth-child(3n + 1) {}")
+  tr.ok(".foo { &:nth-child(3n + 1) {} }")
+  tr.ok("@keyframes spin { 0% {} }")
+  tr.ok("@keyframes spin { to {} from {} }")
+
   tr.notOk("foo {}", {
     message: messages.rejected,
     line: 1,
--- src/rules/selector-no-type/index.js
+++ src/rules/selector-no-type/index.js
@@ -15,11 +15,15 @@ export default function (actual) {
   return (root, result) => {
     const validOptions = validateOptions(result, ruleName, { actual })
     if (!validOptions) { return }
-
+    // Nesting, nth-child, and keyframes
+    const exclusions = ["&", "n", "to", "from"]
     root.walkRules(rule => {
       selectorParser(selectorAST => {
         selectorAST.eachTag(tag => {
-          if (tag.value === "&") { return }
+          // Check if in exclusions
+          if (exclusions.indexOf(tag.value) !== -1) { return }
+          // Also check if starts with a digit, for nth-child(3n) and %s in keyframes
+          if (!isNaN(tag.value.charAt(0))) { return }
           report({
             message: messages.rejected,
             node: rule,
