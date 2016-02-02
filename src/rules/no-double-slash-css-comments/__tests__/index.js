9ed7d53da0742d480724517151451f7d9ea6c1f5
Juriy Zaytsev <kangax@fb.com>
2016-02-01 20:33:17 -0500

    Add no double slash css comments rule

---
+++ src/rules/no-double-slash-css-comments/__tests__/index.js
@@ -0,0 +1,46 @@
+import {
+  ruleTester,
+  warningFreeBasics,
+} from "../../../testUtils"
+import rule, { ruleName, messages } from ".."
+
+const testRule = ruleTester(rule, ruleName)
+
+testRule(undefined, tr => {
+  warningFreeBasics(tr)
+
+  tr.ok("a { /* color: pink; */ }", "regular comment around declaration")
+  tr.ok("/* a { color: pink; } */", "regular comment around rule")
+  tr.ok("a { background: url(//foo.com/bar.png) }", "url with double slash")
+
+  tr.notOk(
+    "a { // color: pink; }",
+    {
+      message: messages.rejected,
+      line: 1,
+      column: 8,
+    },
+    "before declaration"
+  )
+
+  tr.notOk(
+    "// a { color: pink; }",
+    {
+      message: messages.rejected,
+      line: 1,
+      column: 19,
+    },
+    "before rule"
+  )
+
+  tr.notOk(
+    "a, // div { color: pink; }",
+    {
+      message: messages.rejected,
+      line: 1,
+      column: 19,
+    },
+    "between rules"
+  )
+})
+
---
+++ src/rules/no-double-slash-css-comments/index.js
@@ -0,0 +1,41 @@
+import {
+  report,
+  ruleMessages,
+  validateOptions,
+} from "../../utils"
+
+export const ruleName = "no-double-slash-css-comments"
+
+export const messages = ruleMessages(ruleName, {
+  rejected: `Unexpected double-slash CSS comment`,
+})
+
+export default function (actual) {
+  return (root, result) => {
+    const validOptions = validateOptions(result, ruleName, { actual })
+    if (!validOptions) { return }
+
+    root.walkDecls(decl => {
+      if (decl.prop.indexOf('//') === 0) {
+        report({
+          message: messages.rejected,
+          node: decl,
+          result,
+          ruleName,
+        })
+      }
+    })
+    root.walkRules(rule => {
+      rule.selectors.forEach(selector => {
+        if (selector.indexOf('//') === 0) {
+          report({
+            message: messages.rejected,
+            node: rule,
+            result,
+            ruleName,
+          })
+        }
+      })
+    })
+  }
+}
