7f40713268b5bfc4af81c09da1fef13b7a0b17a1
David Clark <david.dave.clark@gmail.com>
2016-03-14 19:23:52 -0700

    Migrate at-rule-* tests

--- src/rules/at-rule-empty-line-before/__tests__/index.js
+++ src/rules/at-rule-empty-line-before/__tests__/index.js
@@ -1,165 +1,326 @@
-import {
-  ruleTester,
-  warningFreeBasics,
-} from "../../../testUtils"
+/* eslint-disable comma-dangle,array-bracket-spacing */
+import testRule from "../../../testUtils/blueTapeStylelintAssert"
+import _ from "lodash"
 import rule, { ruleName, messages } from ".."
 
-const testRule = ruleTester(rule, ruleName)
+const sharedAlwaysTests = {
+  accept: [{
+    code: "a {} b {}",
+    description: "rule ignored",
+  }, {
+    code: "@font-face {}",
+    description: "first node ignored",
+  }, {
+    code: "a {}\n\n@media {}",
+  }, {
+    code: "@keyframes foo {}\n\n@media {}",
+  }, {
+    code: "a {}\r\n\r\n@media {}",
+    description: "windows",
+  }, {
+    code: "a {}\n\r\n@media {}",
+    description: "mixed",
+  }],
 
-function sharedAlwaysTests(tr) {
-  warningFreeBasics(tr)
-
-  tr.ok("a {} b {}", "rule ignored")
-  tr.ok("@font-face {}", "first node ignored")
-  tr.ok("a {}\n\n@media {}")
-  tr.ok("@keyframes foo {}\n\n@media {}")
-  tr.ok("a {}\r\n\r\n@media {}", "windows")
-  tr.ok("a {}\n\r\n@media {}", "mixed")
-
-  tr.notOk("a {} @media {}", messages.expected)
-  tr.notOk("@keyframes foo {} @media {}", messages.expected)
-  tr.notOk("a {}\n@media {}", messages.expected)
-  tr.notOk("a {}\r\n@media {}", messages.expected)
-  tr.notOk("a {}\n\n/* comment */\n@media {}", messages.expected)
-  tr.notOk("a {}\r\n\r\n/* comment */\r\n@media {}", messages.expected)
+  reject: [{
+    code: "a {} @media {}",
+    message: messages.expected,
+  }, {
+    code: "@keyframes foo {} @media {}",
+    message: messages.expected,
+  }, {
+    code: "a {}\n@media {}",
+    message: messages.expected,
+  }, {
+    code: "a {}\r\n@media {}",
+    message: messages.expected,
+  }, {
+    code: "a {}\n\n/* comment */\n@media {}",
+    message: messages.expected,
+  }, {
+    code: "a {}\r\n\r\n/* comment */\r\n@media {}",
+    message: messages.expected,
+  }],
 }
 
-testRule("always", tr => {
-  sharedAlwaysTests(tr)
+const sharedNeverTests = {
+  accept: [{
+    code: "a {}\n\nb {}",
+    description: "rule ignored",
+  }, {
+    code: "\n\n@font-face {}",
+    description: "first node ignored",
+  }, {
+    code: "a {}\n@media {}",
+  }, {
+    code: "a {} @media {}",
+  }, {
+    code: "@keyframes foo {}\n@media {}",
+  }, {
+    code: "@keyframes foo {} @media {}",
+  }],
 
-  tr.ok("a {\n\n  @mixin foo;\n}")
-})
-
-testRule("always", { except: ["blockless-group"] }, tr => {
-  sharedAlwaysTests(tr)
-
-  tr.ok("a {\n\n  @mixin foo;\n}")
-
-  tr.ok("@keyframes foo {}\n\n@import 'x.css'", "empty line not blockless pair")
-  tr.ok("@import 'x.css';\n@import 'y.css'", "no empty line blockless pair")
-  tr.ok("@import 'x.css';", "single blockless rule")
-
-  tr.notOk("@keyframes foo {}\n@import 'x.css'", messages.expected)
-  tr.notOk("@import 'x.css';\n\n@import 'y.css'", messages.rejected)
-})
-
-testRule("always", { ignore: ["after-comment"] }, tr => {
-  tr.ok("/* foo */\n@media {}")
-  tr.ok("/* foo */\n\n@media{}")
-  tr.ok("/* foo */\r\n\r\n@media {}", "CRLF")
-
-  tr.notOk("a {} @media {}", messages.expected)
-})
-
-testRule("always", { except: ["all-nested"] }, tr => {
-  sharedAlwaysTests(tr)
-
-  tr.ok("a {\n  @mixin foo;\n  color: pink;\n}")
-  tr.ok("a {\n  color: pink;\n  @mixin foo;\n}")
-
-  tr.notOk("a {\n\n  @mixin foo;\n  color: pink;\n}", messages.rejected)
-  tr.notOk("a {\n\n  color: pink;\n\n  @mixin foo;\n}", messages.rejected)
-})
-
-testRule("always", { except: ["first-nested"] }, tr => {
-  sharedAlwaysTests(tr)
-
-  tr.ok("a {\n  @mixin foo;\n  color: pink;\n}")
-
-  tr.notOk("a {\n  color: pink;\n  @mixin foo;\n}", messages.expected)
-  tr.notOk("a {\n\n  @mixin foo;\n  color: pink;\n}", messages.rejected)
-})
-
-testRule("always", { ignore: ["all-nested"] }, tr => {
-  sharedAlwaysTests(tr)
-
-  tr.ok("a {\n  @mixin foo;\n  color: pink;\n}")
-  tr.ok("a {\n  color: pink;\n  @mixin foo;\n}")
-  tr.ok("a {\n\n  @mixin foo;\n  color: pink;\n}")
-  tr.ok("a {\n\n  color: pink;\n\n  @mixin foo;\n}")
-})
-
-testRule("always", { except: [ "blockless-group", "all-nested" ] }, tr => {
-  sharedAlwaysTests(tr)
-
-  tr.ok("a {\n  @mixin foo;\n  color: pink;\n}")
-  tr.ok("a {\n  color: pink;\n  @mixin foo;\n}")
-  tr.ok("@keyframes foo {}\n\n@import 'x.css'", "empty line not blockless pair")
-  tr.ok("@import 'x.css';\n@import 'y.css'", "no empty line blockless pair")
-  tr.ok("@import 'x.css';", "single blockless rule")
-
-  tr.notOk("@keyframes foo {}\n@import 'x.css'", messages.expected)
-  tr.notOk("@import 'x.css';\n\n@import 'y.css'", messages.rejected)
-  tr.notOk("a {\n\n  @mixin foo;\n  color: pink;\n}", messages.rejected)
-  tr.notOk("a {\n\n  color: pink;\n\n  @mixin foo;\n}", messages.rejected)
-
-  tr.ok("a {\n  @mixin foo;\n  @mixin bar;\n}")
-})
-
-function sharedNeverTests(tr) {
-  warningFreeBasics(tr)
-
-  tr.ok("a {}\n\nb {}", "rule ignored")
-  tr.ok("\n\n@font-face {}", "first node ignored")
-  tr.ok("a {}\n@media {}")
-  tr.ok("a {} @media {}")
-  tr.ok("@keyframes foo {}\n@media {}")
-  tr.ok("@keyframes foo {} @media {}")
-
-  tr.notOk("a {}\n\n@media {}", messages.rejected)
-  tr.notOk("@keyframes foo {}\n/* comment */\n\n@media {}", messages.rejected)
+  reject: [{
+    code: "a {}\n\n@media {}",
+    message: messages.rejected,
+  }, {
+    code: "@keyframes foo {}\n/* comment */\n\n@media {}",
+    message: messages.rejected,
+  }],
 }
 
-testRule("never", tr => {
-  sharedNeverTests(tr)
-  tr.ok("a {\n  @mixin foo;\n}")
+testRule(rule, _.mergeWith({
+  ruleName: ruleName,
+  config: ["always"],
+
+  accept: [{
+    code: "a {\n\n  @mixin foo;\n}",
+  }],
+}, sharedAlwaysTests, mergeCustomizer))
+
+testRule(rule, _.mergeWith({
+  ruleName: ruleName,
+  config: ["always", { except: ["blockless-group"] }],
+
+  accept: [{
+    code: "a {\n\n  @mixin foo;\n}",
+  }, {
+    code: "@keyframes foo {}\n\n@import 'x.css'",
+    description: "empty line not blockless pair",
+  }, {
+    code: "@import 'x.css';\n@import 'y.css'",
+    description: "no empty line blockless pair",
+  }, {
+    code: "@import 'x.css';",
+    description: "single blockless rule",
+  }],
+
+  reject: [{
+    code: "@keyframes foo {}\n@import 'x.css'",
+    message: messages.expected,
+  }, {
+    code: "@import 'x.css';\n\n@import 'y.css'",
+    message: messages.rejected,
+  }],
+}, sharedAlwaysTests, mergeCustomizer))
+
+testRule(rule, {
+  ruleName: ruleName,
+  config: ["always", { ignore: ["after-comment"] }],
+
+  accept: [{
+    code: "/* foo */\n@media {}",
+  }, {
+    code: "/* foo */\n\n@media{}",
+  }, {
+    code: "/* foo */\r\n\r\n@media {}",
+    description: "CRLF",
+  }],
+
+  reject: [{
+    code: "a {} @media {}",
+    message: messages.expected,
+  }],
 })
 
-testRule("never", { except: ["blockless-group"] }, tr => {
-  sharedNeverTests(tr)
-  tr.ok("a {\n  @mixin foo;\n}")
+testRule(rule, _.mergeWith({
+  ruleName: ruleName,
+  config: ["always", { except: ["all-nested"] }],
 
-  tr.ok("@keyframes foo {}\n@import 'x.css'", "no empty line not blockless pair")
-  tr.ok("@import 'x.css';\n\n@import 'y.css'", "empty line blockless pair")
-  tr.ok("@import 'x.css';", "single blockless rule")
+  accept: [{
+    code: "a {\n  @mixin foo;\n  color: pink;\n}",
+  }, {
+    code: "a {\n  color: pink;\n  @mixin foo;\n}",
+  }],
 
-  tr.notOk("@keyframes foo {}\n\n@import 'x.css'", messages.rejected)
-  tr.notOk("@import 'x.css';\n@import 'y.css'", messages.expected)
+  reject: [{
+    code: "a {\n\n  @mixin foo;\n  color: pink;\n}",
+    message: messages.rejected,
+  }, {
+    code: "a {\n\n  color: pink;\n\n  @mixin foo;\n}",
+    message: messages.rejected,
+  }],
+}, sharedAlwaysTests, mergeCustomizer))
+
+testRule(rule, _.mergeWith({
+  ruleName: ruleName,
+  config: ["always", { except: ["first-nested"] }],
+
+  accept: [{
+    code: "a {\n  @mixin foo;\n  color: pink;\n}",
+  }],
+
+  reject: [{
+    code: "a {\n  color: pink;\n  @mixin foo;\n}",
+    message: messages.expected,
+  }, {
+    code: "a {\n\n  @mixin foo;\n  color: pink;\n}",
+    message: messages.rejected,
+  }],
+}, sharedAlwaysTests, mergeCustomizer))
+
+testRule(rule, _.mergeWith({
+  ruleName: ruleName,
+  config: ["always", { ignore: ["all-nested"] }],
+
+  accept: [{
+    code: "a {\n  @mixin foo;\n  color: pink;\n}",
+  }, {
+    code: "a {\n  color: pink;\n  @mixin foo;\n}",
+  }, {
+    code: "a {\n\n  @mixin foo;\n  color: pink;\n}",
+  }, {
+    code: "a {\n\n  color: pink;\n\n  @mixin foo;\n}",
+  }],
+}, sharedAlwaysTests, mergeCustomizer))
+
+testRule(rule, _.mergeWith({
+  ruleName: ruleName,
+  config: ["always", { except: [ "blockless-group", "all-nested" ] }],
+
+  accept: [{
+    code: "a {\n  @mixin foo;\n  color: pink;\n}",
+  }, {
+    code: "a {\n  color: pink;\n  @mixin foo;\n}",
+  }, {
+    code: "@keyframes foo {}\n\n@import 'x.css'",
+    description: "empty line not blockless pair",
+  }, {
+    code: "@import 'x.css';\n@import 'y.css'",
+    description: "no empty line blockless pair",
+  }, {
+    code: "@import 'x.css';",
+    description: "single blockless rule",
+  }, {
+    code: "a {\n  @mixin foo;\n  @mixin bar;\n}",
+  }],
+
+  reject: [{
+    code: "@keyframes foo {}\n@import 'x.css'",
+    message: messages.expected,
+  }, {
+    code: "@import 'x.css';\n\n@import 'y.css'",
+    message: messages.rejected,
+  }, {
+    code: "a {\n\n  @mixin foo;\n  color: pink;\n}",
+    message: messages.rejected,
+  }, {
+    code: "a {\n\n  color: pink;\n\n  @mixin foo;\n}",
+    message: messages.rejected,
+  }],
+}, sharedAlwaysTests, mergeCustomizer))
+
+testRule(rule, _.mergeWith({
+  ruleName: ruleName,
+  config: ["never"],
+
+  accept: [{
+    code: "a {\n  @mixin foo;\n}",
+  }],
+}, sharedNeverTests, mergeCustomizer))
+
+testRule(rule, _.mergeWith({
+  ruleName: ruleName,
+  config: ["never", { except: ["blockless-group"] }],
+
+  accept: [{
+    code: "a {\n  @mixin foo;\n}",
+  }, {
+    code: "@keyframes foo {}\n@import 'x.css'",
+    description: "no empty line not blockless pair",
+  }, {
+    code: "@import 'x.css';\n\n@import 'y.css'",
+    description: "empty line blockless pair",
+  }, {
+    code: "@import 'x.css';",
+    description: "single blockless rule",
+  }],
+
+  reject: [{
+    code: "@keyframes foo {}\n\n@import 'x.css'",
+    message: messages.rejected,
+  }, {
+    code: "@import 'x.css';\n@import 'y.css'",
+    message: messages.expected,
+  }],
+}, sharedNeverTests, mergeCustomizer))
+
+testRule(rule, _.mergeWith({
+  ruleName: ruleName,
+  config: ["never", { except: ["all-nested"] }],
+
+  accept: [{
+    code: "a {\n\n  @mixin foo;\n  color: pink;\n}",
+  }, {
+    code: "a {\n  color: pink;\n\n  @mixin foo;\n}",
+  }],
+
+  reject: [{
+    code: "a {\n  @mixin foo;\n  color: pink;\n}",
+    message: messages.expected,
+  }, {
+    code: "a {\n\n  color: pink;\n  @mixin foo;\n}",
+    message: messages.expected,
+  }],
+}, sharedNeverTests, mergeCustomizer))
+
+testRule(rule, _.mergeWith({
+  ruleName: ruleName,
+  config: ["never", { except: ["first-nested"] }],
+
+  accept: [{
+    code: "a {\n\n  @mixin foo;\n  color: pink;\n}",
+  }],
+
+  reject: [{
+    code: "a {\n  color: pink;\n\n  @mixin foo;\n}",
+    message: messages.rejected,
+  }, {
+    code: "a {\n  @mixin foo;\n  color: pink;\n}",
+    message: messages.expected,
+  }],
+}, sharedNeverTests, mergeCustomizer))
+
+testRule(rule, {
+  ruleName: ruleName,
+  config: ["never", { ignore: ["after-comment"] }],
+
+  accept: [{
+    code: "/* foo */\n@media {}",
+  }, {
+    code: "/* foo */\r\na@media {}",
+    description: "CRLF",
+  }, {
+    code: "/* foo */\n\n@media {}",
+  }],
+
+  reject: [{
+    code: "b {}\n\n@media {}",
+    message: messages.rejected,
+  }, {
+    code: "b {}\r\n\r\n@media {}",
+    description: "CRLF",
+    message: messages.rejected,
+  }],
 })
 
-testRule("never", { except: ["all-nested"] }, tr => {
-  sharedNeverTests(tr)
+testRule(rule, _.mergeWith({
+  ruleName: ruleName,
+  config: ["never", { ignore: ["all-nested"] }],
 
-  tr.ok("a {\n\n  @mixin foo;\n  color: pink;\n}")
-  tr.ok("a {\n  color: pink;\n\n  @mixin foo;\n}")
+  accept: [{
+    code: "a {\n  @mixin foo;\n  color: pink;\n}",
+  }, {
+    code: "a {\n  color: pink;\n  @mixin foo;\n}",
+  }, {
+    code: "a {\n\n  @mixin foo;\n  color: pink;\n}",
+  }, {
+    code: "a {\n\n  color: pink;\n\n  @mixin foo;\n}",
+  }],
+}, sharedNeverTests, mergeCustomizer))
 
-  tr.notOk("a {\n  @mixin foo;\n  color: pink;\n}", messages.expected)
-  tr.notOk("a {\n\n  color: pink;\n  @mixin foo;\n}", messages.expected)
-})
-
-testRule("never", { except: ["first-nested"] }, tr => {
-  sharedNeverTests(tr)
-
-  tr.ok("a {\n\n  @mixin foo;\n  color: pink;\n}")
-
-  tr.notOk("a {\n  color: pink;\n\n  @mixin foo;\n}", messages.rejected)
-  tr.notOk("a {\n  @mixin foo;\n  color: pink;\n}", messages.expected)
-})
-
-testRule("never", { ignore: ["after-comment"] }, tr => {
-  tr.ok("/* foo */\n@media {}")
-  tr.ok("/* foo */\r\na@media {}", "CRLF")
-  tr.ok("/* foo */\n\n@media {}")
-
-  tr.notOk("b {}\n\n@media {}", messages.rejected)
-  tr.notOk("b {}\r\n\r\n@media {}", messages.rejected, "CRLF")
-})
-
-testRule("never", { ignore: ["all-nested"] }, tr => {
-  sharedNeverTests(tr)
-
-  tr.ok("a {\n  @mixin foo;\n  color: pink;\n}")
-  tr.ok("a {\n  color: pink;\n  @mixin foo;\n}")
-  tr.ok("a {\n\n  @mixin foo;\n  color: pink;\n}")
-  tr.ok("a {\n\n  color: pink;\n\n  @mixin foo;\n}")
-})
+function mergeCustomizer(objValue, srcValue) {
+  if (_.isArray(objValue, mergeCustomizer)) {
+    return objValue.concat(srcValue)
+  }
+}
