717e903f329b7045946608cd897ecb8b271a262e
David Clark <david.dave.clark@gmail.com>
2016-03-15 07:21:33 -0700

    test-rules command

--- src/rules/declaration-block-properties-order/__tests__/index.js
+++
@@ -1,4 +0,0 @@
-import "./alphabetical"
-import "./flat"
-import "./grouped-flexible"
-import "./grouped-strict"
--- src/rules/indentation/__tests__/index.js
+++
@@ -1,6 +0,0 @@
-import "./rules"
-import "./at-rules"
-import "./selectors"
-import "./comments"
-import "./hierarchical"
-import "./functions"
