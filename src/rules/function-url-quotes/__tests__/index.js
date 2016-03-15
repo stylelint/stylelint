932954df40919e3dafd948513823121cf5694e71
David Clark <david.dave.clark@gmail.com>
2016-03-14 19:50:07 -0700

    Migrate f* tests

--- src/rules/function-url-quotes/__tests__/index.js
+++ src/rules/function-url-quotes/__tests__/index.js
@@ -1,323 +1,249 @@
-import {
-  ruleTester,
-  warningFreeBasics,
-} from "../../../testUtils"
+/* eslint-disable comma-dangle,array-bracket-spacing */
+import testRule from "../../../testUtils/blueTapeStylelintAssert"
 import rule, { ruleName, messages } from ".."
 
-const testRule = ruleTester(rule, ruleName)
+testRule(rule, {
+  ruleName: ruleName,
+  config: ["double"],
 
-testRule("double", tr => {
-  warningFreeBasics(tr)
+  accept: [{
+    code: "@import url(\"foo.css\");",
 
-  // at-rules
-  tr.ok("@import url(\"foo.css\");", {
+    description: {
+      message: messages.expected("double quotes"),
+      line: 1,
+      column: 14,
+    },
+  }, {
+    code: "@document url(\"http://www.w3.org/\");",
+  }, {
+    code: "@document url-prefix(\"http://www.w3.org/\");",
+  }, {
+    code: "@document domain(\"http://www.w3.org/\");",
+  }, {
+    code: "@font-face { font-family: 'foo'; src: url(\"foo.ttf\"); }",
+  }, {
+    code: "a { background: url(\"foo.css\"); }",
+  }, {
+    code: "a { cursor: url(\"foo.png\"); }",
+  }, {
+    code: "a { background-image: url(\"foo.css\"), url(\"bar.css\"), url(\"baz.css\"); }",
+  }],
+
+  reject: [{
+    code: "@import url('foo.css');",
     message: messages.expected("double quotes"),
     line: 1,
-    column: 14,
-  })
-  tr.notOk(
-    "@import url('foo.css');",
-    {
-      message: messages.expected("double quotes"),
-      line: 1,
-      column: 13,
-    }
-  )
-  tr.notOk(
-    "@import url(foo.css);",
-    {
-      message: messages.expected("double quotes"),
-      line: 1,
-      column: 13,
-    }
-  )
-
-  tr.ok("@document url(\"http://www.w3.org/\");")
-  tr.ok("@document url-prefix(\"http://www.w3.org/\");")
-  tr.ok("@document domain(\"http://www.w3.org/\");")
-  tr.notOk(
-    "@document url('http://www.w3.org/');",
-    {
-      message: messages.expected("double quotes"),
-      line: 1,
-      column: 15,
-    }
-  )
-  tr.notOk(
-    "@document url-prefix(http://www.w3.org/Style);",
-    {
-      message: messages.expected("double quotes", "url-prefix"),
-      line: 1,
-      column: 22,
-    }
-  )
-  tr.notOk(
-    "@document domain('mozilla.org');",
-    {
-      message: messages.expected("double quotes", "domain"),
-      line: 1,
-      column: 18,
-    }
-  )
-
-  tr.ok("@font-face { font-family: 'foo'; src: url(\"foo.ttf\"); }")
-  tr.notOk(
-    "@font-face { font-family: 'foo'; src: url(foo.ttf); }",
-    {
-      message: messages.expected("double quotes"),
-      line: 1,
-      column: 43,
-    }
-  )
-
-  // rules
-  tr.ok("a { background: url(\"foo.css\"); }")
-  tr.notOk(
-    "a { background: url('foo.css'); }",
-    {
-      message: messages.expected("double quotes"),
-      line: 1,
-      column: 21,
-    }
-  )
-  tr.ok("a { cursor: url(\"foo.png\"); }")
-  tr.notOk(
-    "a { cursor: url(foo.png); }",
-    {
-      message: messages.expected("double quotes"),
-      line: 1,
-      column: 17,
-    }
-  )
-  tr.ok("a { background-image: url(\"foo.css\"), url(\"bar.css\"), url(\"baz.css\"); }")
-  tr.notOk(
-    "a { background-image: url(foo.css), url(\"bar.css\"), url(\"baz.css\"); }",
-    {
-      message: messages.expected("double quotes"),
-      line: 1,
-      column: 27,
-    }
-  )
-  tr.notOk(
-    "a { background-image: url(\"foo.css\"), url(bar.css), url(\"baz.css\"); }",
-    {
-      message: messages.expected("double quotes"),
-      line: 1,
-      column: 43,
-    }
-  )
-  tr.notOk(
-    "a { background-image: url(\"foo.css\"), url(\"bar.css\"), url(baz.css); }",
-    {
-      message: messages.expected("double quotes"),
-      line: 1,
-      column: 59,
-    }
-  )
+    column: 13,
+  }, {
+    code: "@import url(foo.css);",
+    message: messages.expected("double quotes"),
+    line: 1,
+    column: 13,
+  }, {
+    code: "@document url('http://www.w3.org/');",
+    message: messages.expected("double quotes"),
+    line: 1,
+    column: 15,
+  }, {
+    code: "@document url-prefix(http://www.w3.org/Style);",
+    message: messages.expected("double quotes", "url-prefix"),
+    line: 1,
+    column: 22,
+  }, {
+    code: "@document domain('mozilla.org');",
+    message: messages.expected("double quotes", "domain"),
+    line: 1,
+    column: 18,
+  }, {
+    code: "@font-face { font-family: 'foo'; src: url(foo.ttf); }",
+    message: messages.expected("double quotes"),
+    line: 1,
+    column: 43,
+  }, {
+    code: "a { background: url('foo.css'); }",
+    message: messages.expected("double quotes"),
+    line: 1,
+    column: 21,
+  }, {
+    code: "a { cursor: url(foo.png); }",
+    message: messages.expected("double quotes"),
+    line: 1,
+    column: 17,
+  }, {
+    code: "a { background-image: url(foo.css), url(\"bar.css\"), url(\"baz.css\"); }",
+    message: messages.expected("double quotes"),
+    line: 1,
+    column: 27,
+  }, {
+    code: "a { background-image: url(\"foo.css\"), url(bar.css), url(\"baz.css\"); }",
+    message: messages.expected("double quotes"),
+    line: 1,
+    column: 43,
+  }, {
+    code: "a { background-image: url(\"foo.css\"), url(\"bar.css\"), url(baz.css); }",
+    message: messages.expected("double quotes"),
+    line: 1,
+    column: 59,
+  }],
 })
 
-testRule("single", tr => {
-  warningFreeBasics(tr)
+testRule(rule, {
+  ruleName: ruleName,
+  config: ["single"],
 
-  // at-rules
-  tr.ok("@import url('foo.css');")
-  tr.notOk("@import url(\"foo.css\");", {
+  accept: [{
+    code: "@import url('foo.css');",
+  }, {
+    code: "@document url('http://www.w3.org/');",
+  }, {
+    code: "@document url-prefix('http://www.w3.org/');",
+  }, {
+    code: "@document domain('http://www.w3.org/');",
+  }, {
+    code: "@font-face { font-family: 'foo'; src: url('foo.ttf'); }",
+  }, {
+    code: "a { background: url('foo.css'); }",
+  }, {
+    code: "a { cursor: url('foo.png'); }",
+  }, {
+    code: "a { background-image: url('foo.css'), url('bar.css'), url('baz.css'); }",
+  }],
+
+  reject: [{
+    code: "@import url(\"foo.css\");",
     message: messages.expected("single quotes"),
     line: 1,
     column: 13,
-  })
-  tr.notOk("@import url(foo.css);", {
+  }, {
+    code: "@import url(foo.css);",
     message: messages.expected("single quotes"),
     line: 1,
     column: 13,
-  })
-
-  tr.ok("@document url('http://www.w3.org/');")
-  tr.ok("@document url-prefix('http://www.w3.org/');")
-  tr.ok("@document domain('http://www.w3.org/');")
-  tr.notOk(
-    "@document url(\"http://www.w3.org/\");",
-    {
-      message: messages.expected("single quotes"),
-      line: 1,
-      column: 15,
-    }
-  )
-  tr.notOk(
-    "@document url-prefix(http://www.w3.org/Style);",
-    {
-      message: messages.expected("single quotes", "url-prefix"),
-      line: 1,
-      column: 22,
-    }
-  )
-  tr.notOk(
-    "@document domain(\"mozilla.org\");",
-    {
-      message: messages.expected("single quotes", "domain"),
-      line: 1,
-      column: 18,
-    }
-  )
-
-  tr.ok("@font-face { font-family: 'foo'; src: url('foo.ttf'); }")
-  tr.notOk(
-    "@font-face { font-family: 'foo'; src: url(foo.ttf); }",
-    {
-      message: messages.expected("single quotes"),
-      line: 1,
-      column: 43,
-    }
-  )
-
-  // rules
-  tr.ok("a { background: url('foo.css'); }")
-  tr.notOk(
-    "a { background: url(\"foo.css\"); }",
-    {
-      message: messages.expected("single quotes"),
-      line: 1,
-      column: 21,
-    }
-  )
-  tr.ok("a { cursor: url('foo.png'); }")
-  tr.notOk(
-    "a { cursor: url(foo.png); }",
-    {
-      message: messages.expected("single quotes"),
-      line: 1,
-      column: 17,
-    }
-  )
-  tr.ok("a { background-image: url('foo.css'), url('bar.css'), url('baz.css'); }")
-  tr.notOk(
-    "a { background-image: url(foo.css), url('bar.css'), url('baz.css'); }",
-    {
-      message: messages.expected("single quotes"),
-      line: 1,
-      column: 27,
-    }
-  )
-  tr.notOk(
-    "a { background-image: url('foo.css'), url(bar.css), url('baz.css'); }",
-    {
-      message: messages.expected("single quotes"),
-      line: 1,
-      column: 43,
-    }
-  )
-  tr.notOk(
-    "a { background-image: url('foo.css'), url('bar.css'), url(baz.css); }",
-    {
-      message: messages.expected("single quotes"),
-      line: 1,
-      column: 59,
-    }
-  )
+  }, {
+    code: "@document url(\"http://www.w3.org/\");",
+    message: messages.expected("single quotes"),
+    line: 1,
+    column: 15,
+  }, {
+    code: "@document url-prefix(http://www.w3.org/Style);",
+    message: messages.expected("single quotes", "url-prefix"),
+    line: 1,
+    column: 22,
+  }, {
+    code: "@document domain(\"mozilla.org\");",
+    message: messages.expected("single quotes", "domain"),
+    line: 1,
+    column: 18,
+  }, {
+    code: "@font-face { font-family: 'foo'; src: url(foo.ttf); }",
+    message: messages.expected("single quotes"),
+    line: 1,
+    column: 43,
+  }, {
+    code: "a { background: url(\"foo.css\"); }",
+    message: messages.expected("single quotes"),
+    line: 1,
+    column: 21,
+  }, {
+    code: "a { cursor: url(foo.png); }",
+    message: messages.expected("single quotes"),
+    line: 1,
+    column: 17,
+  }, {
+    code: "a { background-image: url(foo.css), url('bar.css'), url('baz.css'); }",
+    message: messages.expected("single quotes"),
+    line: 1,
+    column: 27,
+  }, {
+    code: "a { background-image: url('foo.css'), url(bar.css), url('baz.css'); }",
+    message: messages.expected("single quotes"),
+    line: 1,
+    column: 43,
+  }, {
+    code: "a { background-image: url('foo.css'), url('bar.css'), url(baz.css); }",
+    message: messages.expected("single quotes"),
+    line: 1,
+    column: 59,
+  }],
 })
 
-testRule("none", tr => {
-  warningFreeBasics(tr)
+testRule(rule, {
+  ruleName: ruleName,
+  config: ["none"],
 
-  // at-rules
-  tr.ok("@import url(foo.css);")
-  tr.notOk(
-    "@import url(\"foo.css\");",
-    {
-      message: messages.expected("no quotes"),
-      line: 1,
-      column: 13,
-    }
-  )
-  tr.notOk(
-    "@import url('foo.css');",
-    {
-      message: messages.expected("no quotes"),
-      line: 1,
-      column: 13,
-    }
-  )
+  accept: [{
+    code: "@import url(foo.css);",
+  }, {
+    code: "@document url(http://www.w3.org/);",
+  }, {
+    code: "@document url-prefix(http://www.w3.org/);",
+  }, {
+    code: "@document domain(http://www.w3.org/);",
+  }, {
+    code: "@font-face { font-family: foo; src: url(foo.ttf); }",
+  }, {
+    code: "a { background: url(foo.css); }",
+  }, {
+    code: "a { cursor: url(foo.png); }",
+  }, {
+    code: "a { background-image: url(foo.css), url(bar.css), url(baz.css); }",
+  }],
 
-  tr.ok("@document url(http://www.w3.org/);")
-  tr.ok("@document url-prefix(http://www.w3.org/);")
-  tr.ok("@document domain(http://www.w3.org/);")
-  tr.notOk(
-    "@document url(\"http://www.w3.org/\");",
-    {
-      message: messages.expected("no quotes"),
-      line: 1,
-      column: 15,
-    }
-  )
-  tr.notOk(
-    "@document url-prefix('http://www.w3.org/Style');",
-    {
-      message: messages.expected("no quotes", "url-prefix"),
-      line: 1,
-      column: 22,
-    }
-  )
-  tr.notOk(
-    "@document domain(\"mozilla.org\");",
-    {
-      message: messages.expected("no quotes", "domain"),
-      line: 1,
-      column: 18,
-    }
-  )
-
-  tr.ok("@font-face { font-family: foo; src: url(foo.ttf); }")
-  tr.notOk(
-    "@font-face { font-family: foo; src: url('foo.ttf'); }",
-    {
-      message: messages.expected("no quotes"),
-      line: 1,
-      column: 41,
-    }
-  )
-
-  // rules
-  tr.ok("a { background: url(foo.css); }")
-  tr.notOk(
-    "a { background: url(\"foo.css\"); }",
-    {
-      message: messages.expected("no quotes"),
-      line: 1,
-      column: 21,
-    }
-  )
-  tr.ok("a { cursor: url(foo.png); }")
-  tr.notOk(
-    "a { cursor: url(\"foo.png\"); }",
-    {
-      message: messages.expected("no quotes"),
-      line: 1,
-      column: 17,
-    }
-  )
-  tr.ok("a { background-image: url(foo.css), url(bar.css), url(baz.css); }")
-  tr.notOk(
-    "a { background-image: url('foo.css'), url(bar.css), url(baz.css); }",
-    {
-      message: messages.expected("no quotes"),
-      line: 1,
-      column: 27,
-    }
-  )
-  tr.notOk(
-    "a { background-image: url(foo.css), url('bar.css'), url(baz.css); }",
-    {
-      message: messages.expected("no quotes"),
-      line: 1,
-      column: 41,
-    }
-  )
-  tr.notOk(
-    "a { background-image: url(foo.css), url(bar.css), url('baz.css'); }",
-    {
-      message: messages.expected("no quotes"),
-      line: 1,
-      column: 55,
-    }
-  )
+  reject: [{
+    code: "@import url(\"foo.css\");",
+    message: messages.expected("no quotes"),
+    line: 1,
+    column: 13,
+  }, {
+    code: "@import url('foo.css');",
+    message: messages.expected("no quotes"),
+    line: 1,
+    column: 13,
+  }, {
+    code: "@document url(\"http://www.w3.org/\");",
+    message: messages.expected("no quotes"),
+    line: 1,
+    column: 15,
+  }, {
+    code: "@document url-prefix('http://www.w3.org/Style');",
+    message: messages.expected("no quotes", "url-prefix"),
+    line: 1,
+    column: 22,
+  }, {
+    code: "@document domain(\"mozilla.org\");",
+    message: messages.expected("no quotes", "domain"),
+    line: 1,
+    column: 18,
+  }, {
+    code: "@font-face { font-family: foo; src: url('foo.ttf'); }",
+    message: messages.expected("no quotes"),
+    line: 1,
+    column: 41,
+  }, {
+    code: "a { background: url(\"foo.css\"); }",
+    message: messages.expected("no quotes"),
+    line: 1,
+    column: 21,
+  }, {
+    code: "a { cursor: url(\"foo.png\"); }",
+    message: messages.expected("no quotes"),
+    line: 1,
+    column: 17,
+  }, {
+    code: "a { background-image: url('foo.css'), url(bar.css), url(baz.css); }",
+    message: messages.expected("no quotes"),
+    line: 1,
+    column: 27,
+  }, {
+    code: "a { background-image: url(foo.css), url('bar.css'), url(baz.css); }",
+    message: messages.expected("no quotes"),
+    line: 1,
+    column: 41,
+  }, {
+    code: "a { background-image: url(foo.css), url(bar.css), url('baz.css'); }",
+    message: messages.expected("no quotes"),
+    line: 1,
+    column: 55,
+  }],
 })
--- src/rules/function-whitespace-after/__tests__/index.js
+++ src/rules/function-whitespace-after/__tests__/index.js
@@ -1,109 +1,136 @@
-import {
-  ruleTester,
-  warningFreeBasics,
-} from "../../../testUtils"
+/* eslint-disable comma-dangle,array-bracket-spacing */
+import testRule from "../../../testUtils/blueTapeStylelintAssert"
 import rule, { ruleName, messages } from ".."
-import scssSyntax from "postcss-scss"
 
-const testRule = ruleTester(rule, ruleName)
+testRule(rule, {
+  ruleName: ruleName,
+  config: ["always"],
 
-testRule("always", tr => {
-  warningFreeBasics(tr)
+  accept: [{
+    code: "a::before { content: \"var(--hoot)color(blue)\"; }",
+  }, {
+    code: "a::before { background: url('var(--hoot)color(blue)'); }",
+  }, {
+    code: "a::before { content: attr(data-foo); }",
+  }, {
+    code: "a { transform: translate(1, 1); }",
+  }, {
+    code: "a { transform: translate(1, 1) }",
+  }, {
+    code: "a { transform: translate(1, 1)}",
+  }, {
+    code: "a { transform: translate(1, 1) scale(3); }",
+  }, {
+    code: "a { color: color(rgb(0,0,0) lightness(50%)) };",
+  }, {
+    code: "a { background-image: linear-gradient(#f3c, #4ec), linear-gradient(#f3c, #4ec); }",
+    description: "multiple comma-separated functions ",
+  }, {
+    code: "a { border-color: color(rgb(0,0,0) lightness(50%)) red pink orange; }",
+    description: "function within a function as one of multiple space-separated values",
+  }, {
+    code: "a { transform: translate(1, 1)  scale(3); }",
+  }, {
+    code: "a { transform: translate(1, 1)\nscale(3); }",
+  }, {
+    code: "a { transform: translate(1, 1)\r\nscale(3); }",
+  }, {
+    code: "a { color: color(rgb(0,0,0)  lightness(50%)) };",
+  }, {
+    code: "a { color: color(rgb(0,0,0)\nlightness(50%)) };",
+  }, {
+    code: "a { color: color(rgb(0,0,0)\r\nlightness(50%)) };",
+  }],
 
-  tr.ok("a::before { content: \"var(--hoot)color(blue)\"; }")
-  tr.ok("a::before { background: url('var(--hoot)color(blue)'); }")
-  tr.ok("a::before { content: attr(data-foo); }")
-  tr.ok("a { transform: translate(1, 1); }")
-  tr.ok("a { transform: translate(1, 1) }")
-  tr.ok("a { transform: translate(1, 1)}")
-  tr.ok("a { transform: translate(1, 1) scale(3); }")
-  tr.ok("a { color: color(rgb(0,0,0) lightness(50%)) };")
-  tr.ok("a { background-image: linear-gradient(#f3c, #4ec), linear-gradient(#f3c, #4ec); }",
-    "multiple comma-separated functions ")
-  tr.ok("a { border-color: color(rgb(0,0,0) lightness(50%)) red pink orange; }",
-    "function within a function as one of multiple space-separated values")
-
-  tr.ok("a { transform: translate(1, 1)  scale(3); }")
-  tr.ok("a { transform: translate(1, 1)\nscale(3); }")
-  tr.ok("a { transform: translate(1, 1)\r\nscale(3); }")
-  tr.ok("a { color: color(rgb(0,0,0)  lightness(50%)) };")
-  tr.ok("a { color: color(rgb(0,0,0)\nlightness(50%)) };")
-  tr.ok("a { color: color(rgb(0,0,0)\r\nlightness(50%)) };")
-
-  tr.notOk("a { transform: translate(1, 1)scale(3); }", {
+  reject: [{
+    code: "a { transform: translate(1, 1)scale(3); }",
     message: messages.expected,
     line: 1,
     column: 31,
-  })
-  tr.notOk("a { color: color(rgb(0,0,0)lightness(50%)) };", {
+  }, {
+    code: "a { color: color(rgb(0,0,0)lightness(50%)) };",
     message: messages.expected,
     line: 1,
     column: 28,
-  })
+  }],
 })
 
-testRule("never", tr => {
-  warningFreeBasics(tr)
+testRule(rule, {
+  ruleName: ruleName,
+  config: ["never"],
 
-  tr.ok("a::before { content: \"var(--hoot) color(blue)\"; }")
-  tr.ok("a::before { background: url('var(--hoot) color(blue)'); }")
-  tr.ok("a::before { content: attr(data-foo); }")
-  tr.ok("a { transform: translate(1, 1); }")
-  tr.ok("a { transform: translate(1, 1) }")
-  tr.ok("a { transform: translate(1, 1)}")
-  tr.ok("a { transform: translate(1, 1)scale(3); }")
-  tr.ok("a { color: color(rgb(0,0,0)lightness(50%)) };")
+  accept: [{
+    code: "a::before { content: \"var(--hoot) color(blue)\"; }",
+  }, {
+    code: "a::before { background: url('var(--hoot) color(blue)'); }",
+  }, {
+    code: "a::before { content: attr(data-foo); }",
+  }, {
+    code: "a { transform: translate(1, 1); }",
+  }, {
+    code: "a { transform: translate(1, 1) }",
+  }, {
+    code: "a { transform: translate(1, 1)}",
+  }, {
+    code: "a { transform: translate(1, 1)scale(3); }",
+  }, {
+    code: "a { color: color(rgb(0,0,0)lightness(50%)) };",
+  }],
 
-  tr.notOk("a { transform: translate(1, 1) scale(3); }", {
+  reject: [{
+    code: "a { transform: translate(1, 1) scale(3); }",
     message: messages.rejected,
     line: 1,
     column: 31,
-  })
-  tr.notOk("a { transform: translate(1, 1)  scale(3); }", {
+  }, {
+    code: "a { transform: translate(1, 1)  scale(3); }",
     message: messages.rejected,
     line: 1,
     column: 31,
-  })
-  tr.notOk("a { transform: translate(1, 1)\nscale(3); }", {
+  }, {
+    code: "a { transform: translate(1, 1)\nscale(3); }",
     message: messages.rejected,
     line: 1,
     column: 31,
-  })
-  tr.notOk("a { transform: translate(1, 1)\r\nscale(3); }", {
+  }, {
+    code: "a { transform: translate(1, 1)\r\nscale(3); }",
+    description: "CRLF",
     message: messages.rejected,
     line: 1,
     column: 31,
-  }, "CRLF")
-  tr.notOk("a { color: color(rgb(0,0,0) lightness(50%)) };", {
+  }, {
+    code: "a { color: color(rgb(0,0,0) lightness(50%)) };",
     message: messages.rejected,
     line: 1,
     column: 28,
-  })
-  tr.notOk("a { color: color(rgb(0,0,0)  lightness(50%)) };", {
+  }, {
+    code: "a { color: color(rgb(0,0,0)  lightness(50%)) };",
     message: messages.rejected,
     line: 1,
     column: 28,
-  })
-  tr.notOk("a { color: color(rgb(0,0,0)\nlightness(50%)) };", {
+  }, {
+    code: "a { color: color(rgb(0,0,0)\nlightness(50%)) };",
     message: messages.rejected,
     line: 1,
     column: 28,
-  })
+  }],
 })
 
-const testRuleScss = ruleTester(rule, ruleName, {
-  postcssOptions: {
-    syntax: scssSyntax,
-  },
-})
+testRule(rule, {
+  ruleName: ruleName,
+  config: ["always"],
+  skipBasicChecks: true,
+  syntax: "scss",
 
-testRuleScss("always", tr => {
-  tr.ok("h1 { max-height: #{($line-height) * ($lines-to-show)}em; }",
-    "Sass-style interpolation with curly braces")
+  accept: [{
+    code: "h1 { max-height: #{($line-height) * ($lines-to-show)}em; }",
+    description: "Sass-style interpolation with curly braces",
+  }],
 
-  tr.notOk("a { padding:\n  10px\n  // comment one\n  // comment two\n  var(--boo)orange}", {
+  reject: [{
+    code: "a { padding:\n  10px\n  // comment one\n  // comment two\n  var(--boo)orange}",
     message: messages.expected,
     line: 5,
     column: 13,
-  })
+  }],
 })
