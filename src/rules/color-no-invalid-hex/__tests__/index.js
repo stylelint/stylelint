e2d1e1e2c7085f0cb1ae7d4798909788d8f5252b
David Clark <david.dave.clark@gmail.com>
2016-03-14 19:37:24 -0700

    Migrate color-* tests

--- src/rules/color-no-invalid-hex/__tests__/index.js
+++ src/rules/color-no-invalid-hex/__tests__/index.js
@@ -1,55 +1,63 @@
-import {
-  ruleTester,
-  warningFreeBasics,
-} from "../../../testUtils"
+/* eslint-disable comma-dangle,array-bracket-spacing */
+import testRule from "../../../testUtils/blueTapeStylelintAssert"
 import rule, { ruleName, messages } from ".."
 
-const testRule = ruleTester(rule, ruleName)
+testRule(rule, {
+  ruleName: ruleName,
+  config: [undefined],
 
-testRule(undefined, tr => {
-  warningFreeBasics(tr)
+  accept: [{
+    code: "a { color: pink; }",
+  }, {
+    code: "a { color: #000; }",
+  }, {
+    code: "a { something: #000, #fff, #ababab; }",
+  }, {
+    code: "a { color: #0000ffcc; }",
+    description: "eight digits",
+  }, {
+    code: "a { color:#00fc; }",
+    description: "four digits",
+  }, {
+    code: "a { padding: 000; }",
+  }, {
+    code: "a::before { content: \"#ababa\"; }",
+  }, {
+    code: "a { border-#$side: 0; }",
+    description: "ignore sass-like interpolation",
+  }, {
+    code: "a { box-sizing: #$type-box; }",
+    description: "ignore sass-like interpolation",
+  }, {
+    code: "@font-face {\n" +
+      "font-family: dashicons;\n" +
+      "src: url(data:application/font-woff;charset=utf-8;base64, ABCDEF==) format(\"woff\"),\n" +
+          "url(../fonts/dashicons.ttf) format(\"truetype\"),\n" +
+          "url(../fonts/dashicons.svg#dashicons) format(\"svg\");\n" +
+      "font-weight: normal;\n" +
+      "font-style: normal;\n" +
+    "}",
+  }],
 
-  tr.ok("a { color: pink; }")
-  tr.ok("a { color: #000; }")
-  tr.ok("a { something: #000, #fff, #ababab; }")
-  tr.ok("a { color: #0000ffcc; }", "eight digits")
-  tr.ok("a { color:#00fc; }", "four digits")
-
-  tr.ok("a { padding: 000; }")
-  tr.ok("a::before { content: \"#ababa\"; }")
-
-  tr.ok("a { border-#$side: 0; }", "ignore sass-like interpolation")
-  tr.ok("a { box-sizing: #$type-box; }", "ignore sass-like interpolation")
-
-  tr.notOk("a { color: #ababa; }", {
+  reject: [{
+    code: "a { color: #ababa; }",
     message: messages.rejected("#ababa"),
     line: 1,
     column: 12,
-  })
-  tr.notOk("a { something: #00, #fff, #ababab; }", {
+  }, {
+    code: "a { something: #00, #fff, #ababab; }",
     message: messages.rejected("#00"),
     line: 1,
     column: 16,
-  })
-  tr.notOk("a { something: #000, #fff1az, #ababab; }", {
+  }, {
+    code: "a { something: #000, #fff1az, #ababab; }",
     message: messages.rejected("#fff1az"),
     line: 1,
     column: 22,
-  })
-
-  // No supplementary spaces after colon or comma
-  tr.notOk("a { something:#000,#fff,#12345aa; }", {
+  }, {
+    code: "a { something:#000,#fff,#12345aa; }",
     message: messages.rejected("#12345aa"),
     line: 1,
     column: 25,
-  })
-
-  tr.ok("@font-face {\n" +
-    "font-family: dashicons;\n" +
-    "src: url(data:application/font-woff;charset=utf-8;base64, ABCDEF==) format(\"woff\"),\n" +
-        "url(../fonts/dashicons.ttf) format(\"truetype\"),\n" +
-        "url(../fonts/dashicons.svg#dashicons) format(\"svg\");\n" +
-    "font-weight: normal;\n" +
-    "font-style: normal;\n" +
-  "}")
+  }],
 })
