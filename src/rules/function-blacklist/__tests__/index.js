f7f035f0c090a9d13a95a05e72f76c94a395266b
David Clark <david.dave.clark@gmail.com>
2016-03-14 19:50:07 -0700

    Migrate f* tests

--- src/rules/function-blacklist/__tests__/index.js
+++ src/rules/function-blacklist/__tests__/index.js
@@ -1,67 +1,72 @@
-import {
-  ruleTester,
-  warningFreeBasics,
-} from "../../../testUtils"
+/* eslint-disable comma-dangle,array-bracket-spacing */
+import testRule from "../../../testUtils/blueTapeStylelintAssert"
 import rule, { ruleName, messages } from ".."
 
-const testRule = ruleTester(rule, ruleName)
+testRule(rule, {
+  ruleName: ruleName,
 
-testRule([
-  "rgba",
-  "scale",
-  "linear-gradient",
-], tr => {
-  warningFreeBasics(tr)
-  tr.ok("a { color: pink; }")
-  tr.ok("a { transform: rotate(7deg) }")
-  tr.ok("a { background: -webkit-radial-gradient(red, green, blue); }")
-  tr.ok("a { color: color(rgb(0, 0, 0) lightness(50%)); }")
-  tr.ok("@media (max-width: 10px) { a { color: color(rgb(0, 0, 0) lightness(50%)); } }")
+  config: [[
+    "rgba",
+    "scale",
+    "linear-gradient",
+  ]],
 
-  tr.notOk("a { transform: scale(1); }", {
+  accept: [{
+    code: "a { color: pink; }",
+  }, {
+    code: "a { transform: rotate(7deg) }",
+  }, {
+    code: "a { background: -webkit-radial-gradient(red, green, blue); }",
+  }, {
+    code: "a { color: color(rgb(0, 0, 0) lightness(50%)); }",
+  }, {
+    code: "@media (max-width: 10px) { a { color: color(rgb(0, 0, 0) lightness(50%)); } }",
+  }],
+
+  reject: [{
+    code: "a { transform: scale(1); }",
     message: messages.rejected("scale"),
     line: 1,
     column: 16,
-  })
-  tr.notOk("a { transform : scale(1); }", {
+  }, {
+    code: "a { transform : scale(1); }",
     message: messages.rejected("scale"),
     line: 1,
     column: 17,
-  })
-  tr.notOk("a\n{ transform: scale(1); }", {
+  }, {
+    code: "a\n{ transform: scale(1); }",
     message: messages.rejected("scale"),
     line: 2,
     column: 14,
-  })
-  tr.notOk("a { transform:    scale(1); }", {
+  }, {
+    code: "a { transform:    scale(1); }",
     message: messages.rejected("scale"),
     line: 1,
     column: 19,
-  })
-  tr.notOk("  a { transform: scale(1); }", {
+  }, {
+    code: "  a { transform: scale(1); }",
     message: messages.rejected("scale"),
     line: 1,
     column: 18,
-  })
-  tr.notOk("a { color: rgba(0, 0, 0, 0) }", {
+  }, {
+    code: "a { color: rgba(0, 0, 0, 0) }",
     message: messages.rejected("rgba"),
     line: 1,
     column: 12,
-  })
-  tr.notOk("a { color: color(rgba(0, 0, 0, 0) lightness(50%)); }", {
+  }, {
+    code: "a { color: color(rgba(0, 0, 0, 0) lightness(50%)); }",
     message: messages.rejected("rgba"),
     line: 1,
     column: 18,
-  })
-  tr.notOk("a { background: red, -moz-linear-gradient(45deg, blue, red); }", {
+  }, {
+    code: "a { background: red, -moz-linear-gradient(45deg, blue, red); }",
     message: messages.rejected("-moz-linear-gradient"),
     line: 1,
     column: 22,
-  })
-  tr.notOk("@media (max-width: 10px) { a { color: color(rgba(0, 0, 0) lightness(50%)); } }", {
+  }, {
+    code: "@media (max-width: 10px) { a { color: color(rgba(0, 0, 0) lightness(50%)); } }",
     message: messages.rejected("rgba"),
     line: 1,
     column: 45,
-  })
-
+  }],
 })
