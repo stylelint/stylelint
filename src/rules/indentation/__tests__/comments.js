dc124f14cd7458b0ca5bc8acf466c05a316edebf
David Clark <david.dave.clark@gmail.com>
2016-03-14 20:25:03 -0700

    Migrate indentation tests

--- src/rules/indentation/__tests__/comments.js
+++ src/rules/indentation/__tests__/comments.js
@@ -1,65 +1,51 @@
-/* eslint-disable indent, no-multiple-empty-lines */
-
-import {
-  ruleTester,
-  warningFreeBasics,
-} from "../../../testUtils"
+/* eslint-disable comma-dangle,array-bracket-spacing */
+import testRule from "../../../testUtils/blueTapeStylelintAssert"
 import rule, { ruleName, messages } from ".."
 
-const testRule = ruleTester(rule, ruleName)
+testRule(rule, {
+  ruleName: ruleName,
+  config: ["tab"],
 
-testRule("tab", tr => {
-warningFreeBasics(tr)
+  accept: [{
+    code: "/* blergh */",
+  }, {
+    code: ".foo {\n" +
+    "\t/* blergh */\n" +
+    "\ttop: 0;\n" +
+    "}",
+  }, {
+    code: "@media print {\n" +
+    "\t.foo {\n" +
+    "\t\t/* blergh */\n" +
+    "\t\ttop: 0;\n" +
+    "\t}\n" +
+    "}",
+  }],
 
-tr.ok(
-"/* blergh */")
+  reject: [{
+    code: " /* blergh */",
+    message: messages.expected("0 tabs"),
+    line: 1,
+    column: 2,
+  }, {
+    code: ".foo {\n" +
+    "\t\t/* blergh */\n" +
+    "\ttop: 0;\n" +
+    "}",
 
-tr.notOk(
-" /* blergh */",
-{
-  message: messages.expected("0 tabs"),
-  line: 1,
-  column: 2,
-})
+    message: messages.expected("1 tab"),
+    line: 2,
+    column: 3,
+  }, {
+    code: "@media print {\n" +
+    "\t.foo {\n" +
+    "\t/* blergh */\n" +
+    "\t\ttop: 0;\n" +
+    "\t}\n" +
+    "}",
 
-tr.ok(
-`.foo {
-\t/* blergh */
-\ttop: 0;
-}`
-)
-
-tr.notOk(
-`.foo {
-\t\t/* blergh */
-\ttop: 0;
-}`,
-{
-  message: messages.expected("1 tab"),
-  line: 2,
-  column: 3,
-})
-
-tr.ok(
-`@media print {
-\t.foo {
-\t\t/* blergh */
-\t\ttop: 0;
-\t}
-}`
-)
-
-tr.notOk(
-`@media print {
-\t.foo {
-\t/* blergh */
-\t\ttop: 0;
-\t}
-}`,
-{
-  message: messages.expected("2 tabs"),
-  line: 3,
-  column: 2,
-})
-
+    message: messages.expected("2 tabs"),
+    line: 3,
+    column: 2,
+  }],
 })
--- src/rules/indentation/__tests__/functions.js
+++ src/rules/indentation/__tests__/functions.js
@@ -1,73 +1,59 @@
-/* eslint-disable indent, no-multiple-empty-lines */
-
-import {
-  ruleTester,
-} from "../../../testUtils"
+/* eslint-disable comma-dangle,array-bracket-spacing */
+import testRule from "../../../testUtils/blueTapeStylelintAssert"
 import rule, { ruleName } from ".."
 
-const testRule = ruleTester(rule, ruleName)
+testRule(rule, {
+  ruleName: ruleName,
+  config: [2],
+  skipBasicChecks: true,
 
-// 2 spaces
-testRule(2, tr => {
-
-tr.ok(
-`.foo {
-  color: rgb(0, 0, 0);
-}`
-)
-
-tr.ok(
-`.foo {
-  color: rgb(
-    0,
-    0,
-    0
-  );
-}`
-)
-
-tr.ok(
-`.foo {
-  color: rgb(
-      0,
-      0,
-      0
-    );
-}`
-)
-
-tr.ok(
-`.foo {
-  color: rgb(
-0,
-0,
-0
-    );
-}`
-)
-
-tr.ok(
-`.foo {
-  color: bar(
-    rgb(
-      0,
-      0,
-      0
-    )
-  );
-}`
-)
-
-tr.ok(
-`.foo {
-  color: bar(
-      rgb(
-        0,
-        0,
-        0
-      )
-    );
-}`
-)
-
+  accept: [{
+    code: ".foo {\n" +
+    "  color: rgb(0, 0, 0);\n" +
+    "}",
+  }, {
+    code: ".foo {\n" +
+    "  color: rgb(\n" +
+    "    0,\n" +
+    "    0,\n" +
+    "    0\n" +
+    "  );\n" +
+    "}",
+  }, {
+    code: ".foo {\n" +
+    "  color: rgb(\n" +
+    "      0,\n" +
+    "      0,\n" +
+    "      0\n" +
+    "    );\n" +
+    "}",
+  }, {
+    code: ".foo {\n" +
+    "  color: rgb(\n" +
+    "0,\n" +
+    "0,\n" +
+    "0\n" +
+    "    );\n" +
+    "}",
+  }, {
+    code: ".foo {\n" +
+    "  color: bar(\n" +
+    "    rgb(\n" +
+    "      0,\n" +
+    "      0,\n" +
+    "      0\n" +
+    "    )\n" +
+    "  );\n" +
+    "}",
+  }, {
+    code: ".foo {\n" +
+    "  color: bar(\n" +
+    "      rgb(\n" +
+    "        0,\n" +
+    "        0,\n" +
+    "        0\n" +
+    "      )\n" +
+    "    );\n" +
+    "}",
+  }],
 })
