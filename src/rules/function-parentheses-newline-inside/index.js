62230bd3ee17d1501b09c146db10a14d6b64304a
Rouven Bühlmann <rouven.buehlmann@dreipol.ch>
2016-03-28 14:21:39 +0200

    fixed failing tests

--- src/rules/function-parentheses-newline-inside/index.js
+++ src/rules/function-parentheses-newline-inside/index.js
@@ -1,5 +1,6 @@
 import valueParser from "postcss-value-parser"
 import {
+  cssDeclarationIsMap,
   isSingleLineString,
   declarationValueIndexOffset,
   report,
@@ -31,6 +32,8 @@ export default function (expectation) {
     if (!validOptions) { return }
 
     root.walkDecls(decl => {
+      if (cssDeclarationIsMap(decl)) { return }
+
       if (decl.value.indexOf("(") === -1) { return }
 
       valueParser(decl.value).walk(valueNode => {
