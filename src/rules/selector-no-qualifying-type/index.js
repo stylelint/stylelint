c2e67c7141240a0baa9ad0f339d7ea50c5e1f5b4
Tim Johnson <timothyneiljohnson@gmail.com>
2016-03-29 08:51:55 -0500

    Update index.js

--- src/rules/selector-no-qualifying-type/index.js
+++ src/rules/selector-no-qualifying-type/index.js
@@ -1,107 +1,106 @@
-'use strict';
+"use strict"
 
 import {
   report,
   ruleMessages,
-  isAutoprefixable,
-  validateOptions
-} from '../../utils';
+  validateOptions,
+} from "../../utils"
 
-export const ruleName = 'selector-no-qualifying-type';
-export const messages = ruleMessages(ruleName, {});
+export const ruleName = "selector-no-qualifying-type"
+export const messages = ruleMessages(ruleName, {})
 
 export default (enabled, options) => {
   return (root, result) => {
     const validOptions = validateOptions(result, ruleName, {
       actual: enabled,
-        possible: [true, false]
+      possible: [ true, false ],
     }, {
       actual: options,
       possible: {
-        ignore: ['attribute', 'class', 'id']
+        ignore: [ "attribute", "class", "id" ],
       },
-      optional: true
-    });
+      optional: true,
+    })
 
     const isElementSelector = (selector) => {
-      const firstChar = selector.trim().substring(0, 1);
-      return /^[a-z]+$/i.test(firstChar);
-    };
+      const firstChar = selector.trim().substring(0, 1)
+      return /^[a-z]+$/i.test(firstChar)
+    }
 
     const hasAttributeQualifiedByElement = (selector) => {
-      const selectorArray = selector.split(' ');
-      let isFound = false;
+      const selectorArray = selector.split(" ")
+      let isFound = false
       selectorArray.forEach((value) => {
-        if (isElementSelector(value) && value.indexOf('[') > -1) {
-          isFound = true;
+        if (isElementSelector(value) && value.indexOf("[") > -1) {
+          isFound = true
         }
-      });
-      return isFound;
-    };
+      })
+      return isFound
+    }
 
     const hasClassQualifiedByElement = (selector) => {
-      const selectorArray = selector.split(' ');
-      let isFound = false;
+      const selectorArray = selector.split(" ")
+      let isFound = false
       selectorArray.forEach((value) => {
-        if (isElementSelector(value) && value.indexOf('.') > -1) {
-          isFound = true;
+        if (isElementSelector(value) && value.indexOf(".") > -1) {
+          isFound = true
         }
-      });
-      return isFound;
-    };
+      })
+      return isFound
+    }
 
     const hasIdQualifiedByElement = (selector) => {
-      const selectorArray = selector.split(' ');
-      let isFound = false;
+      const selectorArray = selector.split(" ")
+      let isFound = false
       selectorArray.forEach((value) => {
-        if (isElementSelector(value) && value.indexOf('#') > -1) {
-          isFound = true;
+        if (isElementSelector(value) && value.indexOf("#") > -1) {
+          isFound = true
         }
-      });
-      return isFound;
-    };
+      })
+      return isFound
+    }
 
     const checkForQualifyingElement = (rule) => {
       rule.selectors.forEach(selector => {
-        // Return early if there is interpolation in the selector, or rule is set to 'false'
+        // Return early if there is interpolation in the selector, or rule is set to "false"
         if (/#{.+?}|@{.+?}|\$\(.+?\)/.test(selector) || !enabled) {
-          return;
+          return
         }
 
         // Replace combinators with whitespace, as they not relevant to rule
-        const selectorNoCombinators = selector.replace(/>|\+|~/g, ' ');
+        const selectorNoCombinators = selector.replace(/>|\+|~/g, " ")
 
-        if (options.ignore === 'attribute' && hasAttributeQualifiedByElement(selectorNoCombinators)) {
+        if (options.ignore === "attribute" && hasAttributeQualifiedByElement(selectorNoCombinators)) {
           report({
-            ruleName: ruleName,
-            result: result,
+            ruleName,
+            result,
             node: rule,
-            message: 'Avoid qualifying attribute selectors with an element'
-          });
+            message: "Avoid qualifying attribute selectors with an element",
+          })
         }
-        if (options.ignore === 'class' && hasClassQualifiedByElement(selectorNoCombinators)) {
+        if (options.ignore === "class" && hasClassQualifiedByElement(selectorNoCombinators)) {
           report({
-            ruleName: ruleName,
-            result: result,
+            ruleName,
+            result,
             node: rule,
-            message: 'Avoid qualifying class selectors with an element'
-          });
+            message: "Avoid qualifying class selectors with an element",
+          })
         }
-        if (options.ignore === 'id' && hasIdQualifiedByElement(selectorNoCombinators)) {
+        if (options.ignore === "id" && hasIdQualifiedByElement(selectorNoCombinators)) {
           report({
-            ruleName: ruleName,
-            result: result,
+            ruleName,
+            result,
             node: rule,
-            message: 'Avoid qualifying id selectors with an element'
-          });
+            message: "Avoid qualifying id selectors with an element",
+          })
         }
       })
-    };
-
-    if (!validOptions) {
-      return;
     }
 
-    root.walkRules(checkForQualifyingElement);
-  };
-};
+    if (!validOptions) {
+      return
+    }
+
+    root.walkRules(checkForQualifyingElement)
+  }
+}
