3ca5b28046e0e65441c70208ae0acc626bc83ae8
jeddy3 <jeddy3@users.noreply.github.com>
2016-04-04 16:12:41 +0100

    Restructure tests

--- src/utils/__tests__/cssRuleIsKeyframe-test.js
+++ src/utils/__tests__/cssRuleIsKeyframe-test.js
@@ -3,34 +3,38 @@ import postcss from "postcss"
 import test from "tape"
 
 test("cssRuleIsKeyframe", t => {
-  postcssOkCheck(t, "@keyframes identifier { to {} }", "to")
-  postcssOkCheck(t, "@keyframes identifier { from {} }", "from")
-  postcssOkCheck(t, "@keyframes identifier { 50% {} }", "percentage")
 
-  postcssNotOkCheck(t, "a {}", "rule")
-  postcssNotOkCheck(t, "a { & b {} }", "direct nested rule")
-  postcssNotOkCheck(t, "a { @nest b & {} }", "@nest nested rule")
-  postcssNotOkCheck(t, "@media print { a {} }", "@media")
-  postcssNotOkCheck(t, "@supports (animation-name: test) { a {} }", "@supports")
-  postcssNotOkCheck(t, "@document url(http://www.w3.org/) { a {} }", "@document")
-  postcssNotOkCheck(t, "@page :pseudo-class { a {} }", "page")
-  postcssNotOkCheck(t, "@font-face { src: url(x.woff) }", "font-face")
+  t.plan(9)
 
-  t.end()
+  rules("@keyframes identifier { to {} }", rule => {
+    t.ok(cssRuleIsKeyframe(rule), "to")
+  })
+  rules("@keyframes identifier { from {} }", rule => {
+    t.ok(cssRuleIsKeyframe(rule), "from")
+  })
+  rules("@keyframes identifier { 50% {} }", rule => {
+    t.ok(cssRuleIsKeyframe(rule), "50%")
+  })
+
+  rules("a {}", rule => {
+    t.notOk(cssRuleIsKeyframe(rule), "rule")
+  })
+  rules("a { & b {} }", rule => {
+    t.notOk(cssRuleIsKeyframe(rule), "rule and direct nested rule")
+  })
+  rules("a { @nest b & {} }", rule => {
+    t.notOk(cssRuleIsKeyframe(rule), "@nest nested rule")
+  })
+  rules("@media print { a {} }", rule => {
+    t.notOk(cssRuleIsKeyframe(rule), "@media")
+  })
+  rules("@supports (animation-name: test) { a {} }", rule => {
+    t.notOk(cssRuleIsKeyframe(rule), "@supports")
+  })
 })
 
-function postcssOkCheck(t, css, desc) {
+function rules(css, cb) {
   postcss().process(css).then(result => {
-    result.root.walkRules(rule => {
-      t.ok(cssRuleIsKeyframe(rule), desc)
-    })
-  })
-}
-
-function postcssNotOkCheck(t, css, desc) {
-  postcss().process(css).then(result => {
-    result.root.walkRules(rule => {
-      t.notOk(cssRuleIsKeyframe(rule), desc)
-    })
+    result.root.walkRules(cb)
   })
 }
