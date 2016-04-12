917bb8b02e53bd99086d9aa4a064b1bdecbb09be
evilebottnawi <sheo13666q@gmail.com>
2016-04-12 10:58:53 +0300

    use `cssWordIsVariable` for skip check variables (best performance), also check on `false`  `valueParser.unit` https://github.com/TrySound/postcss-value-parser#valueparserunitquantity

--- src/rules/unit-whitelist/index.js
+++ src/rules/unit-whitelist/index.js
@@ -2,6 +2,7 @@ import { isString } from "lodash"
 import valueParser from "postcss-value-parser"
 
 import {
+  cssWordIsVariable,
   declarationValueIndexOffset,
   report,
   ruleMessages,
@@ -28,9 +29,13 @@ export default function (whitelistInput) {
 
       valueParser(value).walk(function (node) {
         if (node.type === "function" && node.value === "url") { return false }
-        if (node.type !== "word") { return }
+        if (node.type !== "word" || cssWordIsVariable(node.value)) { return }
 
-        const unit = valueParser.unit(node.value).unit
+        const parsedUnit = valueParser.unit(node.value)
+
+        if (!parsedUnit) { return }
+
+        const unit = parsedUnit.unit
 
         if (!unit || whitelist.indexOf(unit) !== -1) { return }
 
