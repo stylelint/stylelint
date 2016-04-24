5bb0b0404b201e0e7ae69f5e67ecfcd2ca7965cb
jeddy3 <jeddy3@users.noreply.github.com>
2016-04-24 16:47:11 +0100

    Ignore less vars as decls

--- src/utils/__tests__/isStandardDeclaration-test.js
+++ src/utils/__tests__/isStandardDeclaration-test.js
@@ -1,20 +1,48 @@
 import isStandardDeclaration from "../isStandardDeclaration"
+import less from "postcss-less"
 import postcss from "postcss"
 import test from "tape"
 
 test("isStandardDeclaration", t => {
-  t.ok(postcssCheck("a: b"))
-  t.ok(postcssCheck("a: $b"))
-  t.ok(postcssCheck("--prefix-a-b: $c"))
-  t.ok(postcssCheck("a : calc(b + c)"))
 
-  t.notOk(postcssCheck("$var: b"), "dollar var")
-  t.notOk(postcssCheck("$list: (key: value, key2: value2)", "dollar list"))
-  t.notOk(postcssCheck("$map: (value, value2)", "dollar map"))
-  t.end()
+  t.plan(8)
+
+  rules("a { a: b }", decl => {
+    t.ok(isStandardDeclaration(decl), "standard prop and value")
+  })
+  rules("a { a: $b }", decl => {
+    t.ok(isStandardDeclaration(decl), "standard prop and scss var")
+  })
+  rules("a { --custom-property: x }", decl => {
+    t.ok(isStandardDeclaration(decl), "custom-property")
+  })
+  rules("a { a : calc(b + c) }", decl => {
+    t.ok(isStandardDeclaration(decl), "standard prop and calc value")
+  })
+
+  rules("$var: b", decl => {
+    t.notOk(isStandardDeclaration(decl), "scss var")
+  })
+  rules("$list: (key: value, key2: value2)", decl => {
+    t.notOk(isStandardDeclaration(decl), "scss list")
+  })
+  rules("$map: (value, value2)", decl => {
+    t.notOk(isStandardDeclaration(decl), "scss map")
+  })
+
+  lessRules("a { @var: b }", decl => {
+    t.notOk(isStandardDeclaration(decl), "less var")
+  })
 })
 
-function postcssCheck(cssString) {
-  const root = postcss.parse(cssString)
-  return isStandardDeclaration(root.first)
+function rules(css, cb) {
+  postcss().process(css).then(result => {
+    result.root.walkDecls(cb)
+  })
+}
+
+function lessRules(css, cb) {
+  postcss().process(css, { syntax: less }).then(result => {
+    result.root.walkDecls(cb)
+  })
 }
