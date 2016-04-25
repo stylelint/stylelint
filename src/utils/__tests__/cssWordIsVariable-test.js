e68719d368e762f414d1fd1c15f93bb85d160ba5
jeddy3 <jeddy3@users.noreply.github.com>
2016-04-25 20:11:08 +0100

    Deprecate cssWordIsVariable

---
+++ src/utils/__tests__/cssWordIsVariable-test.js
@@ -0,0 +1,13 @@
+import test from "tape"
+import cssWordIsVariable from "../cssWordIsVariable"
+
+test("cssWordIsVariable", t => {
+  t.notOk(cssWordIsVariable("border-top-left-radius"))
+  t.notOk(cssWordIsVariable("-webkit-appearance"))
+  t.ok(cssWordIsVariable("--custom-property"))
+  t.ok(cssWordIsVariable("$sass-variable"))
+  t.ok(cssWordIsVariable("@less-variable"))
+  t.ok(cssWordIsVariable("var(--something)"))
+  t.ok(cssWordIsVariable("var(  --something  )"))
+  t.end()
+})
