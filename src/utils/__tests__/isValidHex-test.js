8f13a2314f601237fcd0fa1db47964444c66e8ae
jeddy3 <jeddy3@users.noreply.github.com>
2016-04-25 13:06:54 +0100

    Add description and tests to isValidHex

---
+++ src/utils/__tests__/isValidHex-test.js
@@ -0,0 +1,20 @@
+import test from "tape"
+import isValidHex from "../isValidHex"
+
+test("isValidHex", t => {
+  t.ok(isValidHex("#333"))
+  t.ok(isValidHex("#a3b"))
+  t.ok(isValidHex("#333a"))
+  t.ok(isValidHex("#333afe"))
+  t.ok(isValidHex("#333afeaa"))
+  t.notOk(isValidHex("a"))
+  t.notOk(isValidHex("aaa"))
+  t.notOk(isValidHex("$aaa"))
+  t.notOk(isValidHex("@aaa"))
+  t.notOk(isValidHex("var(aaa)"))
+  t.notOk(isValidHex("#z1"))
+  t.notOk(isValidHex("#00000"))
+  t.notOk(isValidHex("#000000000"))
+  t.notOk(isValidHex("#33z"))
+  t.end()
+})
