8ffc799732b22ca5e54b0f602a63e608903a5c7a
David Clark <david.dave.clark@gmail.com>
2016-05-15 14:55:12 -0700

    Add buildConfig unit tests; closes #570

--- src/buildConfig.js
+++ src/buildConfig.js
@@ -10,19 +10,38 @@ import {
 import { configurationError } from "./utils"
 
 const IGNORE_FILENAME = ".stylelintignore"
+const FILE_NOT_FOUND_ERROR_CODE = "ENOENT"
 
+/**
+ * - Accept a raw config or look up `.stylelintrc` (using cosmiconfig).
+ * - Add patterns from `.stylelintignore` to the config's `ignoreFiles`.
+ * - Resolve plugin names to absolute paths.
+ * - Resolve extends by finding, augmenting, and merging
+ *   extended configs
+ *
+ * @param {object} options - May either be an options object with a `config` property,
+ *   or just the config object itself. All the `options` properties documented below
+ *   are for the options object, not a config.
+ * @param {object} [options.config]
+ * @param {object} [options.configFile] - Specify a configuration file (path) instead
+ * @param {object} [options.configBasedir] - Specify a base directory that things should be
+ *   considered relative to.
+ * @param {object} [options.configOverrides] - An object to merge on top of the
+ *   final derived configuration object
+ * @return {object} Object with `config` and `configDir` properties.
+ */
 export default function (options) {
   const rawConfig = (() => {
     if (options.config) return options.config
     if (options.rules) return options
-    return false
+    return null
   })()
   const configBasedir = options.configBasedir || process.cwd()
 
   if (rawConfig) {
-    return augmentConfig(rawConfig, configBasedir).then(config => {
+    return augmentConfig(rawConfig, configBasedir, { addIgnorePatterns: true }).then(augmentedConfig => {
       return {
-        config: merge(config, options.configOverrides),
+        config: merge(augmentedConfig, options.configOverrides),
         configDir: configBasedir,
       }
     })
@@ -30,7 +49,7 @@ export default function (options) {
 
   const cosmiconfigOptions = {
     // Turn off argv option to avoid hijacking the all-too-common
-    // --config argument, when this is used in conjunction with other CLI's
+    // `--config` argument when stylelint is used in conjunction with other CLI's
     // (e.g. webpack)
     argv: false,
     // Allow extensions on rc filenames
@@ -46,7 +65,7 @@ export default function (options) {
   return cosmiconfig("stylelint", cosmiconfigOptions).then(result => {
     if (!result) throw configurationError("No configuration found")
     rootConfigDir = path.dirname(result.filepath)
-    return augmentConfig(result.config, rootConfigDir)
+    return augmentConfig(result.config, rootConfigDir, { addIgnorePatterns: true })
   }).then(augmentedConfig => {
     const finalConfig = (options.configOverrides)
       ? merge({}, augmentedConfig, options.configOverrides)
@@ -58,48 +77,52 @@ export default function (options) {
   })
 }
 
-function augmentConfig(config, configDir) {
-  return setIgnoreFiles(config, configDir).then(augmentedConfig => {
-    // Absolutize the plugins here, because here is the place
-    // where we know the basedir for this particular config
-    return absolutizePlugins(augmentedConfig, configDir)
-  }).then(augmentedConfig => {
-    if (!config.extends) {
-      return Promise.resolve(augmentedConfig)
+/**
+ * Given a configuration object, return a new augmented version with
+ * - Plugins resolved to absolute paths
+ * - Extended configs merged in
+ *
+ * @param {object} config
+ * @param {string} configDir
+ * @param {object} [options]
+ * @param {boolean} [options.addIgnorePatterns=false] - Look for `.stylelintignore` and
+ *   add its patterns to `config.ignoreFiles`.
+ * @return {object}
+ */
+export function augmentConfig(config, configDir, options = {}) {
+  const start = (options.addIgnorePatterns)
+    ? addIgnoreFiles(config, configDir)
+    : Promise.resolve(config)
+  return start.then(configWithIgnorePatterns => {
+    const absolutizedConfig = absolutizePlugins(configWithIgnorePatterns, configDir)
+    if (absolutizedConfig.extends) {
+      return extendConfig(absolutizedConfig, configDir)
     }
-
-    const extendLookups = [].concat(augmentedConfig.extends)
-    const origConfig = omit(augmentedConfig, "extends")
-    const resultPromise = extendLookups.reduce((mergeConfigs, extendLookup) => {
-      return mergeConfigs.then(mergedConfig => {
-        return loadExtendedConfig(mergedConfig, configDir, extendLookup).then(extendedConfig => {
-          return merge({}, mergedConfig, extendedConfig)
-        })
-      })
-    }, Promise.resolve(origConfig))
-
-    return resultPromise.then(mergedConfig => {
-      return merge({}, mergedConfig, origConfig)
-    })
+    return Promise.resolve(absolutizedConfig)
   })
 }
 
-function setIgnoreFiles(config, configDir) {
+export function addIgnoreFiles(config, configDir) {
   return findIgnorePatterns(configDir).then(ignorePatterns => {
     config.ignoreFiles = [].concat(ignorePatterns, config.ignoreFiles || [])
     return config
   })
 }
 
-function loadExtendedConfig(config, configDir, extendLookup) {
-  const extendPath = getModulePath(configDir, extendLookup)
-  const extendDir = path.dirname(extendPath)
-  return cosmiconfig(null, {
-    configPath: extendPath,
-    // In case --config was used: do not pay attention to it again
-    argv: false,
-  }).then(result => {
-    return augmentConfig(stripIgnoreFiles(result.config), extendDir)
+function findIgnorePatterns(configDir) {
+  const ignoreFilePath = path.resolve(configDir, IGNORE_FILENAME)
+  return new Promise((resolve, reject) => {
+    fs.readFile(ignoreFilePath, "utf8", (err, data) => {
+      if (err) {
+        // If the file's not found, great, we'll just give an empty array
+        if (err.code === FILE_NOT_FOUND_ERROR_CODE) { return resolve([]) }
+        return reject(err)
+      }
+      const ignorePatterns = data.split(/\r?\n/g)
+        .filter(val => val.trim() !== "")     // Remove empty lines
+        .filter(val => val.trim()[0] !== "#") // Remove comments
+      resolve(ignorePatterns)
+    })
   })
 }
 
@@ -111,6 +134,38 @@ function absolutizePlugins(config, configDir) {
   })
 }
 
+function extendConfig(config, configDir) {
+  const extendLookups = [].concat(config.extends)
+  const origConfig = omit(config, "extends")
+  const resultPromise = extendLookups.reduce((mergeConfigs, extendLookup) => {
+    return mergeConfigs.then(mergedConfig => {
+      return loadExtendedConfig(mergedConfig, configDir, extendLookup).then(extendedConfig => {
+        return merge({}, mergedConfig, extendedConfig)
+      })
+    })
+  }, Promise.resolve(origConfig))
+
+  return resultPromise.then(mergedConfig => {
+    return merge({}, mergedConfig, origConfig)
+  })
+}
+
+function loadExtendedConfig(config, configDir, extendLookup) {
+  const extendPath = getModulePath(configDir, extendLookup)
+  const extendDir = path.dirname(extendPath)
+  return cosmiconfig(null, {
+    configPath: extendPath,
+    // In case `--config` was used: do not pay attention to it again
+    argv: false,
+  }).then(result => {
+    // Make sure to also augment the config that we're merging in
+    // ... but the `ignoreFiles` option only works with the
+    // config that is being directly invoked, not any
+    // extended configs
+    return augmentConfig(stripIgnoreFiles(result.config), extendDir)
+  })
+}
+
 function getModulePath(basedir, lookup) {
   const path = resolveFrom(basedir, lookup)
   if (path) return path
@@ -119,27 +174,6 @@ function getModulePath(basedir, lookup) {
   )
 }
 
-function findIgnorePatterns(configDir) {
-  return new Promise((resolve, reject) => {
-    const ignoreFilePath = path.resolve(configDir, IGNORE_FILENAME)
-
-    fs.readFile(ignoreFilePath, "utf8", (err, data) => {
-      if (err) {
-        // If the file's not found, great, we'll just give in an empty array
-        if (err.code === "ENOENT") return resolve([])
-        return reject(err)
-      }
-      const ignorePatterns = data.split(/\r?\n/g)
-        .filter(val => val.trim() !== "")     // Remove empty lines
-        .filter(val => val.trim()[0] !== "#") // Remove comments
-      resolve(ignorePatterns)
-    })
-  })
-}
-
-// The `ignoreFiles` option only works with the
-// config that is being directly invoked, not any
-// extended configs
 function stripIgnoreFiles(config) {
   return omit(config, "ignoreFiles")
 }
