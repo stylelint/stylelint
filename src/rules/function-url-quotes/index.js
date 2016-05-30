811f6c7ae2a63c7a7e4b7d3cca5644574473adc1
DAVID CLARK <dclark@mapbox.com>
2016-05-30 15:01:24 -0700

    Remove deprecated options of function-url-quotes

--- src/rules/function-url-quotes/index.js
+++ src/rules/function-url-quotes/index.js
@@ -10,89 +10,32 @@ import {
 export const ruleName = "function-url-quotes"
 
 export const messages = ruleMessages(ruleName, {
-  expected: (q, f = "url") => `Expected ${q} around ${f} argument`,
+  expected: (f = "url") => `Expected quotes around ${f} argument`,
+  rejected: (f = "url") => `Unexpected quotes around ${f} argument`,
 })
 
 export default function (expectation) {
-
-  const quoteMsg = (function () {
-    switch (expectation) {
-      case "single":
-        return "single quotes"
-      case "double":
-        return "double quotes"
-      case "none":
-        return "no quotes"
-
-      case "always":
-        return "quotes"
-      case "never":
-        return "no quotes"
-    }
-  }())
-
-  const charDefiesExpectation = (function () {
-    switch (expectation) {
-      case "single":
-        return c => c !== "'"
-      case "double":
-        return c => c !== "\""
-      case "none":
-        return c => c === "'" || c === "\""
-
-      case "never":
-        return c => c === "'" || c === "\""
-      case "always":
-        return c => c !== "'" && c !== "\""
-    }
-  }())
-
-  function strDefiesExpectation(str) {
-    return charDefiesExpectation(str[0]) || charDefiesExpectation(str[str.length - 1])
-  }
-
   return (root, result) => {
     const validOptions = validateOptions(result, ruleName, {
       actual: expectation,
       possible: [
-        "single",
-        "double",
-        "none",
-
         "always",
         "never",
       ],
     })
     if (!validOptions) { return }
 
-    if (
-      expectation === "single"
-      || expectation === "double"
-      || expectation === "none"
-    ) {
-      result.warn((
-        "The '" + expectation + "' option for 'function-url-quotes' has been deprecated, "
-          + "and will be removed in '7.0'. Instead, use the 'always' or 'never' options together with the 'string-quotes' rule."
-      ), {
-        stylelintType: "deprecation",
-        stylelintReference: "http://stylelint.io/user-guide/rules/function-url-quotes/",
-      })
-    }
+    root.walkAtRules(checkStatement)
+    root.walkRules(checkStatement)
 
-    root.walkAtRules(check)
-    root.walkRules(check)
-
-    function check(statement) {
+    function checkStatement(statement) {
       if (statement.type === "atrule") {
         checkAtRuleParams(statement)
       }
 
       statement.walkDecls(function (decl) {
         functionArgumentsSearch(decl.toString().toLowerCase(), "url", (args, index) => {
-          if (!isStandardUrl(args)) { return }
-          const trimLeftArgs = args.trimLeft()
-          if (!strDefiesExpectation(trimLeftArgs.trimRight())) { return }
-          complain(messages.expected(quoteMsg), decl, index + args.length - trimLeftArgs.length)
+          checkArgs(args, decl, index, "url")
         })
       })
     }
@@ -100,35 +43,31 @@ export default function (expectation) {
     function checkAtRuleParams(atRule) {
       const atRuleParamsLowerCase = atRule.params.toLowerCase()
       functionArgumentsSearch(atRuleParamsLowerCase, "url", (args, index) => {
-        if (!isStandardUrl(args)) { return }
-        const trimLeftArgs = args.trimLeft()
-        if (!strDefiesExpectation(trimLeftArgs.trimRight())) { return }
-        complain(
-          messages.expected(quoteMsg),
-          atRule,
-          index + args.length - trimLeftArgs.length + atRuleParamIndex(atRule)
-        )
+        checkArgs(args, atRule, index + atRuleParamIndex(atRule), "url")
       })
       functionArgumentsSearch(atRuleParamsLowerCase, "url-prefix", (args, index) => {
-        if (!isStandardUrl(args)) { return }
-        const trimLeftArgs = args.trimLeft()
-        if (!strDefiesExpectation(trimLeftArgs.trimRight())) { return }
-        complain(
-          messages.expected(quoteMsg, "url-prefix"),
-          atRule,
-          index + args.length - trimLeftArgs.length + atRuleParamIndex(atRule)
-        )
+        checkArgs(args, atRule, index + atRuleParamIndex(atRule), "url-prefix")
       })
       functionArgumentsSearch(atRuleParamsLowerCase, "domain", (args, index) => {
-        if (!isStandardUrl(args)) { return }
-        const trimLeftArgs = args.trimLeft()
-        if (!strDefiesExpectation(trimLeftArgs.trimRight())) { return }
-        complain(
-          messages.expected(quoteMsg, "domain"),
-          atRule,
-          index + args.length - trimLeftArgs.length + atRuleParamIndex(atRule)
-        )
+        checkArgs(args, atRule, index + atRuleParamIndex(atRule), "domain")
       })
+    }
+
+    function checkArgs(args, node, index, functionName) {
+      const leftTrimmedArgs = args.trimLeft()
+      if (!isStandardUrl(leftTrimmedArgs)) { return }
+      const complaintIndex = index + args.length - leftTrimmedArgs.length
+      const hasQuotes = leftTrimmedArgs[0] === "'" || leftTrimmedArgs[0] === "\""
+      switch (expectation) {
+        case "always":
+          if (hasQuotes) { return }
+          complain(messages.expected(functionName), node, complaintIndex)
+          return
+        case "never":
+          if (!hasQuotes) { return }
+          complain(messages.rejected(functionName), node, complaintIndex)
+          return
+      }
     }
 
     function complain(message, node, index) {
