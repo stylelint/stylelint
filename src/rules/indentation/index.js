4f5e2ba517ec4a0099f058c7c06c7ad4ca88c474
DAVID CLARK <dclark@mapbox.com>
2016-05-30 15:07:02 -0700

    Remove hierarchicalSelectors option from indentation

--- src/rules/indentation/index.js
+++ src/rules/indentation/index.js
@@ -15,26 +15,12 @@ export const messages = ruleMessages(ruleName, {
   expected: x => `Expected indentation of ${x}`,
 })
 
-// The hierarchyMap keeps track of nodes with confirmed
-// superordinates and indentation levels.
-// It can then be used to quickly check the indentation level of
-// some prior node, or (when hierarchicalSelectors is one) by rules to check
-// if they have a peer in the hierarchyMap, and should share that
-// peer's superordinate.
-const hierarchyMap = new Map()
-
-function addNodeToHierarchy(node, superordinate, level) {
-  hierarchyMap.set(node, { superordinate, level })
-}
-
 /**
  * @param {number|"tab"} space - Number of whitespaces to expect, or else
  *   keyword "tab" for single `\t`
  * @param {object} [options]
  * @param {array} [options.except = ["block", "value"]] - Do *not* expect extra level of
  *   indentation for nested blocks and multi-line values, respectively
- * @param {array} [options.hierarchicalSelectors = false] - If `true`, we'll look for a
- *   hierarchical style of indentation (see tests and docs)
  */
 export default function (space, options = {}) {
   const isTab = space === "tab"
@@ -50,7 +36,6 @@ export default function (space, options = {}) {
       possible: {
         except: [ "block", "value", "param" ],
         ignore: [ "value", "param" ],
-        hierarchicalSelectors: [isBoolean],
         indentInsideParens: [ "once", "twice", "once-at-root-twice-in-block" ],
         indentClosingBrace: [isBoolean],
       },
@@ -59,35 +44,9 @@ export default function (space, options = {}) {
     if (!validOptions) { return }
 
     // Cycle through all nodes using walk.
-    // This is done instead of using
-    // eachRule, eachAtRule, and eachDecl,
-    // so that any hierarchy can be accounted for *in order*.
     root.walk(node => {
 
-      let nodeLevel = indentationLevel(node)
-
-      if (options && options.hierarchicalSelectors) {
-        result.warn((
-          "The 'hierarchicalSelectors' option for 'indentation' has been deprecated, "
-            + "and will be removed in '7.0'. If you use this option please consider "
-            + "creating a plugin for the community."
-        ), {
-          stylelintType: "deprecation",
-          stylelintReference: "http://stylelint.io/user-guide/release-planning/",
-        })
-        // hierarchicalSelectorsLevel will add the node to the hierarchyMap ...
-        nodeLevel = hierarchicalSelectorsLevel(node, nodeLevel)
-      } else {
-        // ... so if it doesn't run we need to add this node to
-        // the hierarchyMap for future reference.
-        // If there isn't a selector hierarchy enforced, then the superordinate
-        // can only be the node's parent.
-        addNodeToHierarchy(node, node.parent, nodeLevel)
-      }
-
-      // At this point, the node's indent level should be calculated,
-      // and this information should be saved in hierarchyMap
-
+      const nodeLevel = indentationLevel(node)
       const expectedWhitespace = repeat(indentChar, nodeLevel)
 
       let before = node.raw("before")
@@ -157,23 +116,12 @@ export default function (space, options = {}) {
     function indentationLevel(node, level = 0) {
       if (node.parent.type === "root") { return level }
 
-      // In case by recursion we're checking a node that's
-      // already been checked ...
-      if (hierarchyMap.has(node)) {
-        return hierarchyMap.get(node).level
-      }
-
       let calculatedLevel
-      if (hierarchyMap.has(node.parent)) {
-        // If the hierarchyMap already contains this node's
-        // parent, use that level
-        calculatedLevel = hierarchyMap.get(node.parent).level + 1
-      } else {
-        // Typically, indentation level equals the ancestor nodes
-        // separating this node from root; so recursively
-        // run this operation
-        calculatedLevel = indentationLevel(node.parent, level + 1)
-      }
+
+      // Indentation level equals the ancestor nodes
+      // separating this node from root; so recursively
+      // run this operation
+      calculatedLevel = indentationLevel(node.parent, level + 1)
 
       // If options.except includes "block",
       // blocks are taken down one from their calculated level
@@ -276,93 +224,4 @@ export default function (space, options = {}) {
       : warningWord + "s"
     return `${count} ${quantifiedWarningWord}`
   }
-}
-
-// Figure the correct level of indentation if this is a rule that is
-// part of a hierarchy of selectors.
-//
-// In the hierarchy, Rule A is subordinate to Rule B if Rule A's
-// selector starts with Rule B's selector. Each rule can be
-// subordinate to one other rule, but superordinate to many.
-//
-// Subordinates do not always immediately follow their
-// superordinates, so it would be overly simplistic to just
-// check if any given rule is subordinate to the previous rule.
-function hierarchicalSelectorsLevel(node, nodeLevel) {
-  const prevNode = node.prev()
-
-  // For various reasons we might rule out that this is
-  // a hierarchical node
-  if (
-    !prevNode
-    || prevNode.type !== "rule"
-    || node.type === "decl"
-    || node.type === "comment"
-  ) {
-    addNodeToHierarchy(node, node.parent, nodeLevel)
-    return nodeLevel
-  }
-
-  // For at-rules: if *all* of the rules in the at-rule start with
-  // selector of the rule before the at-rule, it should be subordinated
-  // to the previous rule
-  if (node.type === "atrule") {
-    let insubordinate
-    node.walkRules(rule => {
-      if (!isSubordinateTo(rule, prevNode)) {
-        insubordinate = true
-        return false
-      }
-    })
-    const expectedLevel = (hierarchyMap.has(prevNode))
-      ? hierarchyMap.get(prevNode).level + 1
-      : nodeLevel + 1
-    if (insubordinate) {
-      addNodeToHierarchy(node, node.parent, expectedLevel)
-      return nodeLevel
-    }
-    addNodeToHierarchy(node, prevNode, expectedLevel)
-    return expectedLevel
-  }
-
-  // For rules ...
-  const isFirstSubordinate = isSubordinateTo(node, prevNode)
-  if (isFirstSubordinate) {
-    const expectedLevel = (hierarchyMap.has(prevNode))
-      ? hierarchyMap.get(prevNode).level + 1
-      : nodeLevel + 1
-    addNodeToHierarchy(node, prevNode, expectedLevel)
-    return expectedLevel
-  }
-
-  // If this node is not subordinate to prevNode, but prevNode was itself a subordinate,
-  // maybe this node is a peer of prevNode (and therefore should be subordinate to the
-  // same superordinate). Or maybe it's a peer of prevNode's superordinate.
-  // Recursively check the hierarchy in this manner for possible peers: if one
-  // is found, use that peer's nodeLevel.
-  let maybePeer = prevNode
-  while (maybePeer) {
-    if (hierarchyMap.has(maybePeer)) {
-      const maybePeerInfo = hierarchyMap.get(maybePeer)
-      if (isSubordinateTo(node, maybePeerInfo.superordinate)) {
-        addNodeToHierarchy(node, maybePeerInfo.superordinate, maybePeerInfo.level)
-        return maybePeerInfo.level
-      } else {
-        maybePeer = maybePeerInfo.superordinate
-      }
-    } else {
-      maybePeer = false
-    }
-  }
-
-  addNodeToHierarchy(node, node.parent, nodeLevel)
-  return nodeLevel
-}
-
-function isSubordinateTo(a, b) {
-  return (
-    a && b
-    && a.selector.indexOf(b.selector) === 0
-    && a.selector !== b.selector
-  )
 }
