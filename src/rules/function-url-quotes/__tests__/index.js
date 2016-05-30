9047d7fb1f03edc9ba9e7bf6909e373627674c1a
DAVID CLARK <dclark@mapbox.com>
2016-05-30 15:01:24 -0700

    Remove deprecated options of function-url-quotes

--- src/rules/function-url-quotes/__tests__/index.js
+++ src/rules/function-url-quotes/__tests__/index.js
@@ -2,10 +2,6 @@ import { testRule } from "../../../testUtils"
 import rules from "../../../rules"
 import { ruleName, messages } from ".."
 
-import postcss from "postcss"
-import test from "tape"
-import stylelint from "../../.."
-
 const rule = rules[ruleName]
 
 testRule(rule, {
@@ -16,7 +12,7 @@ testRule(rule, {
     code: "@import url(\"foo.css\");",
 
     description: {
-      message: messages.expected("quotes"),
+      message: messages.expected(),
       line: 1,
       column: 14,
     },
@@ -24,7 +20,7 @@ testRule(rule, {
     code: "@import uRl(\"foo.css\");",
 
     description: {
-      message: messages.expected("quotes"),
+      message: messages.expected(),
       line: 1,
       column: 14,
     },
@@ -32,7 +28,7 @@ testRule(rule, {
     code: "@import URL(\"foo.css\");",
 
     description: {
-      message: messages.expected("quotes"),
+      message: messages.expected(),
       line: 1,
       column: 14,
     },
@@ -40,7 +36,7 @@ testRule(rule, {
     code: "@import url( \"foo.css\" );",
 
     description: {
-      message: messages.expected("quotes"),
+      message: messages.expected(),
       line: 1,
       column: 15,
     },
@@ -150,67 +146,67 @@ testRule(rule, {
 
   reject: [ {
     code: "@import url(foo.css);",
-    message: messages.expected("quotes"),
+    message: messages.expected(),
     line: 1,
     column: 13,
   }, {
     code: "@import url( foo.css );",
-    message: messages.expected("quotes"),
+    message: messages.expected(),
     line: 1,
     column: 14,
   }, {
     code: "@document url-prefix(http://www.w3.org/Style);",
-    message: messages.expected("quotes", "url-prefix"),
+    message: messages.expected("url-prefix"),
     line: 1,
     column: 22,
   }, {
     code: "@document url-prefix( http://www.w3.org/Style );",
-    message: messages.expected("quotes", "url-prefix"),
+    message: messages.expected("url-prefix"),
     line: 1,
     column: 23,
   }, {
     code: "@font-face { font-family: 'foo'; src: url(foo.ttf); }",
-    message: messages.expected("quotes"),
+    message: messages.expected(),
     line: 1,
     column: 43,
   }, {
     code: "@font-face { font-family: 'foo'; src: url( foo.ttf ); }",
-    message: messages.expected("quotes"),
+    message: messages.expected(),
     line: 1,
     column: 44,
   }, {
     code: "a { cursor: url(foo.png); }",
-    message: messages.expected("quotes"),
+    message: messages.expected(),
     line: 1,
     column: 17,
   }, {
     code: "a { background-image: url(foo.css), url(\"bar.css\"), url(\"baz.css\"); }",
-    message: messages.expected("quotes"),
+    message: messages.expected(),
     line: 1,
     column: 27,
   }, {
     code: "a { background-image: url( foo.css ), url(\"bar.css\"), url(\"baz.css\"); }",
-    message: messages.expected("quotes"),
+    message: messages.expected(),
     line: 1,
     column: 28,
   }, {
     code: "a { background-image: url(\"foo.css\"), url(bar.css), url(\"baz.css\"); }",
-    message: messages.expected("quotes"),
+    message: messages.expected(),
     line: 1,
     column: 43,
   }, {
     code: "a { background-image: url(\"foo.css\"), url( bar.css ), url(\"baz.css\"); }",
-    message: messages.expected("quotes"),
+    message: messages.expected(),
     line: 1,
     column: 44,
   }, {
     code: "a { background-image: url(\"foo.css\"), url(\"bar.css\"), url(baz.css); }",
-    message: messages.expected("quotes"),
+    message: messages.expected(),
     line: 1,
     column: 59,
   }, {
     code: "a { background-image: url(\"foo.css\"), url(\"bar.css\"), url( baz.css ); }",
-    message: messages.expected("quotes"),
+    message: messages.expected(),
     line: 1,
     column: 60,
   } ],
@@ -292,199 +288,138 @@ testRule(rule, {
 
   reject: [ {
     code: "@import url(\"foo.css\");",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 13,
   }, {
     code: "@import uRl(\"foo.css\");",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 13,
   }, {
     code: "@import URL(\"foo.css\");",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 13,
   }, {
     code: "@import url( \"foo.css\" );",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 14,
   }, {
     code: "@import url('foo.css');",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 13,
   }, {
     code: "@import url( 'foo.css' );",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 14,
   }, {
     code: "@document url(\"http://www.w3.org/\");",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 15,
   }, {
     code: "@document url( \"http://www.w3.org/\" );",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 16,
   }, {
     code: "@document url-prefix('http://www.w3.org/Style');",
-    message: messages.expected("no quotes", "url-prefix"),
+    message: messages.rejected("url-prefix"),
     line: 1,
     column: 22,
   }, {
     code: "@document url-prefix( 'http://www.w3.org/Style' );",
-    message: messages.expected("no quotes", "url-prefix"),
+    message: messages.rejected("url-prefix"),
     line: 1,
     column: 23,
   }, {
     code: "@document domain(\"mozilla.org\");",
-    message: messages.expected("no quotes", "domain"),
+    message: messages.rejected("domain"),
     line: 1,
     column: 18,
   }, {
     code: "@document domain( \"mozilla.org\" );",
-    message: messages.expected("no quotes", "domain"),
+    message: messages.rejected("domain"),
     line: 1,
     column: 19,
   }, {
     code: "@font-face { font-family: foo; src: url('foo.ttf'); }",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 41,
   }, {
     code: "@font-face { font-family: foo; src: url( 'foo.ttf' ); }",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 42,
   }, {
     code: "a { background: url(\"foo.css\"); }",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 21,
   }, {
     code: "a { background: uRl(\"foo.css\"); }",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 21,
   }, {
     code: "a { background: URL(\"foo.css\"); }",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 21,
   }, {
     code: "a { background: url( \"foo.css\" ); }",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 22,
   }, {
     code: "a { background: url(  \"foo.css\"  ); }",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 23,
   }, {
     code: "a { cursor: url(\"foo.png\"); }",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 17,
   }, {
     code: "a { background-image: url('foo.css'), url(bar.css), url(baz.css); }",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 27,
   }, {
     code: "a { background-image: url( 'foo.css' ), url(bar.css), url(baz.css); }",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 28,
   }, {
     code: "a { background-image: url(foo.css), url('bar.css'), url(baz.css); }",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 41,
   }, {
     code: "a { background-image: url(foo.css), url( 'bar.css' ), url(baz.css); }",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 42,
   }, {
     code: "a { background-image: url(foo.css), url(bar.css), url('baz.css'); }",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 55,
   }, {
     code: "a { background-image: url(foo.css), url(bar.css), url( 'baz.css' ); }",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 56,
   }, {
     code: "a { background: url(\"/images/my_image@2x.png\") }",
-    message: messages.expected("no quotes"),
+    message: messages.rejected(),
     line: 1,
     column: 21,
   } ],
 })
-
-test("deprecation warning for single", t => {
-  const config = {
-    rules: {
-      "function-url-quotes": "single",
-    },
-  }
-  let planned = 0
-
-  postcss().use(stylelint(config)).process("@import url(\"foo.css\");").then(result => {
-    t.equal(result.warnings().length, 2)
-    t.equal(result.warnings()[0].text, "The 'single' option for 'function-url-quotes' has been deprecated, and will be removed in '7.0'. Instead, use the 'always' or 'never' options together with the 'string-quotes' rule.")
-    t.equal(result.warnings()[0].stylelintType, "deprecation")
-    t.equal(result.warnings()[1].text, "Expected single quotes around url argument (function-url-quotes)")
-  }).catch(logError)
-  planned += 4
-
-  t.plan(planned)
-})
-
-test("deprecation warning for double", t => {
-  const config = {
-    rules: {
-      "function-url-quotes": "double",
-    },
-  }
-  let planned = 0
-
-  postcss().use(stylelint(config)).process("@import url('foo.css');").then(result => {
-    t.equal(result.warnings().length, 2)
-    t.equal(result.warnings()[0].text, "The 'double' option for 'function-url-quotes' has been deprecated, and will be removed in '7.0'. Instead, use the 'always' or 'never' options together with the 'string-quotes' rule.")
-    t.equal(result.warnings()[0].stylelintType, "deprecation")
-    t.equal(result.warnings()[1].text, "Expected double quotes around url argument (function-url-quotes)")
-  }).catch(logError)
-  planned += 4
-
-  t.plan(planned)
-})
-
-test("deprecation warning for none", t => {
-  const config = {
-    rules: {
-      "function-url-quotes": "none",
-    },
-  }
-  let planned = 0
-
-  postcss().use(stylelint(config)).process("@import url(\"foo.css\");").then(result => {
-    t.equal(result.warnings().length, 2)
-    t.equal(result.warnings()[0].text, "The 'none' option for 'function-url-quotes' has been deprecated, and will be removed in '7.0'. Instead, use the 'always' or 'never' options together with the 'string-quotes' rule.")
-    t.equal(result.warnings()[0].stylelintType, "deprecation")
-    t.equal(result.warnings()[1].text, "Expected no quotes around url argument (function-url-quotes)")
-  }).catch(logError)
-  planned += 4
-
-  t.plan(planned)
-})
-
-function logError(err) {
-  console.log(err.stack) // eslint-disable-line no-console
-}
