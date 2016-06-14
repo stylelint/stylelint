d3faa3b855e977c36753e520df83c5c472ff3fb4
Stephen Edgar <stephen@netweb.com.au>
2016-06-14 11:10:45 +1000

    Update visual.sh verbose flag

--- scripts/visual.sh
+++ scripts/visual.sh
@@ -4,4 +4,4 @@ echo "Normal:"
 node ../dist/cli.js visual.css --config=visual-config.json
 
 echo -e "\n\nVerbose:"
-node ../dist/cli.js visual.css --config=visual-config.json --verbose
+node ../dist/cli.js visual.css --config=visual-config.json --formatter verbose
