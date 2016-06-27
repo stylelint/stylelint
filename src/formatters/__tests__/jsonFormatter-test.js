3bc7d0aae9d870edc65ca87bb369be5269a3453e
jeddy3 <jeddy3@users.noreply.github.com>
2016-06-27 09:10:45 +0100

    Add JSON formatter test
    Closes #1359

---
+++ src/formatters/__tests__/jsonFormatter-test.js
@@ -0,0 +1,22 @@
+import test from "tape"
+import jsonFormatter from "../jsonFormatter"
+
+test("json formatter", t => {
+
+  const results = [{
+    "source":  "path/to/file.css",
+    "errored": true,
+    "warnings":[{
+      "line": 1,
+      "column": 2,
+      "rule": "bar",
+      "severity": "error",
+      "text": "Unexpected foo",
+    }],
+    "deprecations": [],
+    "invalidOptionWarnings":[],
+  }]
+
+  t.deepEqual(JSON.parse(jsonFormatter(results)), results)
+  t.end()
+})
