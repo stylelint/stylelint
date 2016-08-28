f72d145ddb9dd31960ae85378f05f368a8f3010a
Julian Grinblat <julian@dotcore.co.il>
2016-08-28 21:58:34 +0900

    Support minus and plus in front of variables in calc

--- src/rules/function-calc-no-unspaced-operator/index.js
+++ src/rules/function-calc-no-unspaced-operator/index.js
@@ -56,7 +56,7 @@ export default function (actual) {
             // (@ and $ are considered "digits" here to allow for variable syntaxes
             // that permit signs in front of variables, e.g. `-$number`)
             // As is "." to deal with fractional numbers without a leading zero
-            if ((symbol === "+" || symbol === "-") && /[\d@\$.]/.test(expression[index + 1])) {
+            if ((symbol === "+" || symbol === "-") && /^([\d@\$.]|var\()/.test(expression.substring(index + 1))) {
               const expressionBeforeSign = expression.substr(0, index)
 
               // Ignore signs that directly follow a opening bracket
