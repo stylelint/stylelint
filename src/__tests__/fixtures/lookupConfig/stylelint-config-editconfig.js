0cb244aaaffad997f60c2205eebad56bc1805952
刘祺 <qil@jumei.com>
2016-09-27 12:05:47 +0800

    set default rule value to `null`

--- src/__tests__/fixtures/lookupConfig/stylelint-config-editconfig.js
+++ src/__tests__/fixtures/lookupConfig/stylelint-config-editconfig.js
@@ -12,9 +12,9 @@ module.exports = function (merged, config) {
   if (config.sourcePath) {
     return editorconfig.parse(config.sourcePath).then(editorconfig => {
       const rules = {
-        "indentation": editorconfig.indent_size,
-        "no-eol-whitespace": editorconfig.trim_trailing_whitespace,
-        "no-missing-end-of-source-newline": editorconfig.insert_final_newline,
+        "indentation": editorconfig.indent_size || null,
+        "no-eol-whitespace": editorconfig.trim_trailing_whitespace || null,
+        "no-missing-end-of-source-newline": editorconfig.insert_final_newline || null,
       }
       for (const ruleName in rules) {
         mergeRule(ruleName, rules[ruleName])
