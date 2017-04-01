1bcfc004667f645d4ad9b26e5cb56e794180e907
Richard Hallows <jeddy3@users.noreply.github.com>
2017-04-01 21:30:14 +0100

    Fix system tests on win32 (#2465)
    Closes #2357

--- system-tests/systemTestUtils.js
+++ system-tests/systemTestUtils.js
@@ -22,8 +22,10 @@ function prepResults(results) {
     // The _postcssResult object is not part of our API and is huge
     const preppedResult = _.omit(result, "_postcssResult")
 
-    // The `source` of each file will not be the same on different machines
-    preppedResult.source = path.relative(__dirname, result.source)
+    // The `source` of each file will not be the same on different machines or platforms
+    preppedResult.source =
+      path.relative(__dirname, result.source)
+      .replace(new RegExp(`\\${path.sep}`, "g"), "/")
 
     return preppedResult
   })
