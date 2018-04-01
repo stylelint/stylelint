f60ab68d2efd6c9a58078aecc85a56d9bd9894af
jeddy3 <jeddy3@users.noreply.github.com>
2018-04-01 11:27:13 +0100

    Increase timeout for long running tests

--- system-tests/001/001.test.js
+++ system-tests/001/001.test.js
@@ -4,13 +4,17 @@
 const stylelint = require("../../lib");
 const systemTestUtils = require("../systemTestUtils");
 
-it("001", () => {
-  return stylelint
-    .lint({
-      files: [systemTestUtils.caseStylesheetGlob("001")],
-      configFile: systemTestUtils.caseConfig("001")
-    })
-    .then(output => {
-      expect(systemTestUtils.prepResults(output.results)).toMatchSnapshot();
-    });
-});
+it(
+  "001",
+  () => {
+    return stylelint
+      .lint({
+        files: [systemTestUtils.caseStylesheetGlob("001")],
+        configFile: systemTestUtils.caseConfig("001")
+      })
+      .then(output => {
+        expect(systemTestUtils.prepResults(output.results)).toMatchSnapshot();
+      });
+  },
+  10000
+);
--- system-tests/005/005.test.js
+++ system-tests/005/005.test.js
@@ -4,13 +4,17 @@
 const stylelint = require("../../lib");
 const systemTestUtils = require("../systemTestUtils");
 
-it("005", () => {
-  return stylelint
-    .lint({
-      files: [systemTestUtils.caseStylesheetGlob("005")],
-      configFile: systemTestUtils.caseConfig("005")
-    })
-    .then(output => {
-      expect(systemTestUtils.prepResults(output.results)).toMatchSnapshot();
-    });
-});
+it(
+  "005",
+  () => {
+    return stylelint
+      .lint({
+        files: [systemTestUtils.caseStylesheetGlob("005")],
+        configFile: systemTestUtils.caseConfig("005")
+      })
+      .then(output => {
+        expect(systemTestUtils.prepResults(output.results)).toMatchSnapshot();
+      });
+  },
+  10000
+);
