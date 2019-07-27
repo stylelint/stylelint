db544040dd6dd93a290de374e2ebd4ca6c1782ff
Ivan Kopeykin <ikopeykin@ozon.ru>
2019-07-27 17:47:31 +0300

    Always return boolean stylelintError (#4174)

---
+++ system-tests/ignoreFile/ignoreFile.test.js
@@ -0,0 +1,18 @@
+/* @flow */
+"use strict";
+
+const stylelint = require("../../lib");
+const systemTestUtils = require("../systemTestUtils");
+
+it("ignoreFile", () => {
+  return stylelint
+    .lint({
+      files: [systemTestUtils.caseStylesheetGlob("ignoreFile")],
+      ignorePath: require("path").join(__dirname, ".stylelintignore"),
+      allowEmptyInput: true,
+      configFile: systemTestUtils.caseConfig("ignoreFile")
+    })
+    .then(output => {
+      expect(output).toMatchSnapshot();
+    });
+}, 10000);
