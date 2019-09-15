9c1b7349d021c164a3b1498559544a30989fe569
Aleks Hudochenkov <aleks@hudochenkov.com>
2019-09-15 22:22:48 +0200

    New code formatting settings (#4259)

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// https://prettier.io/docs/en/options.html
+module.exports = {
+	arrowParens: 'always',
+	printWidth: 100,
+	singleQuote: true,
+	trailingComma: 'all',
+	useTabs: true,
+};
