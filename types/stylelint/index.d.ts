7b732bc96295d3a14d620f10e03a7296e77819e8
Ivan Kopeykin <ikopeykin@ozon.ru>
2019-10-05 15:42:02 +0300

    add types to getPostcssResult

--- types/stylelint/index.d.ts
+++ types/stylelint/index.d.ts
@@ -1,5 +1,35 @@
 declare module 'stylelint' {
-	import { Result, WarningOptions } from 'postcss';
+	import {Result} from 'postcss';
+
+	export type StylelintConfigExtends = string | Array<string>;
+	export type StylelintConfigPlugins = string | Array<string>;
+	export type StylelintConfigProcessors =
+		| string
+		| Array<string | [string, Object]>;
+	export type StylelintConfigIgnoreFiles = string | Array<string>;
+	export type StylelintConfigRuleSettings = any | [any, Object];
+	export type StylelintConfigRules = {
+		[ruleName: string]: StylelintConfigRuleSettings
+	};
+
+	export type StylelintConfig = {
+		extends?: StylelintConfigExtends,
+		plugins?: StylelintConfigPlugins,
+		pluginFunctions?: {
+			[pluginName: string]: Function
+		},
+		processors?: StylelintConfigProcessors,
+		processorFunctions?: Array<Function>,
+		ignoreFiles?: StylelintConfigIgnoreFiles,
+		ignorePatterns?: string,
+		rules?: StylelintConfigRules,
+		codeProcessors?: Array<Function>,
+		resultProcessors?: Array<Function>,
+		quiet?: boolean,
+		defaultSeverity?: string
+	};
+
+	export type StylelintSyntaxes = "scss" | "less" | "sugarss";
 
 	export type DisabledRange = {
 		start: number,
@@ -13,17 +43,148 @@ declare module 'stylelint' {
 	};
 
 	export type StylelintPostcssResult = {
-		ruleSeverities: {[k: string]: any},
-		customMessages: {[k: string]: any},
+		ruleSeverities: Object,
+		customMessages: Object,
 		quiet?: boolean,
 		disabledRanges: DisabledRangeObject,
 		ignored?: boolean,
-		ignoreDisables?: boolean,
 		stylelintError?: boolean
 	};
 
-	export type PostcssResult = Result & {
-		stylelint: StylelintPostcssResult,
-		warn(message: string, options?: WarningOptions & {stylelintType?: string}): void;
+	export type PostcssResult = Result & {stylelint: StylelintPostcssResult};
+
+	export type StylelintOptions = {
+		config?: StylelintConfig,
+		configFile?: string,
+		configBasedir?: string,
+		configOverrides?: Object,
+		ignoreDisables?: boolean,
+		ignorePath?: string,
+		reportInvalidScopeDisables?: boolean,
+		reportNeedlessDisables?: boolean,
+		syntax?: string,
+		customSyntax?: string,
+		fix?: boolean
+	};
+
+	export type GetPostcssOptions = {
+		code?: string,
+		codeFilename?: string,
+		filePath?: string,
+		codeProcessors?: Array<Function>,
+		syntax?: StylelintSyntaxes,
+		customSyntax?: string
+	};
+
+	export type StylelintInternalApi = {
+		_options: StylelintOptions,
+		_extendExplorer: {
+			search: (s: string) => Promise<null | Object>,
+			load: (s: string) => Promise<null | Object>
+		},
+		_fullExplorer: {
+			search: (s: string) => Promise<null | Object>,
+			load: (s: string) => Promise<null | Object>
+		},
+		_configCache: Map<string, Object>,
+		_specifiedConfigCache: Map<StylelintConfig, Object>,
+		_postcssResultCache: Map<string, Result>,
+
+		_augmentConfig: Function,
+		_getPostcssResult: Function,
+		_lintSource: Function,
+		_createStylelintResult: Function,
+		_createEmptyPostcssResult?: Function,
+
+		getConfigForFile: (s?: string) => Promise<{ config: StylelintConfig, filepath: string } | null>,
+		isPathIgnored: (s?: string) => Promise<boolean>,
+		lintSource: Function
+	};
+
+	export type StylelintWarning = {
+		line: number,
+		column: number,
+		rule: string,
+		severity: string,
+		text: string
+	};
+
+	export type StylelintResult = {
+		source?: string,
+		deprecations: Array<{
+			text: string,
+			reference: string
+		}>,
+		invalidOptionWarnings: Array<{
+			text: string
+		}>,
+		parseErrors: Array<StylelintWarning>,
+		errored?: boolean,
+		warnings: Array<StylelintWarning>,
+		ignored?: boolean,
+		_postcssResult?: PostcssResult
+	};
+
+	export type StylelintCssSyntaxError = {
+		column: number,
+		file?: string,
+		input: {
+			column: number,
+			file?: string,
+			line: number,
+			source: string
+		},
+		line: number,
+		message: string,
+		name: string,
+		reason: string,
+		source: string
+	};
+
+	export type StylelintDisableOptionsReport = Array<{
+		source?: string,
+		ranges: Array<{
+			unusedRule: string,
+			start: number,
+			end?: number
+		}>
+	}>;
+
+	export type StylelintStandaloneReturnValue = {
+		results: Array<StylelintResult>,
+		errored: boolean,
+		output: any,
+		maxWarningsExceeded?: {
+			maxWarnings: number,
+			foundWarnings: number
+		},
+		needlessDisables?: StylelintDisableOptionsReport,
+		invalidScopeDisables?: StylelintDisableOptionsReport
+	};
+
+	export type StylelintStandaloneOptions = {
+		files?: string | Array<string>,
+		globbyOptions?: Object,
+		cache?: boolean,
+		cacheLocation?: string,
+		code?: string,
+		codeFilename?: string,
+		config?: StylelintConfig,
+		configFile?: string,
+		configBasedir?: string,
+		configOverrides?: Object,
+		printConfig?: string,
+		ignoreDisables?: boolean,
+		ignorePath?: string,
+		ignorePattern?: RegExp,
+		reportNeedlessDisables?: boolean,
+		reportInvalidScopeDisables?: boolean,
+		maxWarnings?: number,
+		syntax?: StylelintSyntaxes,
+		customSyntax?: string,
+		formatter?: "compact" | "json" | "string" | "unix" | "verbose" | Function,
+		disableDefaultIgnores?: boolean,
+		fix?: boolean,
+		allowEmptyInput: boolean
 	};
 }
