369f6fe1540f34e49e76bd1557545b7572bc4dc4
Ivan Kopeykin <ikopeykin@ozon.ru>
2019-10-18 11:01:32 +0000

    Add types for stylelint config and PostCSS result (#4361)
    * add types to getPostcssResult
    
    * add types to createStylelint
    
    * add types to getConfigForFile
    
    * fix getPostcssResult types
    
    * add types to createPlugin
    
    * add types to isPathIgnored
    
    * add types to printConfig
    
    * fix types for getConfigForFile
    
    * add types to augmentConfig
    
    * fix stylelint types
    
    * improve stylelint types further
    
    * use one type for cosmiconfig result
    
    * add types to normalizeRuleSettings

--- types/stylelint/index.d.ts
+++ types/stylelint/index.d.ts
@@ -1,5 +1,35 @@
 declare module 'stylelint' {
-	import { Result, WarningOptions } from 'postcss';
+	import {Result, WarningOptions} from 'postcss';
+
+	export type StylelintConfigExtends = string | Array<string>;
+	export type StylelintConfigPlugins = string | Array<string>;
+	export type StylelintConfigProcessors =
+		| string
+		| Array<string | [string, Object]>;
+	export type StylelintConfigIgnoreFiles = string | Array<string>;
+	export type StylelintConfigRuleSettings = any | [any, Object];
+	export type StylelintConfigRules = {
+		[ruleName: string]: StylelintConfigRuleSettings
+	};
+
+	export type StylelintConfig = {
+		extends?: StylelintConfigExtends,
+		plugins?: StylelintConfigPlugins,
+		pluginFunctions?: {
+			[pluginName: string]: Function
+		},
+		processors?: StylelintConfigProcessors,
+		processorFunctions?: Array<Function>,
+		ignoreFiles?: StylelintConfigIgnoreFiles,
+		ignorePatterns?: string,
+		rules?: StylelintConfigRules,
+		codeProcessors?: Array<Function>,
+		resultProcessors?: Array<Function>,
+		quiet?: boolean,
+		defaultSeverity?: string
+	};
+
+	export type CosmiconfigResult = { config: StylelintConfig, filepath: string };
 
 	export type DisabledRange = {
 		start: number,
@@ -24,6 +54,79 @@ declare module 'stylelint' {
 
 	export type PostcssResult = Result & {
 		stylelint: StylelintPostcssResult,
-		warn(message: string, options?: WarningOptions & {stylelintType?: string}): void;
+		warn(message: string, options?: WarningOptions & { stylelintType?: string }): void;
+	};
+
+	export type StylelintOptions = {
+		config?: StylelintConfig,
+		configFile?: string,
+		configBasedir?: string,
+		configOverrides?: Object,
+		ignoreDisables?: boolean,
+		ignorePath?: string,
+		reportInvalidScopeDisables?: boolean,
+		reportNeedlessDisables?: boolean,
+		syntax?: string,
+		customSyntax?: string,
+		fix?: boolean
+	};
+
+	export type GetPostcssOptions = {
+		code?: string,
+		codeFilename?: string,
+		filePath?: string,
+		codeProcessors?: Array<Function>,
+		syntax?: string,
+		customSyntax?: string
+	};
+
+	export type StylelintInternalApi = {
+		_options: StylelintOptions,
+		_extendExplorer: {
+			search: (s: string) => Promise<null | CosmiconfigResult>,
+			load: (s: string) => Promise<null | CosmiconfigResult>
+		},
+		_fullExplorer: {
+			search: (s: string) => Promise<null | CosmiconfigResult>,
+			load: (s: string) => Promise<null | CosmiconfigResult>
+		},
+		_configCache: Map<string, Object>,
+		_specifiedConfigCache: Map<StylelintConfig, Object>,
+		_postcssResultCache: Map<string, Result>,
+
+		_getPostcssResult: (options?: GetPostcssOptions) => Promise<Result>,
+		_lintSource: Function,
+		_createStylelintResult: Function,
+		_createEmptyPostcssResult?: Function,
+
+		getConfigForFile: (s?: string) => Promise<{ config: StylelintConfig, filepath: string } | null>,
+		isPathIgnored: (s?: string) => Promise<boolean>,
+		lintSource: Function
+	};
+
+	export type StylelintStandaloneOptions = {
+		files?: string | Array<string>,
+		globbyOptions?: Object,
+		cache?: boolean,
+		cacheLocation?: string,
+		code?: string,
+		codeFilename?: string,
+		config?: StylelintConfig,
+		configFile?: string,
+		configBasedir?: string,
+		configOverrides?: Object,
+		printConfig?: string,
+		ignoreDisables?: boolean,
+		ignorePath?: string,
+		ignorePattern?: RegExp,
+		reportNeedlessDisables?: boolean,
+		reportInvalidScopeDisables?: boolean,
+		maxWarnings?: number,
+		syntax?: string,
+		customSyntax?: string,
+		formatter?: "compact" | "json" | "string" | "unix" | "verbose" | Function,
+		disableDefaultIgnores?: boolean,
+		fix?: boolean,
+		allowEmptyInput: boolean
 	};
 }
