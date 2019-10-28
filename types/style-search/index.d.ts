abd8dace18480b7929c8baf870e61e958e19c079
Ivan Kopeykin <ikopeykin@ozon.ru>
2019-10-28 22:57:54 +0300

    feat(style-search): provide types

--- types/style-search/index.d.ts
+++ types/style-search/index.d.ts
@@ -1,5 +1,17 @@
-declare module 'style-search' {
-	var def: any; // TODO TYPES
+declare function searchFn(
+	param: {
+		source: string,
+		target: string | string[],
+		once?: boolean,
+		comments?: 'skip' | 'check' | 'only',
+		strings?: 'skip' | 'check' | 'only',
+		functionNames?: 'skip' | 'check' | 'only',
+		functionArguments?: 'skip' | 'check' | 'only',
+		parentheticals?: 'skip' | 'check' | 'only'
+	},
+	callback: (param: {startIndex: number, endIndex: number}) => void
+): void
 
-	export = def;
+declare module 'style-search' {
+	export = searchFn;
 }
