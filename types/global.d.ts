160c6ae1af03640141754f875731822683297df1
Richard Hallows <jeddy3@users.noreply.github.com>
2020-05-03 09:07:18 +0100

    Standardise lazy requires (#4729)

---
+++ types/global.d.ts
@@ -0,0 +1,9 @@
+declare module '@stylelint/postcss-css-in-js';
+declare module '@stylelint/postcss-markdown';
+declare module 'import-lazy';
+declare module 'postcss-html';
+declare module 'postcss-less';
+declare module 'postcss-sass';
+declare module 'postcss-scss';
+declare module 'sugarss';
+declare module 'table';
