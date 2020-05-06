5b2dda5c49ac1fdfa86b0d8cca7f6bb976cf8102
jeddy3 <jeddy3@users.noreply.github.com>
2020-05-06 10:09:27 +0100

    Refactor system tests

--- system-tests/systemTestUtils.js
+++ system-tests/systemTestUtils.js
@@ -1,47 +1,73 @@
 'use strict';
 
+const _ = require('lodash');
+const fs = require('fs');
+const os = require('os');
 const path = require('path');
-const stripAnsi = require('strip-ansi');
+const { promisify } = require('util');
 
-function caseFilePath(caseNumber, fileName) {
+const replaceBackslashes = require('../lib/testUtils/replaceBackslashes');
+
+const copyFile = promisify(fs.copyFile);
+const readFile = promisify(fs.readFile);
+
+function caseFilePath(caseNumber, fileName = 'stylesheet') {
 	return replaceBackslashes(path.join(__dirname, caseNumber, fileName));
 }
 
-function caseStylesheetGlob(caseNumber) {
+function caseFiles(caseNumber) {
 	return caseFilePath(caseNumber, 'stylesheet.*');
 }
 
-function caseConfig(caseNumber, ext = 'json') {
+function caseConfigFile(caseNumber, ext = 'json') {
 	return caseFilePath(caseNumber, `config.${ext}`);
 }
 
-function prepResults(results) {
-	return results.map((result) => {
+async function caseConfig(caseNumber) {
+	return JSON.parse(await readFile(caseConfigFile(caseNumber, 'json'), 'utf8'));
+}
+
+async function caseCode(caseNumber, ext = 'css') {
+	return await readFile(`${caseFilePath(caseNumber)}.${ext}`, 'utf8');
+}
+
+async function caseFilesForFix(caseNumber, ext = 'css') {
+	const tempPath = replaceBackslashes(path.join(os.tmpdir(), `stylesheet-${_.uniqueId()}.${ext}`));
+
+	await copyFile(path.join(__dirname, caseNumber, `stylesheet.${ext}`), tempPath);
+
+	return tempPath;
+}
+
+function prepForSnapshot({ results, output, ...rest }) {
+	// If output isn't fixed code
+	if (output.startsWith('[')) {
+		// The `source` of each file varies between platforms or if a tmp file is used
+		output = JSON.parse(output).map((warning) => {
+			delete warning.source;
+
+			return warning;
+		});
+	}
+
+	return {
 		// The _postcssResult object is not part of our API and is huge
-		const { _postcssResult, source, ...preppedResult } = result;
+		results: results.map(({ _postcssResult, ...rest }) => {
+			delete rest.source;
 
-		// The `source` of each file will not be the same on different machines or platforms
-		preppedResult.source = path
-			.relative(__dirname, source)
-			.replace(new RegExp(`\\${path.sep}`, 'g'), '/');
-
-		return preppedResult;
-	});
-}
-
-function replaceBackslashes(str) {
-	return str.replace(/\\/g, '/');
-}
-
-function stripColors(input) {
-	return stripAnsi(input);
+			return rest;
+		}),
+		output,
+		...rest,
+	};
 }
 
 module.exports = {
-	caseFilePath,
-	replaceBackslashes,
-	caseStylesheetGlob,
+	caseCode,
 	caseConfig,
-	prepResults,
-	stripColors,
+	caseConfigFile,
+	caseFilePath,
+	caseFilesForFix,
+	caseFiles,
+	prepForSnapshot,
 };
