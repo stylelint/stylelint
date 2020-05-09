07bef23835969bc75fb16acc68dcc9c83e675ae2
Aleks Hudochenkov <aleks@hudochenkov.com>
2020-05-09 21:58:33 +0200

    Restore getTestRule

--- jest-setup.js
+++ jest-setup.js
@@ -1,3 +1,8 @@
 'use strict';
 
+const getTestRule = require('jest-preset-stylelint/getTestRule');
+const stylelint = require('./lib');
+
 jest.mock('./lib/utils/getOsEol', () => () => '\n');
+
+global.testRule = getTestRule(stylelint);
