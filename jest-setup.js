b124d151eeb183a4afd56549d759a50a241829eb
Aleks Hudochenkov <aleks@hudochenkov.com>
2020-05-10 00:14:00 +0200

    Use new preset

--- jest-setup.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const getTestRule = require('jest-preset-stylelint/getTestRule');
-const stylelint = require('./lib');
-
-jest.mock('./lib/utils/getOsEol', () => () => '\n');
-
-global.testRule = getTestRule(stylelint);
