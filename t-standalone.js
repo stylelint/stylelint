c8dd0a9a9f61f47ab85dc65fa9e070c4ba894e41
m-allanson <michael.allanson@gmail.com>
2020-06-18 22:32:31 +0100

    Update parcel 2 from alpha to beta version

---
+++ t-standalone.js
@@ -0,0 +1,18 @@
+// 'use strict';
+
+// const stylelint = require('./lib/index.js');
+
+// async function run() {
+// 	const result = await stylelint.lint({
+// 		code: `a { color: #fff; }`,
+// 		config: {
+// 			rules: {
+// 				'color-hex-length': 'long',
+// 			},
+// 		},
+// 	});
+
+// 	console.log(result);
+// }
+
+// run();
