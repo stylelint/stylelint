f56bf980d564a5cec27e9ccc03a9292956a4e2ee
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-04-10 02:23:44 +0900

    Use built-in type definitions of `import-lazy`
    This change uses the built-in type definitions of the `import-lazy` package.
    
    `importLazy()` accepts an argument as a module ID:
    https://github.com/sindresorhus/import-lazy/blob/v4.0.0/index.js#L2-L3

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,6 +1,5 @@
 declare module '@stylelint/postcss-css-in-js';
 declare module '@stylelint/postcss-markdown';
-declare module 'import-lazy';
 declare module 'postcss-html';
 declare module 'postcss-sass';
 declare module 'sugarss';
