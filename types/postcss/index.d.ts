4b0b28c1af089576837517d88cbc781d6a858635
Aleks Hudochenkov <aleks@hudochenkov.com>
2021-06-07 10:01:18 +0200

    Fix types issues after PostCSS update (#5346)

--- types/postcss/index.d.ts
+++ types/postcss/index.d.ts
@@ -1,51 +1,14 @@
 declare module 'postcss/lib/comment' {
-	import { Comment, NodeRaws } from 'postcss';
+	import { Comment } from 'postcss';
 
-	interface NodeRawsExt extends NodeRaws {
-		// Used by the SCSS parser to indicate `//` comments.
-		inline?: boolean;
-	}
-
-	interface CommentExt extends Comment {
+	interface CommentExtended extends Comment {
 		// Used by the Less parser to indicate `//` comments.
 		inline?: boolean;
-		raws: NodeRawsExt;
+		raws: Comment['raws'] & {
+			// Used by the SCSS parser to indicate `//` comments.
+			inline?: boolean;
+		};
 	}
 
-	export = CommentExt;
-}
-
-declare module 'postcss/lib/lazy-result' {
-	import {
-		LazyResult,
-		Result,
-		Processor,
-		ResultMap,
-		ResultMessage,
-		ResultOptions,
-		Root,
-		Warning,
-	} from 'postcss';
-
-	class LazyResultImpl extends Promise<Result> implements LazyResult {
-		constructor(p: Processor, code: string, options: { from?: string; syntax: any });
-
-		content: string;
-		css: string;
-		map: ResultMap;
-		messages: ResultMessage[];
-		opts: ResultOptions;
-		processor: Processor;
-		root: Root;
-
-		warnings(): Warning[];
-	}
-
-	export = LazyResultImpl;
-}
-
-declare module 'postcss/lib/result' {
-	import { Result } from 'postcss';
-
-	export = Result;
+	export = CommentExtended;
 }
