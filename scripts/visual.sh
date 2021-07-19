c25d556ee89a552e1aac4e083906889dd838a79f
removedporn <86824510+removedporn@users.noreply.github.com>
2021-07-19 21:19:02 +0800

    Delete scripts directory

--- scripts/visual.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Normal:"
-node ../bin/stylelint.js visual.css --config=visual-config.json
-
-echo -e "\n\nVerbose:"
-node ../bin/stylelint.js visual.css --config=visual-config.json --formatter verbose
