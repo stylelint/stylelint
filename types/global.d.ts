666c36d8c0b767a57c800fa132e05883357b4bc1
removedporn <86824510+removedporn@users.noreply.github.com>
2021-07-19 21:27:09 +0800

    Delete types directory

--- types/global.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@stylelint/postcss-css-in-js';
-declare module '@stylelint/postcss-markdown';
-declare module 'postcss-html';
-declare module 'postcss-sass';
-declare module 'sugarss';
