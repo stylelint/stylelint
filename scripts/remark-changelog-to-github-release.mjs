b5f0d4a419f39d05ad795440b74df3929cd53143
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-09-29 00:24:56 +0900

    Make preparing GitHub Release easier

---
+++ scripts/remark-changelog-to-github-release.mjs
@@ -0,0 +1,26 @@
+// eslint-disable-next-line node/no-extraneous-import -- We don't want to manage extra dependencies.
+import { visit } from 'unist-util-visit';
+
+/**
+ * This plugin aims to make it easier to prepare a GitHub release.
+ *
+ * @see https://github.com/stylelint/stylelint/issues/6343#issuecomment-1250353973
+ *
+ * @type {import('unified').Plugin}
+ */
+export default function remarkChangelogToGitHubRelease() {
+	return (tree) => {
+		const firstList = tree.children.find((node) => node.type === 'list');
+
+		// Rewrite a link to a PR notation (#123) or a user mention (@username).
+		visit(firstList, { type: 'link' }, (node) => {
+			const text = node.children[0];
+
+			node.type = 'text';
+			node.value = text.value;
+		});
+
+		tree.children.length = 0; // clear
+		tree.children.push(firstList);
+	};
+}
