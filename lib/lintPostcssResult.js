stylelint/stylelint

commit 6ab4199af4fc00f04b051064523adcb252eb4fa3

Add disableFix secondary option (#5460)

lib/lintPostcssResult.js
@@ -87,6 +87,12 @@ function lintPostcssResult(stylelintOptions, postcssResult, config) {
 
 		// Log the rule's severity in the PostCSS result
 		const defaultSeverity = config.defaultSeverity || 'error';
+		// disableFix in secondary option
+		const disableFix = (secondaryOptions && secondaryOptions.disableFix === true) || false;
+
+		if (disableFix) {
+			postcssResult.stylelint.ruleDisableFix = true;
+		}
 
 		postcssResult.stylelint.ruleSeverities[ruleName] =
 			(secondaryOptions && secondaryOptions.severity) || defaultSeverity;
@@ -97,6 +103,7 @@ function lintPostcssResult(stylelintOptions, postcssResult, config) {
 				postcssRoots.map((postcssRoot) =>
 					ruleFunction(primaryOption, secondaryOptions, {
 						fix:
+							!disableFix &&
 							stylelintOptions.fix &&
 							// Next two conditionals are temporary measures until #2643 is resolved
 							isFileFixCompatible &&

lib/standalone.js
@@ -151,7 +151,12 @@ module.exports = function (options) {
 				const postcssResult = stylelintResult._postcssResult;
 				const returnValue = prepareReturnValue([stylelintResult], options, formatterFunction);
 
-				if (options.fix && postcssResult && !postcssResult.stylelint.ignored) {
+				if (
+					options.fix &&
+					postcssResult &&
+					!postcssResult.stylelint.ignored &&
+					!postcssResult.stylelint.ruleDisableFix
+				) {
 					if (!postcssResult.stylelint.disableWritingFix) {
 						// If we're fixing, the output should be the fixed code
 						returnValue.output = postcssResult.root.toString(postcssResult.opts.syntax);
@@ -251,6 +256,7 @@ module.exports = function (options) {
 							postcssResult.root &&
 							postcssResult.opts &&
 							!postcssResult.stylelint.ignored &&
+							!postcssResult.stylelint.ruleDisableFix &&
 							options.fix &&
 							!postcssResult.stylelint.disableWritingFix
 						) {

types/stylelint/index.d.ts
@@ -94,6 +94,7 @@ declare module 'stylelint' {
 		stylelintError?: boolean;
 		disableWritingFix?: boolean;
 		config?: StylelintConfig;
+		ruleDisableFix?: boolean;
 	};
 
 	type EmptyResult = {

lib/__tests__/standalone-fix.test.js
@@ -225,3 +225,96 @@ describe('writing fixes to files', () => {
 		expect(fileContent.startsWith('\uFEFF')).toBe(true);
 	});
 });
+
+it('one rule being disabled', async () => {
+	const code = `
+		a {
+			color: red;
+		}`;
+
+	const result = await standalone({
+		code,
+		config: {
+			rules: {
+				indentation: [
+					2,
+					{
+						disableFix: true,
+					},
+				],
+			},
+		},
+		fix: true,
+	});
+
+	expect(JSON.parse(result.output)[0].errored).toBe(true);
+});
+
+it('two rules being disabled', async () => {
+	const code = `
+		a {
+			COLOR: red;
+		}`;
+
+	const result = await standalone({
+		code,
+		config: {
+			rules: {
+				indentation: [
+					2,
+					{
+						disableFix: true,
+					},
+				],
+				'property-case': [
+					'lower',
+					{
+						disableFix: true,
+					},
+				],
+			},
+		},
+		fix: true,
+	});
+
+	const warnings = JSON.parse(result.output)[0].warnings;
+
+	expect(warnings.some((w) => w.text === 'Expected indentation of 0 spaces (indentation)')).toBe(
+		true,
+	);
+	expect(warnings.some((w) => w.text === 'Expected "COLOR" to be "color" (property-case)')).toBe(
+		true,
+	);
+});
+
+it('one rule being disabled and another still autofixing', async () => {
+	const code = `
+		a {
+			COLOR: red;
+		}`;
+
+	const result = await standalone({
+		code,
+		config: {
+			rules: {
+				indentation: [
+					2,
+					{
+						disableFix: true,
+					},
+				],
+				'property-case': ['lower'],
+			},
+		},
+		fix: true,
+	});
+
+	const warnings = JSON.parse(result.output)[0].warnings;
+
+	expect(warnings.some((w) => w.text === 'Expected indentation of 0 spaces (indentation)')).toBe(
+		true,
+	);
+	expect(warnings.some((w) => w.text === 'Expected "COLOR" to be "color" (property-case)')).toBe(
+		false,
+	);
+});

lib/utils/validateOptions.js
@@ -3,7 +3,7 @@
 const arrayEqual = require('./arrayEqual');
 const { isPlainObject } = require('is-plain-object');
 
-const IGNORED_OPTIONS = new Set(['severity', 'message', 'reportDisables']);
+const IGNORED_OPTIONS = new Set(['severity', 'message', 'reportDisables', 'disableFix']);
 
 /** @typedef {(value: unknown) => boolean} PossibleFunc */
 

commit 8f972dabba93f339e48a53f26a83e101966e625e

Fix Unexpected Node API output when any rule contains `disableFix: true` #6542
fixes #6542 and bv that should also fix https://github.com/stylelint/vscode-stylelint/issues/369

Issue: The API did not return the fixed css in `output` if any rule contained `ruleDisableFix`.

I have removed the `ruleDisableFix` code, because it did not affect the code fixing itself and was assumingly wrongly used to change the content format of `output`.
I have adjusted some tests to adapt to the changed behaviour and extended the test conditions to check that the returned code has the correct fixes applied.

Issue was introduced in https://github.com/stylelint/stylelint/pull/5460

lib/lintPostcssResult.js
@@ -91,10 +91,6 @@ function lintPostcssResult(stylelintOptions, postcssResult, config) {
 		// disableFix in secondary option
 		const disableFix = (secondaryOptions && secondaryOptions.disableFix === true) || false;
 
-		if (disableFix) {
-			postcssResult.stylelint.ruleDisableFix = true;
-		}
-
 		postcssResult.stylelint.ruleSeverities[ruleName] =
 			(secondaryOptions && secondaryOptions.severity) || defaultSeverity;
 		postcssResult.stylelint.customMessages[ruleName] = secondaryOptions && secondaryOptions.message;

lib/standalone.js
@@ -139,12 +139,7 @@ async function standalone({
 		const postcssResult = stylelintResult._postcssResult;
 		const returnValue = prepareReturnValue([stylelintResult], maxWarnings, formatterFunction, cwd);
 
-		if (
-			fix &&
-			postcssResult &&
-			!postcssResult.stylelint.ignored &&
-			!postcssResult.stylelint.ruleDisableFix
-		) {
+		if (fix && postcssResult && !postcssResult.stylelint.ignored) {
 			returnValue.output =
 				!postcssResult.stylelint.disableWritingFix && postcssResult.opts
 					? // If we're fixing, the output should be the fixed code

types/stylelint/index.d.ts
@@ -110,7 +110,6 @@ declare module 'stylelint' {
 			stylelintWarning?: boolean;
 			disableWritingFix?: boolean;
 			config?: Config;
-			ruleDisableFix?: boolean;
 		};
 
 		type EmptyResult = {

lib/__tests__/standalone-fix.test.js
@@ -246,7 +246,8 @@ it('one rule being disabled', async () => {
 		fix: true,
 	});
 
-	expect(JSON.parse(result.output)[0].errored).toBe(true);
+	expect(result.results[0].errored).toBe(true);
+	expect(result.output).toBe('\n		a {\n			color: red;\n		}');
 });
 
 it('two rules being disabled', async () => {
@@ -276,7 +277,7 @@ it('two rules being disabled', async () => {
 		fix: true,
 	});
 
-	const warnings = JSON.parse(result.output)[0].warnings;
+	const warnings = result.results[0].warnings;
 
 	expect(warnings.some((w) => w.text === 'Expected indentation of 0 spaces (indentation)')).toBe(
 		true,
@@ -284,6 +285,7 @@ it('two rules being disabled', async () => {
 	expect(warnings.some((w) => w.text === 'Expected "COLOR" to be "color" (property-case)')).toBe(
 		true,
 	);
+	expect(result.output).toBe('\n		a {\n			COLOR: red;\n		}');
 });
 
 it('one rule being disabled and another still autofixing', async () => {
@@ -296,24 +298,25 @@ it('one rule being disabled and another still autofixing', async () => {
 		code,
 		config: {
 			rules: {
-				indentation: [
-					2,
+				indentation: [0],
+				'property-case': [
+					'lower',
 					{
 						disableFix: true,
 					},
 				],
-				'property-case': ['lower'],
 			},
 		},
 		fix: true,
 	});
 
-	const warnings = JSON.parse(result.output)[0].warnings;
+	const warnings = result.results[0].warnings;
 
 	expect(warnings.some((w) => w.text === 'Expected indentation of 0 spaces (indentation)')).toBe(
-		true,
-	);
-	expect(warnings.some((w) => w.text === 'Expected "COLOR" to be "color" (property-case)')).toBe(
 		false,
 	);
+	expect(warnings.some((w) => w.text === 'Expected "COLOR" to be "color" (property-case)')).toBe(
+		true,
+	);
+	expect(result.output).toBe('\na {\nCOLOR: red;\n}');
 });