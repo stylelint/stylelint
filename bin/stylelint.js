107bc5f9ffeddcc58be69e146c6030f1467468bf
Daniel Fischer <daniel@d-fischer.dev>
2023-06-05 21:59:49 +0200

    Comment out v8-compile-cache usage

--- bin/stylelint.js
+++ bin/stylelint.js
@@ -3,6 +3,7 @@
 'use strict';
 
 // to use V8's code cache to speed up instantiation time
-require('v8-compile-cache');
+// (in its current state, this breaks any dynamic imports)
+// require('v8-compile-cache');
 
 require('../lib/cli')(process.argv.slice(2));
