681878a73e5b1709e13bb190dcaec1a6f3bda50b
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-07-04 23:11:55 +0900

    Run `npm run build-check` on CI

--- scripts/build-check.mjs
+++ scripts/build-check.mjs
@@ -1,12 +1,8 @@
-import { env, exit, stderr, stdout } from 'node:process';
+import { exit, stderr, stdout } from 'node:process';
 import { EOL } from 'node:os';
 import { execSync } from 'node:child_process';
 
 function main() {
-	if (!env.CI) {
-		return;
-	}
-
 	try {
 		execSync('git diff --exit-code --name-only -- lib', { encoding: 'utf8' });
 	} catch (error) {
