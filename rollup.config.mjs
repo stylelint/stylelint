6833422d1c212505b67726046b7c8ab45ace83fe
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-07-07 16:56:50 +0900

    Not build `lib/cli.mjs` to prevent a build error

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,13 @@
 import fg from 'fast-glob';
 
-const inputFiles = fg.globSync(['lib/**/*.mjs', '!**/__tests__/**', '!lib/testUtils/**']);
+const inputFiles = fg.globSync([
+	'lib/**/*.mjs',
+	'!**/__tests__/**',
+	'!lib/testUtils/**',
+
+	// NOTE: We cannot support CJS for `cli.mjs` since the `meow` dependency is pure ESM.
+	'!lib/cli.mjs',
+]);
 
 export default inputFiles.map((input) => {
 	return {
