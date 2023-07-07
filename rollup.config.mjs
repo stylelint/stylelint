2adec1a5f2ac79987a3b9d264346ea9e2899479f
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-07-07 17:02:20 +0900

    Clean up `*.cjs` before build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,3 +1,5 @@
+import { rmSync } from 'node:fs';
+
 import fg from 'fast-glob';
 
 const inputFiles = fg.globSync([
@@ -8,6 +10,11 @@ const inputFiles = fg.globSync([
 	// NOTE: We cannot support CJS for `cli.mjs` since the `meow` dependency is pure ESM.
 	'!lib/cli.mjs',
 ]);
+
+// clean up
+for (const input of inputFiles) {
+	rmSync(input.replace('.mjs', '.cjs'), { force: true });
+}
 
 export default inputFiles.map((input) => {
 	return {
