b8c25a155de54ea850a9dfcd38aaf59d3c7c8e05
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-12-10 23:48:45 +0900

    Avoid segmentation fault on Node.js 18

--- system-tests/006/fs.test.mjs
+++ system-tests/006/fs.test.mjs
@@ -1,16 +1,28 @@
+import process from 'node:process';
+
 import stylelint from '../../lib/index.mjs';
 
 import { caseConfigFile, caseFiles, prepForSnapshot } from '../systemTestUtils.mjs';
 
 const CASE_NUMBER = '006';
 
-it('fs - import packages', async () => {
-	expect(
-		prepForSnapshot(
-			await stylelint.lint({
-				files: caseFiles(CASE_NUMBER),
-				configFile: caseConfigFile(CASE_NUMBER),
-			}),
-		),
-	).toMatchSnapshot();
-}, 10000);
+// TODO: This test fails due to SIGSEGV on Node.js 18 for some reason.
+// Remove the skip when dropping the support for Node.js 18.
+const [nodeMajorVersion] = process.versions.node.split('.', 1);
+const testFn = Number(nodeMajorVersion) >= 20 ? test : test.skip;
+
+testFn(
+	'fs - import packages',
+	async () => {
+		// eslint-disable-next-line jest/no-standalone-expect -- False positive.
+		expect(
+			prepForSnapshot(
+				await stylelint.lint({
+					files: caseFiles(CASE_NUMBER),
+					configFile: caseConfigFile(CASE_NUMBER),
+				}),
+			),
+		).toMatchSnapshot();
+	},
+	10000,
+);
--- system-tests/006/no-fs.test.mjs
+++ system-tests/006/no-fs.test.mjs
@@ -1,16 +1,28 @@
+import process from 'node:process';
+
 import stylelint from '../../lib/index.mjs';
 
 import { caseCode, caseConfig, prepForSnapshot } from '../systemTestUtils.mjs';
 
 const CASE_NUMBER = '006';
 
-it('no-fs - import packages', async () => {
-	expect(
-		prepForSnapshot(
-			await stylelint.lint({
-				code: await caseCode(CASE_NUMBER),
-				config: await caseConfig(CASE_NUMBER),
-			}),
-		),
-	).toMatchSnapshot();
-}, 10000);
+// TODO: This test fails due to SIGSEGV on Node.js 18 for some reason.
+// Remove the skip when dropping the support for Node.js 18.
+const [nodeMajorVersion] = process.versions.node.split('.', 1);
+const testFn = Number(nodeMajorVersion) >= 20 ? test : test.skip;
+
+testFn(
+	'no-fs - import packages',
+	async () => {
+		// eslint-disable-next-line jest/no-standalone-expect -- False positive.
+		expect(
+			prepForSnapshot(
+				await stylelint.lint({
+					code: await caseCode(CASE_NUMBER),
+					config: await caseConfig(CASE_NUMBER),
+				}),
+			),
+		).toMatchSnapshot();
+	},
+	10000,
+);
