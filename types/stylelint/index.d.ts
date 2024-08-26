91821d17e972f1228e2dbe073e1dce9111f0d2c7
Gary Gozlan <Mouvedia@users.noreply.github.com>
2024-08-26 10:35:17 +0200

    revert

--- types/stylelint/index.d.ts
+++ types/stylelint/index.d.ts
@@ -246,7 +246,7 @@ declare namespace stylelint {
 	/** @internal */
 	export type RuleBase<P = any, S = any> = (
 		primaryOption: P,
-		secondaryOptions: S extends Record<string, any> ? S : never,
+		secondaryOptions: S,
 		context: RuleContext,
 	) => (root: PostCSS.Root, result: PostcssResult) => Promise<void> | void;
 
