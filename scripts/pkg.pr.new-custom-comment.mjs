b93ca4a5ecade496548ac96e24a0ebf91be0316a
yosuke ota <otameshiyo23@gmail.com>
2025-01-26 18:49:35 +0900

    Introduce pkg.pr.new

---
+++ scripts/pkg.pr.new-custom-comment.mjs
@@ -0,0 +1,136 @@
+import LZString from 'lz-string';
+
+/**
+ * Used in `/.github/workflows/pkg.pr.new.yml`
+ */
+export async function postCustomComment({ github, context, output }) {
+	// eslint-disable-next-line no-console -- For debugging on github actions.
+	console.log('pkg-pr-new publish output:', JSON.stringify(output));
+
+	const sha =
+		context.eventName === 'pull_request'
+			? context.payload.pull_request.head.sha
+			: context.payload.after;
+	const commitUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${sha}`;
+
+	const pullRequestNumber = await getPullRequestNumber();
+
+	const packages = output.packages.map((p) => {
+		let normalizedUrl = p.url;
+
+		if (pullRequestNumber && p.url.endsWith(sha)) {
+			normalizedUrl = `${p.url.slice(0, -sha.length)}${pullRequestNumber}`;
+		}
+
+		const repoPath = `/${context.repo.owner}/${context.repo.repo}/`;
+
+		normalizedUrl = normalizedUrl.replace(repoPath, '/');
+
+		return {
+			name: p.name,
+			url: normalizedUrl,
+		};
+	});
+
+	const botCommentIdentifier = '<!-- posted by pkg.pr.new-custom-comment.mjs -->';
+
+	const onlineUrl = new URL('https://stylelint.io/demo/');
+	const deps = {
+		stylelint: 'latest',
+		'stylelint-config-standard': 'latest',
+	};
+
+	for (const p of packages) {
+		deps[p.name] = p.url;
+	}
+
+	onlineUrl.hash = compress({ deps });
+	const body = `${botCommentIdentifier}
+
+## Try the Instant Preview in Online Demo
+
+[Stylelint Online Demo](${onlineUrl})
+
+## Install the Instant Preview to Your Local
+
+\`\`\`
+npm i -D ${packages.map((p) => p.url).join(' ')}
+\`\`\`
+
+---
+
+[View Commit](${commitUrl})`;
+
+	if (pullRequestNumber) {
+		await createOrUpdateComment(pullRequestNumber);
+	} else {
+		/* eslint-disable no-console -- For debugging on github actions. */
+		console.log(
+			'No open pull request found for this push. Logging publish information to console:',
+		);
+		console.log(`\n${'='.repeat(50)}`);
+		console.log(body);
+		console.log(`\n${'='.repeat(50)}`);
+		/* eslint-enable no-console -- For debugging on github actions. */
+	}
+
+	async function getPullRequestNumber() {
+		if (context.eventName === 'pull_request') {
+			if (context.issue.number) {
+				return context.issue.number;
+			}
+		} else if (context.eventName === 'push') {
+			const pullRequests = await github.rest.pulls.list({
+				owner: context.repo.owner,
+				repo: context.repo.repo,
+				state: 'open',
+				head: `${context.repo.owner}:${context.ref.replace('refs/heads/', '')}`,
+			});
+
+			if (pullRequests.data.length > 0) {
+				return pullRequests.data[0].number;
+			}
+		}
+
+		return null;
+	}
+
+	async function findBotComment(issueNumber) {
+		const comments = await github.rest.issues.listComments({
+			owner: context.repo.owner,
+			repo: context.repo.repo,
+			issue_number: issueNumber,
+		});
+
+		return comments.data.find((comment) => comment.body.includes(botCommentIdentifier));
+	}
+
+	async function createOrUpdateComment(issueNumber) {
+		const existingComment = await findBotComment(issueNumber);
+
+		if (existingComment) {
+			await github.rest.issues.updateComment({
+				owner: context.repo.owner,
+				repo: context.repo.repo,
+				comment_id: existingComment.id,
+				body,
+			});
+		} else {
+			await github.rest.issues.createComment({
+				issue_number: issueNumber,
+				owner: context.repo.owner,
+				repo: context.repo.repo,
+				body,
+			});
+		}
+	}
+}
+
+function compress(data) {
+	try {
+		return LZString.compressToEncodedURIComponent(JSON.stringify(data));
+	} catch {
+		// return silently
+		return '';
+	}
+}
