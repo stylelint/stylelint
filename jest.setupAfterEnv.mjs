d56208999d82b5ee9a2a019bc9ece7697505ed9f
Ryo Matsukawa <76232929+ryo-manba@users.noreply.github.com>
2025-05-24 17:49:17 +0900

    Add emit experimental warnings

--- jest.setupAfterEnv.mjs
+++ jest.setupAfterEnv.mjs
@@ -1,5 +1,7 @@
 import { clearEmittedDeprecationWarnings } from './lib/utils/emitDeprecationWarning.mjs';
+import { clearEmittedExperimentalWarnings } from './lib/utils/emitExperimentalWarning.mjs';
 
 afterEach(() => {
 	clearEmittedDeprecationWarnings();
+	clearEmittedExperimentalWarnings();
 });
