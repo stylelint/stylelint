3f0ed8ec5928ae664b8478ed0b507cbc84ceadd0
jeddy3 <jeddy3@users.noreply.github.com>
2025-12-02 10:43:59 +0000

    Use node:test for reinstated test

--- node-tests/README.md
+++ node-tests/README.md
@@ -1,4 +1,4 @@
-Temporarily migrated two Jest tests to Vitest due to x
+Temporarily migrated tests to node:test due to:
 
 - https://github.com/jestjs/jest/issues/15275
 - https://github.com/jestjs/jest/issues/9430
--- node-tests/require.test.mjs
+++ node-tests/require.test.mjs
@@ -1,11 +1,12 @@
-import { expect, it } from 'vitest';
+import assert from 'node:assert/strict';
 import { createRequire } from 'node:module';
+import test from 'node:test'; /* eslint-disable-line n/no-unsupported-features/node-builtins */
 
 const require = createRequire(import.meta.url);
 
 const stylelint = require('../lib/index.mjs').default;
 
-it('require(ESM) in CommonJS plugin', async () => {
+test('require(ESM) in CommonJS plugin', async () => {
 	const result = await stylelint.lint({
 		code: '.foo {}',
 		config: {
@@ -18,7 +19,7 @@ it('require(ESM) in CommonJS plugin', async () => {
 	});
 	const warnings = result.results[0].warnings;
 
-	expect(warnings).toHaveLength(2);
-	expect(warnings[0].text).toBe('found .foo (plugin/warn-about-foo)');
-	expect(warnings[1].text).toBe('Unexpected empty block (block-no-empty)');
+	assert.strictEqual(warnings.length, 2);
+	assert.strictEqual(warnings[0].text, 'found .foo (plugin/warn-about-foo)');
+	assert.strictEqual(warnings[1].text, 'Unexpected empty block (block-no-empty)');
 });
