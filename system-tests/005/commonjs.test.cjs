5bc11f94fb399cab006a9973fd75306f30ee8c1f
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-12-03 19:14:07 +0900

    Restore `system-tests/005` with `node:test`

---
+++ system-tests/005/commonjs.test.cjs
@@ -0,0 +1,45 @@
+// NOTE: This test uses the `node:test` module to avoid the Jest issue
+// that has not supported `require(ESM)` yet.
+// Ref https://github.com/jestjs/jest/issues/15275
+
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const path = require('node:path');
+
+const stylelint = require('../../lib/index.mjs').default;
+
+test(
+	'CommonJS API and config',
+	async () => {
+		const result = await stylelint.lint({
+			files: [caseFilePath('stylesheet.css')],
+			configFile: caseFilePath('config.cjs'),
+		});
+
+		assert.deepEqual(
+			normalizeResult(result),
+			require('./__snapshots__/commonjs.test.cjs.snap.mjs').default,
+		);
+	},
+	{ timeout: 20000 },
+);
+
+function caseFilePath(basename) {
+	return path.join(__dirname, basename).replaceAll('\\', '/');
+}
+
+function normalizeResult({ cwd, output, report, results, ...rest }) {
+	const dummySource = '/path/to/dummy.css';
+
+	cwd = '/path/to/cwd';
+	output = JSON.parse(output).map((entry) => ({ ...entry, source: dummySource }));
+	report = JSON.parse(report).map((entry) => ({ ...entry, source: dummySource }));
+
+	results = results.map((entry) => {
+		delete entry._postcssResult;
+
+		return { ...entry, source: dummySource };
+	});
+
+	return { cwd, output, report, results, ...rest };
+}
